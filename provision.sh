6af192790ebcf710e961a83739dded6963bf993e
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-04-22 18:32:52 -0500

    wip move to go vs makeself

--- provision.sh
+++ provision.sh
@@ -1,43 +1,14 @@
 #!/bin/bash
-
 set -e
 
-K3S_DIR="/var/lib/rancher/k3s"
-
-# info logs the given argument at info log level.
-info() {
-    tput setaf 6
-    echo "[INFO] " "$@"
-    tput sgr0
-}
-
-# warn logs the given argument at warn log level.
-warn() {
-    tput setaf 3
-    echo "[WARN] " "$@" >&2
-    tput sgr0    
-}
-
-# fatal logs the given argument at fatal log level.
-fatal() {
-    tput setaf 1
-    echo "[ERROR] " "$@" >&2
-    tput sgr0    
-    exit 1
-}
-
-timestamp() {
-  date "+%Y-%m-%d %H:%M:%S"
-}
-
 configureVIP() {
-  info "Discovering ethernet network interface name"
+  # info "Discovering ethernet network interface name"
   vipi=$(ip -o addr show scope global | awk '/^[0-9]:/{print $2}' | cut -f1 -d '/')
 
-  info "Allocating vip on $vipi"
+  # info "Allocating vip on $vipi"
   vipa=$(ip addr show |grep -w inet |grep -v 127.0.0.1|awk '{ print $4}')
 
-  info "Telling kube-vip about what we found"
+  # info "Telling kube-vip about what we found"
   find . -type f -name "kube-vip.yaml" -exec sed -i -e 's|$VIP_INTERFACE|'$vipi'|g' -e 's|$VIP_ADDRESS|'$vipa'|g' {} \;
 }
 
@@ -46,26 +17,20 @@ setupDependencies() {
 
   # https://rancher.com/docs/k3s/latest/en/advanced/#additional-preparation-for-red-hat-centos-enterprise-linux
   if [ -f /etc/redhat-release ]; then
-    info "Setting up dependencies for a RHEL-based distro"
+    # info "Setting up dependencies for a RHEL-based distro"
     systemctl disable firewalld --now
     yum localinstall -y --disablerepo=* --exclude container-selinux-1* /opt/shift/rpms/*.rpm
   fi
 
-  info "Moving k3s components..."
-  mv rancher/ /var/lib/
-  chmod -R 0700 /var/lib/rancher
-
-  info "Moving k3s executable..."
-  mv -f k3s/{k3s,init-k3s.sh} /usr/local/bin
 }
 
 installK3s() {
-  info "Install K3s"
+  # info "Install K3s"
   K3S_KUBECONFIG_MODE="644" \
   INSTALL_K3S_SKIP_DOWNLOAD=true \
       /usr/local/bin/init-k3s.sh --disable=metrics-server --disable=traefik
 
-  info "Setup kubectl autocompletion"
+  # info "Setup kubectl autocompletion"
   /usr/local/bin/k3s kubectl completion bash >/etc/bash_completion.d/kubectl
 }
 
---
+++ src/utils/io.go
@@ -0,0 +1,80 @@
+package utils
+
+import (
+	"embed"
+	log "github.com/sirupsen/logrus"
+	"io/fs"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// VerifyBinary returns true if binary is available
+func VerifyBinary(binary string) bool {
+	_, err := exec.LookPath(binary)
+	return err == nil
+}
+
+// CreateDirectory
+func CreateDirectory(path string, mode os.FileMode) error {
+	if InvalidPath(path) {
+		return os.Mkdir(path, mode)
+	}
+
+	return nil
+}
+
+// Invalid checks if the given path exists
+func InvalidPath(dir string) bool {
+	_, err := os.Stat(dir)
+	return os.IsNotExist(err)
+}
+
+// WriteAssets writes given files to the filesystem from the binary storage
+func WriteAssets(assets embed.FS, source string, destination string) {
+
+	logContext := log.WithFields(log.Fields{
+		"source":      source,
+		"destination": destination,
+	})
+
+	logContext.Info("Expanding resources")
+
+	fs.WalkDir(assets, source, func(path string, d fs.DirEntry, err error) error {
+
+		if err != nil {
+			logContext.Fatal("Error reading embedded resource")
+			return err
+		}
+
+		fullPath := strings.Replace(path, source, destination, 1)
+		fileLogContext := log.WithField("path", fullPath)
+
+		if d.IsDir() {
+			fileLogContext.Info("Created directory")
+			errDir := os.MkdirAll(fullPath, 0700)
+			if errDir != nil {
+				fileLogContext.Fatal("Error creating directory")
+				return err
+			}
+		} else {
+			fileLogContext.Info("Writing file")
+
+			file, err := os.Create(fullPath)
+			if err != nil {
+				fileLogContext.Fatal("Error creating file")
+			}
+
+			defer file.Close()
+
+			fileData, _ := assets.ReadFile(path)
+			_, err = file.Write(fileData)
+			if err != nil {
+				fileLogContext.Fatal("Error writing to file")
+			}
+
+		}
+
+		return nil
+	})
+}
