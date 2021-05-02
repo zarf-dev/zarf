00ad13943a0b7ca1d594e26c212125c7c71f7afb
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-05-02 02:05:52 -0500

    start some go tests
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- src/go.mod
+++ src/go.mod
@@ -3,8 +3,11 @@ module repo1.dso.mil/platform-one/big-bang/apps/product-tools/shift/cli/src
 go 1.16
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/mholt/archiver/v3 v3.5.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
+	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
--- src/internal/utils/io.go
+++ src/internal/utils/io.go
@@ -11,31 +11,38 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var tempDestination string
+var TempDestination string
+var ArchivePath = "shift-pack.tar.zst"
 
 func extractArchive() {
 	tmp, err := ioutil.TempDir("", "shift-pack-")
-	log.WithField("path", tmp).Info("Extracting assets")
+	logContext := log.WithFields(log.Fields{
+		"source":      ArchivePath,
+		"destination": tmp,
+	})
+	
+	logContext.Info("Extracting assets")
 
 	if err != nil {
-		log.WithField("path", tmp).Fatal("Unable to create temp directory")
+		logContext.Fatal("Unable to create temp directory")
 	}
 
-	err = archiver.Unarchive("shift-pack.tar.zst", tmp)
+	err = archiver.Unarchive(ArchivePath, tmp)
 	if err != nil {
-		log.Fatal("Unable to extract the shift-pack.tar.zst contents")
+		logContext.Fatal("Unable to extract the arhive contents")
 	}
 
-	tempDestination = tmp
+	TempDestination = tmp
 }
 
 func AssetPath(partial string) string {
-	if tempDestination == "" {
+	if TempDestination == "" {
 		extractArchive()
 	}
-	return tempDestination + "/" + partial
+	return TempDestination + "/" + partial
 }
 
+// AssetList given a path, return a glob matching all files in the archive
 func AssetList(partial string) []string {
 	path := AssetPath(partial)
 	matches, err := filepath.Glob(path)
