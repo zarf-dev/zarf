b224542dc6ceceb22c66a8f103b47188d7af6956
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-05-17 17:18:56 -0500

    add hostname validity check because the Navy plays dirty with hostnames
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- src/internal/utils/preflight.go
+++ src/internal/utils/preflight.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"os"
 	"runtime"
+	"regexp"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -10,15 +11,31 @@ import (
 // injected checksum for the tarball bundled with this binary
 var packageChecksum string
 
+func IsValidHostName() bool {
+	log.Info("Preflight check: validating hostname")
+	// Quick & dirty character validation instead of a complete RFC validation since the OS is already allowing it
+	expression := regexp.MustCompile(`^[a-zA-Z0-9\-\.]`)
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		return false
+	}
+
+	return expression.MatchString(hostname)
+}
+
 func IsUserRoot() bool {
+	log.Info("Preflight check: validating user is root")
 	return os.Getuid() == 0
 }
 
 func IsAMD64() bool {
+	log.Info("Preflight check: validating AMD64 arch")
 	return runtime.GOARCH == "amd64"
 }
 
 func IsLinux() bool {
+	log.Info("Preflight check: validating os type")
 	return runtime.GOOS == "linux"
 }
 
@@ -37,6 +54,10 @@ func RunPreflightChecks() {
 
 	if !IsUserRoot() {
 		log.Fatal("You must run this program as root.")
+	}
+
+	if !IsValidHostName() {
+		log.Fatal("Please ensure this hostname is valid according to https://www.ietf.org/rfc/rfc1123.txt.")
 	}
 }
 
