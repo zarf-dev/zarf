d21862f93c7658fc6c2b2183a6c8ca8d79428b36
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-05-18 12:53:13 -0500

    handle RHEL env when the package was built without RHEL support
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- src/internal/k3s/rhel.go
+++ src/internal/k3s/rhel.go
@@ -10,6 +10,10 @@ func ConfigureRHEL() {
 	rancherKeyPath := utils.AssetPath("rpms/rancher.key")
 
 	log.Info("Setting up RHEL-specific dependenices and configs")
+	if utils.InvalidPath(rancherKeyPath) {
+		log.Fatal("Package missing RHEL dependencies.  Please ensure this package with built with RHEL=7 or RHEL=8 in the env file.  Refer to the README for more details.")
+	}
+
 	// @todo: k3s docs recommend disabling this, but we should look at just tuning it appropriately
 	utils.ExecCommand([]string{}, "systemctl", "disable", "firewalld", "--now")
 	// Import the rancher gpg for RPM install
