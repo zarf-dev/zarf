1171ffb07b931c3353cba529e254b21d81b1961f
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-05-20 13:13:45 -0500

    continue development of repo pushing into the cluster
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- src/cmd/update.go
+++ src/cmd/update.go
@@ -5,6 +5,7 @@ import (
 	"repo1.dso.mil/platform-one/big-bang/apps/product-tools/shift/cli/src/internal/git"
 )
 
+var gitUrlPath string
 var repoPath string
 
 // updateCmd represents the update command
@@ -12,11 +13,14 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Push latest changes to the utility server",
 	Run: func(cmd *cobra.Command, args []string) {
-		git.Push("http://bigbang:change_me@localhost:8080", repoPath)
+		git.PushAllDirectories("http://bigbang:change_me@localhost:8080", repoPath)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(updateCmd)
+	updateCmd.Flags().StringVarP(&gitUrlPath, "url", "u", "", "The git server url with auth if needed, e.g. \"http://bigbang:change_me@localhost\"")
 	updateCmd.Flags().StringVarP(&repoPath, "path", "p", "", "The path for the repo to evaluate")
+	updateCmd.MarkFlagRequired("url")
+	updateCmd.MarkFlagRequired("path")
 }
