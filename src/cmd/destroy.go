512556e74fba647ac0aa0cd3773e7c6cf4677461
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-05-20 13:13:20 -0500

    change initialize to default dryun and add promps for init/destroy
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- src/cmd/destroy.go
+++ src/cmd/destroy.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/manifoldco/promptui"
 	"repo1.dso.mil/platform-one/big-bang/apps/product-tools/shift/cli/src/internal/utils"
 
 	"github.com/spf13/cobra"
@@ -11,7 +12,16 @@ var destroyCmd = &cobra.Command{
 	Use:   "destroy",
 	Short: "Remove the k3s installation",
 	Run: func(cmd *cobra.Command, args []string) {
-		utils.ExecCommand([]string{}, "/usr/local/bin/k3s-uninstall.sh")
+		prompt := promptui.Prompt{
+			Label:     "Are you sure you want to destroy this deployment?",
+			IsConfirm: true,
+		}
+
+		result, err := prompt.Run()
+
+		if err != nil && result == "y" {
+			utils.ExecCommand([]string{}, "/usr/local/bin/k3s-uninstall.sh")
+		}
 	},
 }
 
