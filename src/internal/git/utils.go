1b445e5513e6c8c5a88fb0fc5322fa25d50130e4
Jeff McCoy <jeffrey.mccoy.10@us.af.mil>
2021-05-23 02:29:55 -0500

    refactor into mutliple repos
    - move src code to shift-cli repo
    - remove custom init functions (payload/Earthfile)
    - simplify internals to focus on basic utility cluster only
    - stadardize earthly targets to common target baseline
    - remove embeded sha has to consume a standard published shift-cli binary
    - switch to shasum for all files in the build output
    -
    
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- src/internal/git/utils.go
+++
@@ -1,9 +0,0 @@
-package git
-
-import "regexp"
-
-func transformURL(baseUrl string, url string) (string, string) {
-	regex := regexp.MustCompile(`://|[^\w\-\.]`)
-	replaced := regex.ReplaceAllString(url, "__")
-	return baseUrl + "/bigbang/" + replaced, replaced
-}
