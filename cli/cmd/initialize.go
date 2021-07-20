90dc1d409ce8a53f4dd997f8e15675a02288de40
Jeff McCoy <code@jeffm.us>
2021-07-20 01:39:11 -0500

    [cli] add ability to inject server keypair for ingress tls

--- cli/cmd/initialize.go
+++ cli/cmd/initialize.go
@@ -5,12 +5,15 @@ import (
 	"repo1.dso.mil/platform-one/big-bang/apps/product-tools/zarf/cli/internal/k3s"
 	"repo1.dso.mil/platform-one/big-bang/apps/product-tools/zarf/cli/internal/utils"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var confirmInit bool
 var host string
 var applianceMode bool
+var certPublicPath string
+var certPrivatePath string
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
@@ -18,7 +21,15 @@ var initCmd = &cobra.Command{
 	Short: "Deploys the utility cluster on a clean linux box",
 	Run: func(cmd *cobra.Command, args []string) {
 		k3s.Install(host, applianceMode)
-		utils.GeneratePKI(host)
+		if certPublicPath != "" && certPrivatePath != "" {
+			logrus.WithFields(logrus.Fields{
+				"public":  certPublicPath,
+				"private": certPrivatePath,
+			}).Info("Injecting user-provided keypair for ingress TLS")
+			utils.InjectServerCert(certPublicPath, certPrivatePath)
+		} else {
+			utils.GeneratePKI(host)
+		}
 	},
 }
 
@@ -27,6 +38,8 @@ func init() {
 	initCmd.Flags().BoolVar(&confirmInit, "confirm", false, "Confirm the install action")
 	initCmd.Flags().StringVar(&host, "host", "zarf-server", "Specify the host or IP for the utility cluster ingress")
 	initCmd.Flags().BoolVar(&applianceMode, "appliance-mode", false, "Enable appliance mode, ensure the "+config.PackageApplianceName+" package is in the same directory")
+	initCmd.Flags().StringVar(&certPublicPath, "server-crt", "", "Path to the server public key if not generating unique PKI")
+	initCmd.Flags().StringVar(&certPrivatePath, "server-key", "", "Path to the server private key if not generating unique PKI")
 	initCmd.MarkFlagRequired("confirm")
 	initCmd.MarkFlagRequired("host")
 }
