1e2459e40a4e6c07a23e0639cfdb4bf64ad42968
Jeff McCoy <code@jeffm.us>
2021-07-22 02:21:34 -0500

    [cli] add package inspect basic content validation

---
+++ cli/internal/packager/inspect.go
@@ -0,0 +1,34 @@
+package packager
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/mholt/archiver/v3"
+	"github.com/sirupsen/logrus"
+	"repo1.dso.mil/platform-one/big-bang/apps/product-tools/zarf/cli/internal/utils"
+)
+
+// Inspect list the contents of a package
+func Inspect(packageName string) {
+	tempPath := createPaths()
+
+	if utils.InvalidPath(packageName) {
+		logrus.WithField("archive", packageName).Fatal("The package archive seems to be missing or unreadable.")
+	}
+
+	// Extract the archive
+	archiver.Extract(packageName, "config.yaml", tempPath.base)
+
+	content, err := ioutil.ReadFile(tempPath.base + "/config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Convert []byte to string and print to screen
+	text := string(content)
+	fmt.Println(text)
+
+	cleanup(tempPath)
+}
