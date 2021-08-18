dcff7a79f08b32321463bf86cd7702ec58a220fb
Jeff McCoy <code@jeffm.us>
2021-08-18 23:20:27 +0000

    add zarf arg helpers for package create/deploy
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -3,13 +3,29 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"repo1.dso.mil/platform-one/big-bang/apps/product-tools/zarf/cli/internal/packager"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "zarf",
+	Use:   "zarf COMMAND|ZARF-PACKAGE|ZARF-CONFIG",
 	Short: "Small tool to bundle dependencies with K3s for airgapped deployments",
+	Args:  cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			if strings.Contains(args[0], "zarf-package-") {
+				packager.Deploy(args[0], confirmDeploy, "")
+				return
+			}
+			if args[0] == "zarf-config.yaml" {
+				packager.Create(confirmCreate)
+				return
+			}
+		}
+		cmd.Help()
+	},
 }
 
 func Execute() {
