f5f2d9157d77d05774afe4aef48c1794aff1e189
Jon Perry <yrrepnoj@gmail.com>
2021-10-27 13:55:55 -0400

    Remove default hostname when regenerating pki and changed to a prompt
    The `zarf pki regenerate` listed 'zarf-server' as the default host but would also
    require the user to pass the --host flag anyways. This was counterintuitive. Updated
    so that if host is not passed as a flag the user would be prompted, similar to how
    it works in the init command.
    
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- cli/cmd/pki.go
+++ cli/cmd/pki.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/defenseunicorns/zarf/cli/internal/utils"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -17,6 +18,15 @@ var pkiRegenerate = &cobra.Command{
 	Use:   "regenerate",
 	Short: "Regenerate the pki certs for the cluster ingress",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Prompt for a hostname if it wasn't provided as a command flag
+		if pkiOptions.Host == "" {
+			prompt := &survey.Input{
+				Message: "Enter a host DNS entry or IP Address for the gitops service ingress",
+			}
+			_ = survey.AskOne(prompt, &pkiOptions.Host, survey.WithValidator(survey.Required))
+		}
+
+		// Verify the hostname provided is valid
 		if !utils.CheckHostName(pkiOptions.Host) {
 			logrus.Fatalf("The hostname provided (%v) was not a valid hostname. The hostname can only contain: 'a-z', 'A-Z', '0-9', '-', and '.' characters.\n", pkiOptions.Host)
 		}
@@ -38,8 +48,7 @@ func init() {
 	pkiCmd.AddCommand(pkiRegenerate)
 	pkiCmd.AddCommand(pkiImport)
 
-	pkiRegenerate.Flags().StringVar(&pkiOptions.Host, "host", "zarf-server", "Specify the host or IP for the gitops service ingress")
-	_ = pkiRegenerate.MarkFlagRequired("host")
+	pkiRegenerate.Flags().StringVar(&pkiOptions.Host, "host", "", "Specify the host or IP for the gitops service ingress")
 
 	pkiImport.Flags().StringVar(&pkiOptions.CertPublicPath, "server-crt", "", "Path to the server public key if not generating unique PKI")
 	pkiImport.Flags().StringVar(&pkiOptions.CertPrivatePath, "server-key", "", "Path to the server private key if not generating unique PKI")
