9385da7bb8d88a1d43b4e811caeece1682f2239a
Andrew Roth <roth.andy@gmail.com>
2021-11-05 13:37:09 -0700

    wip: Utility registry requires credentials

--- cli/internal/utils/auth.go
+++ cli/internal/utils/auth.go
@@ -10,7 +10,7 @@ import (
 )
 // Login adds the given creds to the user's Docker config, usually located at $HOME/.docker/config.yaml. It does not try
 // to connect to the given registry, it just simply adds another entry to the config file.
-// This function was mostly adapted from https://github.com/google/go-containerregistry/blob/main/cmd/crane/cmd/auth.go
+// This function was mostly adapted from https://github.com/google/go-containerregistry/blob/5c9c442d5d68cd96787559ebf6e984c7eb084913/cmd/crane/cmd/auth.go
 func Login(serverAddress string, user string, password string) error {
 	cf, err := config.Load(os.Getenv("DOCKER_CONFIG"))
 	if err != nil {
