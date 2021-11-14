aa562bcc12d997a835b1f26b720633adac664319
Justin Firsching <JustinFirsching@gmail.com>
2021-11-13 23:18:59 -0500

    Git Repository Overhaul (#155)
    Update the pull function so that tag-provided mirrors do not fetch all
    tags, instead later fetching (externally) only the tag they need.
    
    Implement tag fetching function to retrieve only the desired tag when
    creating a tag-provided repo mirror.
    
    Checkout the tag into a detatched HEAD state at the end of the create
    stage of tag-provided repository mirrors.
    
    Implement ref removal and addition functions used during package
    creation and mirror push to ensure that refs that aren't wanted on the
    mirror won't be pushed.
    
    Update the refspecs used in the push to push detatched HEAD, branches,
    online remote, and tags to the offline mirror. Note that if we later
    checkout a branch from the remote and do not clean up the remote ref it
    will lead to a duplicate ref name and the push will fail on one of the
    refs (likely the online one since it is later in the refspec slice).
    
    Fixes #154
    
    feat: Allow for repos to be provided without a tag to mirror all branches/tags
    feat: Make tag-provided repository mirrors use the tag as master
    fix: Prevent tag-provided repo mirrors from storing extra refs
    fix: tag-provided clones use trunk branch name
    docs: Update gitops-data Example README
    Signed-off-by: Jeff McCoy <code@jeffm.us>

--- cli/internal/git/pull.go
+++ cli/internal/git/pull.go
@@ -5,20 +5,27 @@ import (
 
 	"github.com/defenseunicorns/zarf/cli/internal/utils"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/sirupsen/logrus"
+
+	"strings"
 )
 
 const onlineRemoteName = "online-upstream"
 
 func DownloadRepoToTemp(gitUrl string) string {
 	path := utils.MakeTempDir()
+	// If downloading to temp, grab all tags since the repo isn't being
+	// packaged anyways and it saves us from having to fetch the tags
+	// later if we need them
 	pull(gitUrl, path)
 	return path
 }
 
-func Pull(gitUrl string, targetFolder string) {
+func Pull(gitUrl string, targetFolder string) string {
 	path := targetFolder + "/" + transformURLtoRepoName(gitUrl)
 	pull(gitUrl, path)
+	return path
 }
 
 func pull(gitUrl string, targetFolder string) {
@@ -29,10 +36,16 @@ func pull(gitUrl string, targetFolder string) {
 
 	gitCred := FindAuthForHost(gitUrl)
 
+	matches := strings.Split(gitUrl, "@")
+	fetchAllTags := len(matches) == 1
 	cloneOptions := &git.CloneOptions{
-		URL:        gitUrl,
+		URL:        matches[0],
 		Progress:   os.Stdout,
 		RemoteName: onlineRemoteName,
+	}
+
+	if !fetchAllTags {
+		cloneOptions.Tags = git.NoTags
 	}
 
 	// Gracefully handle no git creds on the system (like our CI/CD)
@@ -41,13 +54,39 @@ func pull(gitUrl string, targetFolder string) {
 	}
 
 	// Clone the given repo
-	_, err := git.PlainClone(targetFolder, false, cloneOptions)
+	repo, err := git.PlainClone(targetFolder, false, cloneOptions)
 
 	if err == git.ErrRepositoryAlreadyExists {
 		logContext.Info("Repo already cloned")
 	} else if err != nil {
 		logContext.Debug(err)
 		logContext.Fatal("Not a valid git repo or unable to clone")
+	}
+
+	if !fetchAllTags {
+		tag := matches[1]
+
+		// Identify the remote trunk branch name
+		trunkBranchName := plumbing.NewBranchReferenceName("master")
+		head, err := repo.Head()
+
+		if err != nil {
+			// No repo head available
+			logContext.Debug(err)
+			logContext.Warn("Failed to identify repo head. Tag will be pushed to 'master'.")
+		} else if head.Name().IsBranch() {
+			// Valid repo head and it is a branch
+			trunkBranchName = head.Name()
+		} else {
+			// Valid repo head but not a branch
+			logContext.Warn("No branch found for this repo head. Tag will be pushed to 'master'.")
+		}
+
+		RemoveLocalBranchRefs(targetFolder)
+		RemoveOnlineRemoteRefs(targetFolder)
+
+		FetchTag(targetFolder, tag)
+		CheckoutTagAsBranch(targetFolder, tag, trunkBranchName)
 	}
 
 	logContext.Info("Git repo synced")
