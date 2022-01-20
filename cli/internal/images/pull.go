d15a3bdeaee4714657472ee0794a8d1141fce8d0
Jeff McCoy <code@jeffm.us>
2022-01-20 17:42:40 -0600

    Multi-distro support
    - add arm64/generic arch support
    - adds in-memory embedded registry to serve registry image from "seed" config spec
    - add local-distro image injection support
    - handles helm charts install/upgrade/rollback/uninstall via helm directly
    - manages raw manifests as zarf-generated helm charts for helm-native management
    - add tunnel capability to access zarf endpoints without ingress config
    - flattens image collection to single tarball
    - remove root / linux requirements for zarf deployments
    - add K8s distro-detection and distro-specific seed image behaviors
    - document local registry per https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
    - reorganize logging, remove logrus, replace with pterm, add progress cli feedback
    - add support for stacking helm values files
    - normalize log levels: info, warn, debug, trace 
    - update go mod deps
    - move zarf state to a k8s secret in the zarf namespace
    - support zarf destroy for 3rd-party clusters
    - move crane image cache to sub folder under home
    - add support for multiple users with htpasswd
    - add path key for git-based helm charts
    - remove need for PKI prompts on default init
    - add storage class tracking in zarf state for multi-distro compatibility 
    - reduce code for init/deploy to the same function call

--- cli/internal/images/pull.go
+++ cli/internal/images/pull.go
@@ -1,33 +1,111 @@
 package images
 
 import (
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/defenseunicorns/zarf/cli/config"
+	"github.com/defenseunicorns/zarf/cli/internal/message"
+	"github.com/defenseunicorns/zarf/cli/internal/utils"
 	"github.com/google/go-containerregistry/pkg/crane"
+	"github.com/google/go-containerregistry/pkg/logs"
+	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/cache"
-	"github.com/sirupsen/logrus"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/pterm/pterm"
 )
 
-const cachePath = ".image-cache"
-
 func PullAll(buildImageList []string, imageTarballPath string) {
-	logrus.Info("Loading images")
-	cranePlatformOptions := crane.WithPlatform(&v1.Platform{OS: "linux", Architecture: "amd64"})
+	var (
+		longer     string
+		imageCount = len(buildImageList)
+	)
+
+	// Give some additional user feedback on larger image sets
+	if imageCount > 15 {
+		longer = "This step may take a couple of minutes to complete."
+	} else if imageCount > 5 {
+		longer = "This step may take several seconds to complete."
+	}
+
+	spinner := message.NewProgressSpinner("Loading metadata for %d images. %s", imageCount, longer)
+	defer spinner.Stop()
+
 	imageMap := map[string]v1.Image{}
 
-	for _, src := range buildImageList {
-		logContext := logrus.WithField("image", src)
-		logContext.Info("Fetching image metadata")
-		img, err := crane.Pull(src, cranePlatformOptions)
+	if message.GetLogLevel() >= message.DebugLevel {
+		logs.Warn.SetOutput(spinner)
+		logs.Progress.SetOutput(spinner)
+	}
+
+	for idx, src := range buildImageList {
+		spinner.Updatef("Fetching image metadata (%d of %d): %s", idx+1, imageCount, src)
+		img, err := crane.Pull(src, config.ActiveCranePlatform)
 		if err != nil {
-			logContext.Warn("Unable to pull the image")
+			spinner.Fatalf(err, "Unable to pull the image %s", src)
 		}
 		img = cache.Image(img, cache.NewFilesystemCache(cachePath))
 		imageMap[src] = img
 	}
 
-	logrus.Info("Creating image tarball (this will take a while)")
-	if err := crane.MultiSave(imageMap, imageTarballPath); err != nil {
-		logrus.Debug(err)
-		logrus.Fatal("Unable to save the tarball")
+	spinner.Updatef("Creating image tarball (this will take a while)")
+
+	tagToImage := map[name.Tag]v1.Image{}
+
+	for src, img := range imageMap {
+		ref, err := name.ParseReference(src)
+		if err != nil {
+			spinner.Fatalf(err, "parsing ref %q", src)
+		}
+
+		tag, ok := ref.(name.Tag)
+		if !ok {
+			d, ok := ref.(name.Digest)
+			if !ok {
+				spinner.Fatalf(nil, "image reference %s wasn't a tag or digest", src)
+			}
+			tag = d.Repository.Tag("digest-only")
+		}
+		tagToImage[tag] = img
+	}
+	spinner.Success()
+
+	progress := make(chan v1.Update, 200)
+
+	go func() {
+		_ = tarball.MultiWriteToFile(imageTarballPath, tagToImage, tarball.WithProgress(progress))
+	}()
+
+	var progressBar *pterm.ProgressbarPrinter
+	var title string
+
+	for update := range progress {
+		switch {
+		case update.Error != nil && errors.Is(update.Error, io.EOF):
+			_, _ = progressBar.Stop()
+			title = fmt.Sprintf("Pulling %v images (%s)", len(imageMap), utils.ByteFormat(float64(update.Total), 2))
+			pterm.Success.Println(title)
+			return
+		case update.Error != nil:
+			message.Fatal(update.Error, "error writing image tarball")
+		default:
+			title = fmt.Sprintf("Pulling %v images (%s of %s)", len(imageMap),
+				utils.ByteFormat(float64(update.Complete), 2),
+				utils.ByteFormat(float64(update.Total), 2),
+			)
+			if progressBar == nil {
+				progressBar, _ = pterm.DefaultProgressbar.
+					WithTotal(int(update.Total)).
+					WithShowCount(false).
+					WithTitle(title).
+					WithRemoveWhenDone(true).
+					Start()
+			}
+			progressBar.UpdateTitle(title)
+			chunk := int(update.Complete) - progressBar.Current
+			progressBar.Add(chunk)
+		}
 	}
 }
