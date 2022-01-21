658237fccd0d6aa1a8a74a8d3137363202fdcec9
Jeff McCoy <code@jeffm.us>
2022-01-21 03:40:53 -0600

    unify init/deploy & add basic chart validation before create

--- cli/internal/packager/create.go
+++ cli/internal/packager/create.go
@@ -2,6 +2,7 @@ package packager
 
 import (
 	"fmt"
+	"github.com/defenseunicorns/zarf/cli/internal/packager/validate"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -34,6 +35,9 @@ func Create() {
 	if err := config.BuildConfig(configFile); err != nil {
 		message.Fatalf(err, "Unable to write the %s file", configFile)
 	}
+
+	// Perform early package validation
+	validate.Run()
 
 	if !confirmAction(configFile, "Create") {
 		os.Exit(0)
