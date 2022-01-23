9d5c28a22f8ec5878e0122fce3c2134066c98bc6
Jeff McCoy <code@jeffm.us>
2022-01-23 04:36:12 -0600

    secret cleanup

--- cli/internal/template/template.go
+++ cli/internal/template/template.go
@@ -10,15 +10,18 @@ import (
 )
 
 type Values struct {
-	state          types.ZarfState
-	htpasswd       string
-	seedRegistry   string
-	registry       string
-	registryPush   string
-	registryPull   string
-	registrySecret string
-	gitPush        string
-	gitPull        string
+	state        types.ZarfState
+	registry     string
+	seedRegistry string
+	secret       struct {
+		htpasswd       string
+		registryPush   string
+		registryPull   string
+		registrySecret string
+		gitPush        string
+		gitPull        string
+		logging        string
+	}
 }
 
 func Generate() Values {
@@ -33,24 +36,26 @@ func Generate() Values {
 		message.Debug(errPush, errPull)
 		message.Fatal(nil, "Unable to define `htpasswd` string for the Zarf user")
 	}
-	generated.htpasswd = fmt.Sprintf("%s\\n%s", pushUser, pullUser)
+	generated.secret.htpasswd = fmt.Sprintf("%s\\n%s", pushUser, pullUser)
 
 	generated.registry = config.GetRegistry()
 	generated.seedRegistry = config.GetSeedRegistry()
 
-	generated.registryPush = config.GetSecret(config.StateRegistryPush)
-	generated.registryPull = config.GetSecret(config.StateRegistryPull)
-	generated.registrySecret = config.GetSecret(config.StateRegistrySecret)
+	generated.secret.registryPush = config.GetSecret(config.StateRegistryPush)
+	generated.secret.registryPull = config.GetSecret(config.StateRegistryPull)
+	generated.secret.registrySecret = config.GetSecret(config.StateRegistrySecret)
 
-	generated.gitPush = config.GetSecret(config.StateGitPush)
-	generated.gitPull = config.GetSecret(config.StateGitPull)
+	generated.secret.gitPush = config.GetSecret(config.StateGitPush)
+	generated.secret.gitPull = config.GetSecret(config.StateGitPull)
+
+	generated.secret.logging = config.GetSecret(config.StateLogging)
 
 	message.Debugf("Template values: %v", generated)
 	return generated
 }
 
 func (values Values) Ready() bool {
-	return values.htpasswd != ""
+	return values.secret.htpasswd != ""
 }
 
 func (values Values) GetRegistry() string {
@@ -63,7 +68,7 @@ func (values Values) Apply(path string) {
 
 	if !values.Ready() {
 		// This should only occur if the state couldn't be pulled or on init if a template is attempted before the pre-seed stage
-		message.Fatalf(nil, "template.Apply() called bofore template.Generate()")
+		message.Fatalf(nil, "template.Apply() called before template.Generate()")
 	}
 
 	mappings := map[string]string{
@@ -71,12 +76,13 @@ func (values Values) Apply(path string) {
 		"SEED_REGISTRY":      values.seedRegistry,
 		"REGISTRY":           values.registry,
 		"REGISTRY_NODEPORT":  values.state.Registry.NodePort,
-		"REGISTRY_SECRET":    values.registrySecret,
-		"REGISTRY_AUTH_PUSH": values.registryPush,
-		"REGISTRY_AUTH_PULL": values.registryPull,
-		"GIT_AUTH_PUSH":      values.gitPush,
-		"GIT_AUTH_PULL":      values.gitPull,
-		"HTPASSWD":           values.htpasswd,
+		"REGISTRY_SECRET":    values.secret.registrySecret,
+		"REGISTRY_AUTH_PUSH": values.secret.registryPush,
+		"REGISTRY_AUTH_PULL": values.secret.registryPull,
+		"GIT_AUTH_PUSH":      values.secret.gitPush,
+		"GIT_AUTH_PULL":      values.secret.gitPull,
+		"LOGGING_AUTH":       values.secret.logging,
+		"HTPASSWD":           values.secret.htpasswd,
 	}
 
 	message.Debug(mappings)
