593c8ef53b15b02823c1dbdcad1d56f28a540679
Jeff McCoy <code@jeffm.us>
2022-01-23 04:19:12 -0600

    Change pterm default out to stderr for cleaner output capture

--- cli/internal/message/message.go
+++ cli/internal/message/message.go
@@ -25,6 +25,8 @@ const (
 var logLevel = InfoLevel
 
 func init() {
+	// Help capture text cleaner
+	pterm.SetDefaultOutput(os.Stderr)
 	pterm.ThemeDefault.SuccessMessageStyle = *pterm.NewStyle(pterm.FgLightGreen)
 	// Customize default error.
 	pterm.Success.Prefix = pterm.Prefix{
