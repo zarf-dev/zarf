2e6e40070b0285d47f194c01a1a84eb3dfd21b85
Andy Roth <roth.andy@gmail.com>
2022-01-26 11:05:25 -0800

    Add skeleton of new E2E test for testing the Data Injection example (#242)
    Signed-off-by: Jeff McCoy <code@jeffm.us>

---
+++ test/e2e/e2e_data_injection_test.go
@@ -0,0 +1,67 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestDataInjection(t *testing.T) {
+	t.Parallel()
+	return
+	//// Our SSH username, will change based on which AMI we use
+	//username := "ubuntu"
+	//
+	//// Copy the terraform folder to a temp directory so we can run multiple tests in parallel
+	//tmpFolder := teststructure.CopyTerraformFolderToTemp(t, "..", "tf/public-ec2-instance")
+	//
+	//// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	//defer teststructure.RunTestStage(t, "TEARDOWN", func() {
+	//	teardown(t, tmpFolder)
+	//})
+	//
+	//// Deploy the terraform infra
+	//teststructure.RunTestStage(t, "SETUP", func() {
+	//	setup(t, tmpFolder)
+	//})
+	//
+	//// Upload the Zarf artifacts
+	//teststructure.RunTestStage(t, "UPLOAD", func() {
+	//	terraformOptions := teststructure.LoadTerraformOptions(t, tmpFolder)
+	//	keyPair := teststructure.LoadEc2KeyPair(t, tmpFolder)
+	//
+	//	syncFileToRemoteServer(t, terraformOptions, keyPair, username, "../../build/zarf", fmt.Sprintf("/home/%s/build/zarf", username), "0700")
+	//	syncFileToRemoteServer(t, terraformOptions, keyPair, username, "../../build/zarf-init.tar.zst", fmt.Sprintf("/home/%s/build/zarf-init.tar.zst", username), "0600")
+	//})
+	//
+	//teststructure.RunTestStage(t, "TEST", func() {
+	//	terraformOptions := teststructure.LoadTerraformOptions(t, tmpFolder)
+	//	keyPair := teststructure.LoadEc2KeyPair(t, tmpFolder)
+	//
+	//	// Finally run the actual test
+	//	testGiteaAndGrafana(t, terraformOptions, keyPair, username)
+	//})
+}
+
+//func testGiteaAndGrafana(t *testing.T, terraformOptions *terraform.Options, keyPair *aws.Ec2Keypair, username string) {
+//	// Run `terraform output` to get the value of an output variable
+//	publicInstanceIP := terraform.Output(t, terraformOptions, "public_instance_ip")
+//
+//	// We're going to try to SSH to the instance IP, using the Key Pair we created earlier, and the user "ubuntu",
+//	// as we know the Instance is running an Ubuntu AMI that has such a user
+//	publicHost := ssh.Host{
+//		Hostname:    publicInstanceIP,
+//		SshKeyPair:  keyPair.KeyPair,
+//		SshUserName: username,
+//	}
+//
+//	// run `zarf init`
+//	output, err := ssh.CheckSshCommandE(t, publicHost, fmt.Sprintf("sudo bash -c 'cd /home/%s/build && ./zarf init --confirm --components management,logging,gitops-service --host 127.0.0.1'", username))
+//	require.NoError(t, err, output)
+//
+//	// Make sure Gitea comes up cleanly
+//	output, err = ssh.CheckSshCommandE(t, publicHost, "timeout 300 bash -c 'while [[ \"$(curl -sfSL --retry 15 --retry-connrefused --retry-delay 5 -o /dev/null -w \"%{http_code}\" \"https://127.0.0.1/api/v1/user\")\" != \"401\" ]]; do sleep 1; done' || false")
+//	require.NoError(t, err, output)
+//
+//	// Make sure Grafana comes up cleanly
+//	output, err = ssh.CheckSshCommandE(t, publicHost, "timeout 300 bash -c 'while [[ \"$(curl -sfSL --retry 15 --retry-connrefused --retry-delay 5 -o /dev/null -w \"%{http_code}\" \"https://127.0.0.1/monitor/api/org\")\" != \"401\" ]]; do sleep 1; done' || false")
+//	require.NoError(t, err, output)
+//}
