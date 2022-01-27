2658d2cd7c08346f1901ed510c93885c1249133a
Jeff McCoy <code@jeffm.us>
2022-01-27 01:06:54 -0600

    more small cli ui cleanup

--- cli/internal/utils/auth.go
+++ cli/internal/utils/auth.go
@@ -1,18 +1,20 @@
 package utils
 
 import (
+	"os"
+
+	"github.com/defenseunicorns/zarf/cli/internal/message"
 	"github.com/docker/cli/cli/config"
 	"github.com/docker/cli/cli/config/types"
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
-	"log"
-	"os"
 )
 
-// Login adds the given creds to the user's Docker config, usually located at $HOME/.docker/config.yaml. It does not try
+// DockerLogin adds the given creds to the user's Docker config, usually located at $HOME/.docker/config.yaml. It does not try
 // to connect to the given registry, it just simply adds another entry to the config file.
 // This function was mostly adapted from https://github.com/google/go-containerregistry/blob/5c9c442d5d68cd96787559ebf6e984c7eb084913/cmd/crane/cmd/auth.go
-func Login(serverAddress string, user string, password string) error {
+func DockerLogin(serverAddress string, user string, password string) error {
+	message.Debugf("utils.DockerLogin(%s, %s, %s)", serverAddress, user, password)
 	cf, err := config.Load(os.Getenv("DOCKER_CONFIG"))
 	if err != nil {
 		return err
@@ -32,6 +34,6 @@ func Login(serverAddress string, user string, password string) error {
 	if err := cf.Save(); err != nil {
 		return err
 	}
-	log.Printf("logged in via %s", cf.Filename)
+	message.Debugf("logged in via %s", cf.Filename)
 	return nil
 }
