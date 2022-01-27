7549e7b6a1bf71f478c7602fc57c4dfa621c9c69
Jon Perry <yrrepnoj@gmail.com>
2022-01-27 10:49:58 -0500

    Actually remove HandleIfURL tempPath early deletion

--- cli/internal/packager/common.go
+++ cli/internal/packager/common.go
@@ -173,7 +173,6 @@ func HandleIfURL(packagePath string, shasum string, insecureDeploy bool) (string
 
 	// Write the package to a local file
 	tempPath := createPaths()
-	defer tempPath.clean()
 
 	localPackagePath := tempPath.base + providedURL.Path
 	message.Debugf("Creating local package with the path: %s", localPackagePath)
