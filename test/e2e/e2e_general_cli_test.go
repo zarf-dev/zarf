d3e65b2358418567d6e6b999470b8c10b180629d
Jon Perry <yrrepnoj@gmail.com>
2022-02-14 11:00:17 -0500

    Initial attempt at reworking e2e tests
    This is an potential implementation as to how we can write our e2e tests so that they can be run as part of a build pipeline as well as run locally.
    In both cases, the e2e uses a KinD cluster to test against.

--- test/e2e/e2e_general_cli_test.go
+++ test/e2e/e2e_general_cli_test.go
@@ -2,91 +2,95 @@ package test
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"testing"
 
-	teststructure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestGeneralCli(t *testing.T) {
+var (
+	e2e ZarfE2ETest
+)
 
-	e2e := NewE2ETest(t)
+// TestMain will exec each test, one by one
+func TestMain(m *testing.M) {
+	// Create a kubeconfig and start up a KinD cluster
+	err := e2e.setUp()
+	if err != nil {
+		fmt.Printf("Unable to setup environment to run the e2e test because of err: %v\n", err)
+		os.Exit(1)
+	}
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer teststructure.RunTestStage(e2e.testing, "TEARDOWN", e2e.teardown)
+	// exec test and this returns an exit code to pass to os
+	retCode := m.Run()
 
-	// Upload the Zarf artifacts
-	teststructure.RunTestStage(e2e.testing, "UPLOAD", func() {
-		e2e.syncFileToRemoteServer("../../build/zarf", fmt.Sprintf("/home/%s/build/zarf", e2e.username), "0700")
-		e2e.syncFileToRemoteServer("../../build/zarf-init.tar.zst", fmt.Sprintf("/home/%s/build/zarf-init.tar.zst", e2e.username), "0700")
-		e2e.syncFileToRemoteServer("../../build/zarf-package-kafka-strimzi-demo.tar.zst", fmt.Sprintf("/home/%s/build/zarf-package-kafka-strimzi-demo.tar.zst", e2e.username), "0700")
-	})
+	// Unless told to skip, destroy the KinD Cluster and delete the generated kubeconfig
+	if os.Getenv("SKIP_TEARDOWN") == "" && !e2e.clusterAlreadyExists {
+		err = e2e.tearDown()
+		if err != nil {
+			fmt.Printf("Unable to teardown test environment after completion of tests: %v\n", err)
+		}
+	}
 
-	teststructure.RunTestStage(e2e.testing, "TEST", func() {
-		// Test `zarf prepare sha256sum` for a local asset
-		expectedShasum := "61b50898f982d015ed87093ba822de0fe011cec6dd67db39f99d8c56391a6109\n"
-		output, err := e2e.runSSHCommand("cd /home/%s/build && echo 'random test data 🦄' > shasum-test-file", e2e.username)
-		require.NoError(e2e.testing, err, output)
+	// If exit code is distinct of zero, the test will be failed (red)
+	os.Exit(retCode)
+}
 
-		output, err = e2e.runSSHCommand("cd /home/%s/build && ./zarf prepare sha256sum shasum-test-file 2> /dev/null", e2e.username)
-		require.NoError(e2e.testing, err, output)
-		assert.Equal(e2e.testing, expectedShasum, output, "The expected SHASUM should equal the actual SHASUM")
+func TestGeneralCLI(t *testing.T) {
+	// Test `zarf prepare sha256sum` for a local asset
+	expectedShasum := "61b50898f982d015ed87093ba822de0fe011cec6dd67db39f99d8c56391a6109\n"
 
-		// Test `zarf prepare sha256sum` for a remote asset
-		expectedShasum = "c3cdea0573ba5a058ec090b5d2683bf398e8b1614c37ec81136ed03b78167617\n"
-		output, err = e2e.runSSHCommand("cd /home/%s/build && ./zarf prepare sha256sum https://zarf-public.s3-us-gov-west-1.amazonaws.com/pipelines/zarf-prepare-shasum-remote-test-file.txt 2> /dev/null", e2e.username)
-		require.NoError(e2e.testing, err, output)
-		assert.Equal(e2e.testing, expectedShasum, output, "The expected SHASUM should equal the actual SHASUM")
+	// TODO: There has to be a better way to pipe this output to the file.. For some reason exec.Command( ... > file ).Output() couldn't pipe to file
+	// output, err = exec.Command("bash", "-c", "\"echo 'random test data 🦄' > shasum-test-file\"").Output()
+	shasumTestFilePath := "shasum-test-file"
+	testfile, _ := os.Create(shasumTestFilePath)
+	cmd := exec.Command("echo", "random test data 🦄")
+	cmd.Stdout = testfile
+	cmd.Run()
 
-		// Test `zarf version`
-		output, err = e2e.runSSHCommand("cd /home/%s/build && ./zarf version", e2e.username)
-		require.NoError(e2e.testing, err, output)
-		assert.NotNil(e2e.testing, output)
-		assert.NotEqual(e2e.testing, len(output), 0, "Zarf version should not be an empty string")
-		assert.NotEqual(e2e.testing, string(output), "UnknownVersion", "Zarf version should not be the default value")
+	output, err := exec.Command(e2e.zarfBinPath, "prepare", "sha256sum", shasumTestFilePath).Output()
+	assert.NoError(t, err, output)
+	assert.Equal(t, expectedShasum, string(output), "The expected SHASUM should equal the actual SHASUM")
+	os.Remove(shasumTestFilePath)
 
-		// Test for expected failure when given a bad component input
-		output, err = e2e.runSSHCommand("cd /home/%s/build && ./zarf init --confirm --components k3s,foo,logging", e2e.username)
-		require.Error(e2e.testing, err, output)
+	// Test `zarf prepare sha256sum` for a remote asset
+	expectedShasum = "c3cdea0573ba5a058ec090b5d2683bf398e8b1614c37ec81136ed03b78167617\n"
+	output, err = exec.Command(e2e.zarfBinPath, "prepare", "sha256sum", "https://zarf-public.s3-us-gov-west-1.amazonaws.com/pipelines/zarf-prepare-shasum-remote-test-file.txt").Output()
+	assert.NoError(t, err, output)
+	assert.Equal(t, expectedShasum, string(output), "The expected SHASUM should equal the actual SHASUM")
 
-		// Test for expected failure when given invalid hostnames
-		output, err = e2e.runSSHCommand("cd /home/%s/build && ./zarf init --confirm --host localhost", e2e.username)
-		require.Error(e2e.testing, err, output)
+	// Test `zarf version`
+	output, err = exec.Command(e2e.zarfBinPath, "version").Output()
+	assert.NoError(t, err)
+	assert.NotEqual(t, len(output), 0, "Zarf version should not be an empty string")
+	assert.NotEqual(t, string(output), "UnknownVersion", "Zarf version should not be the default value")
 
-		output, err = e2e.runSSHCommand("cd /home/%s/build && ./zarf pki regenerate --host zarf@server", e2e.username)
-		require.Error(e2e.testing, err, output)
-		output, err = e2e.runSSHCommand("cd /home/%s/build && ./zarf pki regenerate --host some_unique_server", e2e.username)
-		require.Error(e2e.testing, err, output)
+	// Test for expected failure when given a bad componenet input
+	output, err = exec.Command(e2e.zarfBinPath, "init", "--confirm", "--components=k3s,foo,logging").Output()
+	assert.Error(t, err)
 
-		// Initialize Zarf for the next set of tests
-		output, err = e2e.runSSHCommand("sudo bash -c 'cd /home/%s/build && ./zarf init --confirm --components k3s'", e2e.username)
-		require.NoError(e2e.testing, err, output)
+	// Test for expected failure when given invalid hostnames
+	output, err = exec.Command(e2e.zarfBinPath, "pki", "regenerate", "--host", "zarf@server").Output()
+	assert.Error(t, err, output)
+	output, err = exec.Command(e2e.zarfBinPath, "pki", "regenerate", "--host=some_unique_server").Output()
+	assert.Error(t, err, output)
 
-		// Verify that we do not timeout when passing the `--confirm` flag without specifying the `--components` flag
-		output, err = e2e.runSSHCommand("sudo timeout 120 sudo bash -c 'cd /home/%s/build && ./zarf package deploy zarf-package-kafka-strimzi-demo.tar.zst --confirm' || false", e2e.username)
-		require.NoError(e2e.testing, err, output)
+	// Test that changing the log level actually applies the requested level
+	output, _ = exec.Command(e2e.zarfBinPath, "version", "--log-level=warn").CombinedOutput()
+	expectedOutString := "Log level set to warn"
+	require.Contains(t, string(output), expectedOutString, "The log level should be changed to 'warn'")
+}
 
-		// Test that Zarf checks the $KUBECONFIG env variable
-		output, err = e2e.runSSHCommand("sudo timeout 120 sudo bash -c 'rm -rf /tmp/zarf-* && mv /root/.kube/config /home/%s/zarf-kube-config && export KUBECONFIG=/home/%s/zarf-kube-config && echo 'JPERRYISSTINKY' && echo $KUBECONFIG && cd /home/%s/build && ./zarf package deploy zarf-package-kafka-strimzi-demo.tar.zst -l trace --confirm' || false", e2e.username, e2e.username, e2e.username)
-		require.NoError(e2e.testing, err, output)
+func TestInitZarf(t *testing.T) {
 
-		// Test that `zarf package deploy` doesn't die when given a URL
-		// NOTE: Temporarily commenting this out because this seems out of scope for a general cli test. Having this included also means we would have to fully standup a `zarf init` command.
-		// TODO: Move this to it's own e2e test.
-		// output, err = ssh.CheckSshCommandE(t, publicHost, fmt.Sprintf("sudo bash -c 'cd /home/%s/build && ./zarf package deploy https://zarf-examples.s3.amazonaws.com/zarf-package-appliance-demo-doom.tar.zst --confirm --insecure'", username))
-		// require.NoError(t, err, output)
-		// output, err = ssh.CheckSshCommandE(t, publicHost, fmt.Sprintf("sudo bash -c 'cd /home/%s/build && ./zarf package deploy https://zarf-examples.s3.amazonaws.com/zarf-package-appliance-demo-doom.tar.zst --confirm --shasum e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'", username))
-		// require.NoError(t, err, output)
+	// Initialize Zarf for the next set of tests
+	// This also confirms that using the `--confirm` flags does not hang when not also specifying the `--components` flag
+	output, err := exec.Command(e2e.zarfBinPath, "init", "--confirm").CombinedOutput()
+	assert.NoError(t, err, string(output))
 
-		// Test that `zarf package deploy` gives an error if deploying a remote package without the --insecure or --shasum flags
-		output, err = e2e.runSSHCommand("sudo bash -c 'cd /home/%s/build && ./zarf package deploy https://zarf-examples.s3.amazonaws.com/zarf-package-appliance-demo-doom-20210125.tar.zst --confirm'", e2e.username)
-		require.Error(e2e.testing, err, output)
-
-		// Test that changing the log level actually applies the requested level
-		output, _ = e2e.runSSHCommand("cd /home/%s/build && ./zarf version --log-level warn 1> /dev/null", e2e.username)
-		expectedOutString := "Log level set to warn"
-		require.Contains(e2e.testing, output, expectedOutString, "The log level should be changed to 'warn'")
-	})
-
+	// Test that `zarf package deploy` gives an error if deploying a remote package without the --insecure or --shasum flags
+	output, err = exec.Command(e2e.zarfBinPath, "package", "deploy", "https://zarf-examples.s3.amazonaws.com/zarf-package-appliance-demo-doom-20210125.tar.zst", "--confirm").Output()
+	assert.Error(t, err, string(output))
 }
