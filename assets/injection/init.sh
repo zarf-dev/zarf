8c9a9b395f049b9c332243ca006bf647a1eed34d
Jeff McCoy <code@jeffm.us>
2022-02-14 16:41:08 +0000

    more injection.sh testing

--- assets/injection/init.sh
+++ assets/injection/init.sh
@@ -1,19 +1,15 @@
 #!/zarf-bin/busybox sh
 set -ex
 
-RUN=/zarf-bin/busybox
-
-# Before running anything, verify the busybox binary 
-RUN echo "$SHA256_BUSYBOX  /zarf-bin/busybox" | RUN sha256sum -c
+# Before /zarf-bin/busyboxning anything, verify the busybox binary 
+/zarf-bin/busybox echo "$SHA256_BUSYBOX  /zarf-bin/busybox" | /zarf-bin/busybox sha256sum -c
 
 # Wait to receive files piped via tar/netcat
-RUN netcat -l -p 25000 -w 360 | RUN tar xv
+/zarf-bin/busybox netcat -l -p 25000 -w 360 | /zarf-bin/busybox tar xv
 
 # Verify that the zarf assets are properly loaded
-RUN echo "$SHA256_ZARF zarf" | RUN sha256sum -c
-RUN echo "$SHA256_IMAGES seed-images.tar" | RUN sha256sum -c
+/zarf-bin/busybox echo "$SHA256_ZARF  zarf" | /zarf-bin/busybox sha256sum -c
+/zarf-bin/busybox echo "$SHA256_IMAGES  seed-images.tar" | /zarf-bin/busybox sha256sum -c
 
 # Load the seed registry
 /payload/zarf init bootstrap /payload/seed-images.tar library/registry:2.7.1 -l=trace
-
-
