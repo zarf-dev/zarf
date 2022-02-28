77980858d414e85cde22a8f187554f4a33b39db7
Jeff McCoy <code@jeffm.us>
2022-02-28 03:37:19 -0600

    wait up to 5 minutes for an image to exist for k8s.GetAllImages()

--- cli/internal/k8s/images.go
+++ cli/internal/k8s/images.go
@@ -3,14 +3,40 @@ package k8s
 import (
 	"fmt"
 	"sort"
+	"time"
 
+	"github.com/defenseunicorns/zarf/cli/internal/message"
 	corev1 "k8s.io/api/core/v1"
 )
 
 type ImageMap map[string]bool
 
 func GetAllImages() ([]string, error) {
-	return GetImages(corev1.NamespaceAll)
+	var images []string
+	var err error
+	timeout := time.After(5 * time.Minute)
+
+	for {
+		// delay check 3 seconds
+		time.Sleep(2 * time.Second)
+		select {
+
+		// on timeout abort
+		case <-timeout:
+			message.Debug("get image list timed-out")
+			return images, nil
+
+		// after delay, try running
+		default:
+			// If no images or an error, log and loop
+			if images, err = GetImages(corev1.NamespaceAll); len(images) < 1 || err != nil {
+				message.Debug(err)
+			} else {
+				// Otherwise, return the image list
+				return images, nil
+			}
+		}
+	}
 }
 
 func GetImages(namespace string) ([]string, error) {
