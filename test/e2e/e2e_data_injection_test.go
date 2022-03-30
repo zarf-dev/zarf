a48c9f040343e04a502c639c42cbfe1e933fbf86
Jon Perry <yrrepnoj@gmail.com>
2022-03-30 15:56:12 +0000

    fix typo and remove unnecessary retry loop

--- test/e2e/e2e_data_injection_test.go
+++ test/e2e/e2e_data_injection_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -22,35 +21,22 @@ func TestDataInjection(t *testing.T) {
 	output, err = e2e.execZarfCommand("package", "deploy", path, "--confirm", "-l=trace")
 	require.NoError(t, err, output)
 
-	time.Sleep(5 * time.Second)
-
 	// Get the data injection pod
-	namespace := "demo'"
+	namespace := "demo"
 	pods, err := e2e.getPodsFromNamespace(namespace)
 	require.NoError(t, err)
 	require.Equal(t, len(pods.Items), 1)
 	podname := pods.Items[0].Name
 
 	// Test to confirm the root file was placed
-	// TODO: This retry is disgusting, but race condition...
 	var execStdOut string
-	attempt := 0
-	for attempt < 10 && execStdOut == "" {
-		execStdOut, _, err = e2e.execCommandInPod(podname, namespace, []string{"ls", "/test"})
-		attempt++
-		time.Sleep(2 * time.Second)
-	}
+	execStdOut, _, err = e2e.execCommandInPod(podname, namespace, []string{"ls", "/test"})
 	assert.NoError(t, err)
 	assert.Contains(t, execStdOut, "subdirectory-test")
 
 	// Test to confirm the subdirectory file was placed
 	execStdOut = ""
-	attempt = 0
-	for attempt < 10 && execStdOut == "" {
-		execStdOut, _, err = e2e.execCommandInPod(podname, namespace, []string{"ls", "/test/subdirectory-test"})
-		attempt++
-		time.Sleep(2 * time.Second)
-	}
+	execStdOut, _, err = e2e.execCommandInPod(podname, namespace, []string{"ls", "/test/subdirectory-test"})
 	assert.NoError(t, err)
 	assert.Contains(t, execStdOut, "this-is-an-example-file.txt")
 }
