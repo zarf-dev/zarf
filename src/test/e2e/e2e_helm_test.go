4c9729d06b77c58416d624beaeaf2ff87bf69618
David Smithbauer <david.smithbauer@leidos.com>
2022-05-19 09:32:53 -0400

    Added multiple Helm releaseName example and test
    Signed-off-by: Jon Perry <yrrepnoj@gmail.com>

---
+++ src/test/e2e/e2e_helm_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestE2eExampleHelm(t *testing.T) {
+	//defer e2e.cleanupAfterTest(t)
+
+	//run `zarf init`
+	output, err := e2e.execZarfCommand("init", "--confirm")
+	require.NoError(t, err, output)
+
+	path := fmt.Sprintf("../../../build/zarf-package-test-helm-releasename-%s.tar.zst", e2e.arch)
+
+	// Deploy the charts
+	output, err = e2e.execZarfCommand("package", "deploy", path, "--confirm")
+	require.NoError(t, err, output)
+
+	// Verify the configmap was properly templated
+	kubectlOut, _ := exec.Command("kubectl", "-A", "get", "pods", "--selector=app.kubernetes.io/name=etcd", "--no-headers").Output()
+	assert.Contains(t, string(kubectlOut), "zarf-etcd-0")
+	assert.Contains(t, string(kubectlOut), "zarf-etcd-2-0")
+	assert.Contains(t, string(kubectlOut), "zarf-etcd-3-0")
+	assert.Contains(t, string(kubectlOut), "zarf-etcd-4-0")
+}
