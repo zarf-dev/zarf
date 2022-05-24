ba11db89ddc78f08f81fb0cd6b9c65e8345a5804
David Smithbauer <david.smithbauer@leidos.com>
2022-05-23 21:20:32 -0400

    Fixed test and changed back to Nginx chart
    Signed-off-by: Jon Perry <yrrepnoj@gmail.com>

--- src/test/e2e/e2e_helm_test.go
+++ src/test/e2e/e2e_helm_test.go
@@ -19,13 +19,12 @@ func TestE2eExampleHelm(t *testing.T) {
 	path := fmt.Sprintf("../../../build/zarf-package-test-helm-releasename-%s.tar.zst", e2e.arch)
 
 	// Deploy the charts
-	output, err = e2e.execZarfCommand("package", "deploy", path, "--confirm", "-l=trace")
+	output, err = e2e.execZarfCommand("package", "deploy", path, "--confirm")
 	require.NoError(t, err, output)
 
-	// Verify the configmap was properly templated
-	kubectlOut, _ := exec.Command("kubectl", "get", "pods", "-A", "--selector=app.kubernetes.io/name=etcd", "--no-headers").Output()
-	assert.Contains(t, string(kubectlOut), "zarf-etcd-0")
-	assert.Contains(t, string(kubectlOut), "zarf-etcd-2-0")
-	assert.Contains(t, string(kubectlOut), "zarf-etcd-3-0")
-	assert.Contains(t, string(kubectlOut), "zarf-etcd-4-0")
+	// Verify multiple helm installs of different release names were deployed
+	kubectlOut, _ := exec.Command("kubectl", "get", "pods", "-A", "--selector=app.kubernetes.io/name=nginx", "--no-headers").Output()
+	assert.Contains(t, string(kubectlOut), "zarf-nginx-2")
+	assert.Contains(t, string(kubectlOut), "zarf-nginx-3")
+	assert.Contains(t, string(kubectlOut), "zarf-nginx-4")
 }
