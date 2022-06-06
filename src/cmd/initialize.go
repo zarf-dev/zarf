01ef90229d775b88b9d225bc279cad1fcdfb7332
Jon Perry <yrrepnoj@gmail.com>
2022-06-06 13:01:58 -0400

    add more docs for the 'init' command

--- src/cmd/initialize.go
+++ src/cmd/initialize.go
@@ -18,8 +18,14 @@ import (
 var initCmd = &cobra.Command{
 	Use:     "init",
 	Aliases: []string{"i"},
-	Short:   "Deploys the gitops service or appliance cluster on a clean linux box",
-	Long:    "Flags are only required if running via automation, otherwise the init command will prompt you for your configuration choices",
+	Short:   "Prepares a k8s cluster for the deployment of Zarf packages",
+	Long: "Uses a local 'zarf-init' package found either in the directory you executed " +
+		"the command from or the directory that the Zarf binaries exists. " +
+		"Injects a docker registry as well as other optional useful things (such as a git server " +
+		"and a logging stack) into a k8s cluster under the 'zarf' namespace " +
+		"to support future application deployments. \n" +
+		"If you do not have a k8s cluster already configured, this command will give you " +
+		"the ability to install a cluster locally.\n",
 	Run: func(cmd *cobra.Command, args []string) {
 		zarfLogo := message.GetLogo()
 		_, _ = fmt.Fprintln(os.Stderr, zarfLogo)
@@ -46,8 +52,7 @@ var initCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(initCmd)
 	initCmd.Flags().BoolVar(&config.DeployOptions.Confirm, "confirm", false, "Confirm the install without prompting")
-	initCmd.Flags().StringVar(&config.DeployOptions.Components, "components", "", "Comma-separated list of components to install.  Adding this flag will skip the init prompts for which components to install")
-
+	initCmd.Flags().StringVar(&config.DeployOptions.Components, "components", "", "Comma-separated list of components to instal.")
 	initCmd.Flags().StringVar(&config.DeployOptions.StorageClass, "storage-class", "", "Describe the StorageClass to be used")
 	initCmd.Flags().StringVar(&config.DeployOptions.Secret, "secret", "", "Root secret value that is used to 'seed' other secrets")
 	initCmd.Flags().StringVar(&config.DeployOptions.NodePort, "nodeport", "", "Nodeport to access the Zarf container registry. Between [30000-32767]")
