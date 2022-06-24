982403deff40b0e8f0d25016c2fda1c2814e7eb0
Jeff McCoy <code@jeffm.us>
2022-06-24 13:48:55 -0500

    breaking change: include sanitized hostname in offline registry path

--- src/internal/utils/image.go
+++ src/internal/utils/image.go
@@ -1,12 +1,16 @@
 package utils
 
-import "regexp"
+import (
+	"regexp"
 
-// For further explanation see https://regex101.com/library/PiL191 and https://regex101.com/r/PiL191/1
-var hostParser = regexp.MustCompile(`(?im)^([a-z0-9\-.]+\.[a-z0-9\-]+:?[0-9]*)?/?(.+)$`)
+	"github.com/defenseunicorns/zarf/src/internal/message"
+)
+
+// For further explanation see https://regex101.com/r/DzfGag/1
+var hostParser = regexp.MustCompile(`(?im)^(.*):(.*/.*)`)
 
 // SwapHost Perform base url replacment without the docker libs
-func SwapHost(src string, targetHost string) string {
-	var substitution = targetHost + "/$2"
-	return hostParser.ReplaceAllString(src, substitution)
+func SwapHost(src, targetHost string) string {
+	message.Debugf("images.SwapHost(%s, %s)", src, targetHost)
+	return hostParser.ReplaceAllString(src, targetHost+"/$1.$2")
 }
