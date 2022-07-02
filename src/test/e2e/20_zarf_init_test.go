a3023e5f8277cb5b0bca61ebf6f022102b661208
Minimind <882485+jeff-mccoy@users.noreply.github.com>
2022-07-01 23:10:09 -0500

    Converts E2E tests to user journey tests & adds `--no-progress` flag for cleaner CI logs (#581)
    * initial test migration
    * update helm releasename tests
    * trace log data injection test
    * cleanup test sequence & resource reqs for shared runners
    * install kind directly vs helm action
    * match kind install sequence
    * add k8s resource dump for ci sanity in the future
    * add --no-progress flag
    * fix imports / types
    * add CI for k3s in sudo
    * use message.progress
    * Match user journey from https://zarf.dev/install/
    * try wrapping `uname -m` in sh command
    * correct exec path for install test

---
+++ src/test/e2e/20_zarf_init_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/defenseunicorns/zarf/src/internal/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestZarfInit(t *testing.T) {
+	t.Log("E2E: Zarf init (limit to 10 minutes)")
+	e2e.setup(t)
+	defer e2e.teardown(t)
+	
+	initComponents := "logging,git-server"
+	// Add k3s compoenent in appliance mode
+	if e2e.applianceMode {
+		initComponents = "k3s,logging,git-server"
+	}
+
+	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Minute)
+	defer cancel()
+
+	// run `zarf init`
+	_, _, err := utils.ExecCommandWithContext(ctx, true, e2e.zarfBinPath, "init", "--components="+initComponents, "--confirm")
+	require.NoError(t, err)
+}
