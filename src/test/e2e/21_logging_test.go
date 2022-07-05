124777bb0b85ae5f609475a1584e4e7a539801db
Minimind <882485+jeff-mccoy@users.noreply.github.com>
2022-07-05 12:27:15 -0500

    Increase docker registry and git-server push fault-tolerance (#586)
    * Stop adding zarf registry to local docker config
    * allow dynamic port binding for git/registry
    * fix updates to git-server test & optimize test sequence
    * clean connect table and zarf connect msg output
    * remove test time buffer now that ports are dynamic
    * Cleanup repo/image pushing & add retry logic
    * Only dump tunnel endpoint if auto-open is disabled

--- src/test/e2e/21_logging_test.go
+++ src/test/e2e/21_logging_test.go
@@ -1,9 +1,11 @@
 package test
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/defenseunicorns/zarf/src/internal/k8s"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,17 +14,20 @@ func TestLogginga(t *testing.T) {
 	e2e.setup(t)
 	defer e2e.teardown(t)
 
+	// Get a random local port for this instance
+	localPort, _ := k8s.GetAvailablePort()
+
 	// Establish the port-forward into the logging service
-	err := e2e.execZarfBackgroundCommand("connect", "logging", "--cli-only")
+	err := e2e.execZarfBackgroundCommand("connect", "logging", fmt.Sprintf("--local-port=%d", localPort), "--cli-only")
 	assert.NoError(t, err, "unable to establish tunnel to logging")
 
 	// Make sure Grafana comes up cleanly
-	resp, err := http.Get("http://127.0.0.1:45002/monitor/login")
+	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/monitor/login", localPort))
 	assert.NoError(t, err)
 	assert.Equal(t, 200, resp.StatusCode)
 
 	e2e.chartsToRemove = append(e2e.chartsToRemove, ChartTarget{
 		namespace: "zarf",
 		name:      "zarf-loki-stack",
-	})	
+	})
 }
