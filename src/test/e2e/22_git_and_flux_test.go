bc55913092d980eb88a995ea001cd27d34fcc102
Jeff McCoy <code@jeffm.us>
2022-07-12 16:23:30 -0500

    add loop/timeout for flux test

--- src/test/e2e/22_git_and_flux_test.go
+++ src/test/e2e/22_git_and_flux_test.go
@@ -31,15 +31,38 @@ func TestGitAndFlux(t *testing.T) {
 	localPort, _ := k8s.GetAvailablePort()
 	gitUrl := fmt.Sprintf("http://127.0.0.1:%d", localPort)
 
+	testGitServerConnect(t, localPort, gitUrl)
+	testGitServerReadOnly(t, gitUrl)
+	waitFluxPodInfoDeployment(t)
+
+	e2e.chartsToRemove = append(e2e.chartsToRemove,
+		ChartTarget{
+			namespace: "podinfo",
+			name:      "zarf-raw-podinfo-via-flux",
+		},
+		ChartTarget{
+			namespace: "flux",
+			name:      "zarf-raw-flux-crds",
+		},
+		ChartTarget{
+			namespace: "zarf",
+			name:      "zarf-gitea",
+		},
+	)
+}
+
+func testGitServerConnect(t *testing.T, localPort int, gitUrl string) {
 	// Establish the port-forward into the game service
-	err = e2e.execZarfBackgroundCommand("connect", "git", fmt.Sprintf("--local-port=%d", localPort), "--cli-only")
+	err := e2e.execZarfBackgroundCommand("connect", "git", fmt.Sprintf("--local-port=%d", localPort), "--cli-only")
 	require.NoError(t, err, "unable to connect to the git port-forward")
 
 	// Make sure Gitea comes up cleanly
 	resp, err := http.Get(gitUrl + "/explore/repos")
 	assert.NoError(t, err)
 	assert.Equal(t, 200, resp.StatusCode)
+}
 
+func testGitServerReadOnly(t *testing.T, gitUrl string) {
 	// Init the state variable
 	state := k8s.LoadZarfState()
 	config.InitState(state)
@@ -57,31 +80,40 @@ func TestGitAndFlux(t *testing.T) {
 	assert.False(t, permissionsMap["admin"].(bool))
 	assert.False(t, permissionsMap["push"].(bool))
 	assert.True(t, permissionsMap["pull"].(bool))
+}
 
+func waitFluxPodInfoDeployment(t *testing.T) {
 	// Deploy the flux example and verify that it works
-	path = fmt.Sprintf("build/zarf-package-flux-test-%s.tar.zst", e2e.arch)
-	stdOut, stdErr, err = e2e.execZarfCommand("package", "deploy", path, "--confirm")
+	path := fmt.Sprintf("build/zarf-package-flux-test-%s.tar.zst", e2e.arch)
+	stdOut, stdErr, err := e2e.execZarfCommand("package", "deploy", path, "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 
-	// Temp hack, wait for 30 seconds for flux things
-	time.Sleep(30 * time.Second)
+	var kubectlOut []byte
+	timeout := time.After(3 * time.Minute)
 
-	// Check that flux deployed the podinfo example
-	kubectlOut, _ := exec.Command("kubectl", "wait", "deployment", "-n=podinfo", "podinfo", "--for", "condition=Available=True", "--timeout=60s").Output()
+timer:
+	for {
+		// delay check 3 seconds
+		time.Sleep(2 * time.Second)
+		select {
+
+		// on timeout abort
+		case <-timeout:
+			t.Error("Timeout waiting for flux podinfo deployment")
+
+			// after delay, try running
+		default:
+			// Check that flux deployed the podinfo example
+			kubectlOut, err = exec.Command("kubectl", "wait", "deployment", "-n=podinfo", "podinfo", "--for", "condition=Available=True", "--timeout=3s").Output()
+			// Log error
+			if err != nil {
+				t.Log(string(kubectlOut), err)
+			} else {
+				// Otherwise, break the loop and continue
+				break timer
+			}
+		}
+	}
+
 	assert.Contains(t, string(kubectlOut), "condition met")
-
-	e2e.chartsToRemove = append(e2e.chartsToRemove,
-		ChartTarget{
-			namespace: "podinfo",
-			name:      "zarf-raw-podinfo-via-flux",
-		},
-		ChartTarget{
-			namespace: "flux",
-			name:      "zarf-raw-flux-crds",
-		},
-		ChartTarget{
-			namespace: "zarf",
-			name:      "zarf-gitea",
-		},
-	)
 }
