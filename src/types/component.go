994e38d95ba8361eaf0d554331e0c3d53bfcfc64
Jeff McCoy <code@jeffm.us>
2022-07-26 01:50:59 -0500

    Require container with data injection

--- src/types/component.go
+++ src/types/component.go
@@ -106,10 +106,10 @@ type ZarfComponentScripts struct {
 
 // ZarfContainerTarget defines the destination info for a ZarfData target
 type ZarfContainerTarget struct {
-	Namespace string `yaml:"namespace"`
-	Selector  string `yaml:"selector"`
-	Container string `yaml:"container,omitempty"`
-	Path      string `yaml:"path"`
+	Namespace string `yaml:"namespace" jsonschema:"description=The namespace to targer for data injection"`
+	Selector  string `yaml:"selector" jsonschema:"description=The K8s selector to target for data injection"`
+	Container string `yaml:"container" jsonschema:"description=The container to target for data injection"`
+	Path      string `yaml:"path" jsonschema:"description=The path to copy the data to in the container"`
 }
 
 // ZarfDataInjection is a data-injection definition
