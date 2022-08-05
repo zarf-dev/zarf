b7ba4d6906ab6553c49b0f35fa89572f6184abf0
Megamind <882485+jeff-mccoy@users.noreply.github.com>
2022-08-05 17:13:05 -0500

    Add new "prepare" stage for components scripts to perform actions on "package create" (#660)

--- go.mod
+++ go.mod
@@ -60,16 +60,12 @@ require (
 	github.com/PaesslerAG/gval v1.0.0 // indirect
 	github.com/PaesslerAG/jsonpath v0.1.1 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20220730123233-d6ffb7692adf // indirect
-	github.com/PuerkitoBio/purell v1.1.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/Shopify/logrus-bugsnag v0.0.0-20171204204709-577dee27f20d // indirect
 	github.com/ThalesIgnite/crypto11 v1.2.5 // indirect
 	github.com/acobaugh/osrelease v0.1.0 // indirect
 	github.com/acomagu/bufpipe v1.0.3 // indirect
 	github.com/adrg/xdg v0.4.0 // indirect
 	github.com/anchore/go-macholibre v0.0.0-20220308212642-53e6d0aaf6fb // indirect
-	github.com/anchore/go-rpmdb v0.0.0-20210914181456-a9c52348da63 // indirect
-	github.com/anchore/go-version v1.2.2-0.20200701162849-18adb9c92b9b // indirect
 	github.com/anchore/packageurl-go v0.1.1-0.20220428202044-a072fa3cb6d7 // indirect
 	github.com/anchore/stereoscope v0.0.0-20220708133445-777471f38c5b // indirect
 	github.com/anchore/syft v0.53.0 // indirect
--- src/test/e2e/02_component_scripts_test.go
+++ src/test/e2e/02_component_scripts_test.go
@@ -12,13 +12,46 @@ func TestComponentScripts(t *testing.T) {
 	e2e.setup(t)
 	defer e2e.teardown(t)
 
+	// Note these files will be created in the package directory, not CWD
+	prepareArtifact := "examples/component-scripts/test-prepare.txt"
+	deployArtifacts := []string{
+		"test-deploy-before.txt",
+		"test-deploy-after.txt",
+	}
+	allArtifacts := append(deployArtifacts, prepareArtifact)
+	e2e.cleanFiles(allArtifacts...)
+	defer e2e.cleanFiles(allArtifacts...)
+
+	// Try creating the package to test the create scripts
+	stdOut, stdErr, err := e2e.execZarfCommand("package", "create", "examples/component-scripts/", "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+
+	// Test for package create prepare artirfact
+	require.FileExists(t, prepareArtifact)
+
+	// Test to ensure the deploy scripts are not executed
+	for _, artifact := range deployArtifacts {
+		require.NoFileExists(t, artifact)
+	}
+
+	// Remove the prepare artifact before running package deploy
+	e2e.cleanFiles(prepareArtifact)
+
 	path := fmt.Sprintf("build/zarf-package-component-scripts-%s.tar.zst", e2e.arch)
 
 	// Deploy the simple script that should pass
-	stdOut, stdErr, err := e2e.execZarfCommand("package", "deploy", path, "--confirm", "--components=passes")
+	stdOut, stdErr, err = e2e.execZarfCommand("package", "deploy", path, "--confirm", "--components=prepare,deploy")
 	require.NoError(t, err, stdOut, stdErr)
 
+	// Check that the deploy artifacts were created
+	for _, artifact := range deployArtifacts {
+		require.FileExists(t, artifact)
+	}
+
+	// Check that the prepare artifact were not created
+	require.NoFileExists(t, prepareArtifact)
+
 	// Deploy the simple script that should fail the timeout
-	stdOut, stdErr, err = e2e.execZarfCommand("package", "deploy", path, "--confirm", "--components=does-not-pass")
+	stdOut, stdErr, err = e2e.execZarfCommand("package", "deploy", path, "--confirm", "--components=timeout")
 	require.Error(t, err, stdOut, stdErr)
 }
