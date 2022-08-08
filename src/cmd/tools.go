1cf2d1f67df2acfb5425354507395026972e1842
Jon Perry <yrrepnoj@gmail.com>
2022-08-08 11:23:57 -0400

    rename get-admin-password command to get-git-password
    The get-admin-password was a bad name because it wasn't clear what 'admin' meant. We are now making the name more explicit

--- src/cmd/tools.go
+++ src/cmd/tools.go
@@ -62,8 +62,9 @@ var registryCmd = &cobra.Command{
 }
 
 var readCredsCmd = &cobra.Command{
-	Use:   "get-admin-password",
-	Short: "Returns the Zarf admin password for gitea read from the zarf-state secret in the zarf namespace",
+	Use:   "get-git-password",
+	Short: "Returns the push users password for the Git server",
+	Long:  "Reads the password for a user with push access to the configured Git server from the zarf-state secret in the zarf namespace",
 	Run: func(cmd *cobra.Command, args []string) {
 		state, err := k8s.LoadZarfState()
 		if err != nil {
