850c815543add6f72cde2bf25564b33bc4cde06d
Tim Worcester <tworcester@millennial.software>
2022-08-29 23:18:33 -0400

    Add Or operator to properly handle unique hostParser case
    Added Or operator to handle cases where hostnames have more than one dash.

--- src/internal/utils/image.go
+++ src/internal/utils/image.go
@@ -2,8 +2,8 @@ package utils
 
 import "regexp"
 
-// For further explanation see https://regex101.com/library/PiL191 and https://regex101.com/r/PiL191/1
-var hostParser = regexp.MustCompile(`(?im)^([a-z0-9\-.]+\.[a-z0-9\-]+:?[0-9]*)?/?(.+)$`)
+// For further explanation see https://regex101.com/library/PiL191 and https://regex101.com/r/xVUd5j/1
+var hostParser = regexp.MustCompile(`(?im)^((?:(?:https|http)://)?[\w\-.]+[\.|\-|:][\w\-]+)?/?(.+)$`)
 
 // SwapHost Perform base url replacment without the docker libs
 func SwapHost(src string, targetHost string) string {
