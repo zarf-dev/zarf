78da9123078a9e2e8c9e70c019164e2bf789a196
Megamind <882485+jeff-mccoy@users.noreply.github.com>
2022-09-08 02:08:36 -0500

    Vendor syft/migrate witness -> syft direct (#716)
    * vendor syft/migrate witness -> syft direct
    * Update examples and docs
    * dropping the flake-tastic rhel runner test
    
    Co-authored-by: Wayne Starr <me@racer159.com>

--- src/internal/sbom/catalog.go
+++ src/internal/sbom/catalog.go
@@ -2,27 +2,35 @@ package sbom
 
 import (
 	"embed"
-	"encoding/json"
 	"fmt"
-	"html/template"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
 
+	"github.com/anchore/stereoscope/pkg/image"
+	"github.com/anchore/syft/syft"
+	"github.com/anchore/syft/syft/pkg/cataloger"
+	"github.com/anchore/syft/syft/sbom"
+	"github.com/anchore/syft/syft/source"
 	"github.com/defenseunicorns/zarf/src/config"
 	"github.com/defenseunicorns/zarf/src/internal/message"
 	"github.com/defenseunicorns/zarf/src/internal/utils"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
-	"github.com/testifysec/go-witness/attestation"
-	"github.com/testifysec/go-witness/attestation/syft"
 )
+
+type Builder struct {
+	spinner       *message.Spinner
+	cachePath     string
+	tarPath       string
+	dir           string
+	jsonImageList []byte
+}
 
 //go:embed viewer/*
 var viewerAssets embed.FS
-var tranformRegex = regexp.MustCompile(`(?m)[^a-zA-Z0-9\.\-]`)
+var transformRegex = regexp.MustCompile(`(?m)[^a-zA-Z0-9\.\-]`)
 
 func CatalogImages(tagToImage map[name.Tag]v1.Image, sbomDir, tarPath string) {
 	// Ignore SBOM creation if there the flag is set
@@ -32,127 +40,108 @@ func CatalogImages(tagToImage map[name.Tag]v1.Image, sbomDir, tarPath string) {
 	}
 
 	imageCount := len(tagToImage)
-	spinner := message.NewProgressSpinner("Creating SBOMs for %d images.", imageCount)
-	defer spinner.Stop()
-
-	actx, err := attestation.NewContext([]attestation.Attestor{})
-	if err != nil {
-		spinner.Fatalf(err, "Unable to make attestation context")
+	builder := Builder{
+		spinner:   message.NewProgressSpinner("Creating SBOMs for %d images.", imageCount),
+		cachePath: config.GetImageCachePath(),
+		tarPath:   tarPath,
+		dir:       sbomDir,
 	}
+	defer builder.spinner.Stop()
 
 	// Ensure the sbom directory exists
-	_ = utils.CreateDirectory(sbomDir, 0700)
+	_ = utils.CreateDirectory(builder.dir, 0700)
 
-	cachePath := config.GetImageCachePath()
 	currImage := 1
 
 	// Generate a list of images for the sbom viewer
-	jsonImageList, err := generateImageListJSON(tagToImage)
-	if err != nil {
-		spinner.Fatalf(err, "Unable to generate the SBOM image list")
+	if json, err := builder.generateImageListJSON(tagToImage); err != nil {
+		builder.spinner.Fatalf(err, "Unable to generate the SBOM image list")
+	} else {
+		builder.jsonImageList = json
 	}
 
 	// Generate SBOM for each image
 	for tag := range tagToImage {
-		spinner.Updatef("Creating image SBOMs (%d of %d): %s", currImage, imageCount, tag)
+		builder.spinner.Updatef("Creating image SBOMs (%d of %d): %s", currImage, imageCount, tag)
 
-		// Get the image
-		tarballImg, err := tarball.ImageFromPath(tarPath, &tag)
+		jsonData, err := builder.createImageSBOM(tag)
 		if err != nil {
-			spinner.Fatalf(err, "Unable to open image %s", tag.String())
+			builder.spinner.Fatalf(err, "Unable to create SBOM for image %s", tag)
 		}
 
-		// Create the sbom
-		sbomAttestor := syft.New(syft.WithImageSource(tarballImg, cachePath, tag.String()))
-		if err := sbomAttestor.Attest(actx); err != nil {
-			spinner.Fatalf(err, "Unable to build sbom for image %s", tag.String())
-		}
-
-		// Write the sbom to disk using the image tag as the filename
-		normalized := tranformRegex.ReplaceAllString(tag.String(), "_")
-		sbomFile, err := os.Create(filepath.Join(sbomDir, fmt.Sprintf("%s.json", normalized)))
-		if err != nil {
-			spinner.Fatalf(err, "Unable to create SBOM file for image %s", tag.String())
-		}
-
-		// Create the sbom viewer file for the image
-		sbomViewerFile, err := os.Create(filepath.Join(sbomDir, fmt.Sprintf("sbom-viewer-%s.html", normalized)))
-		if err != nil {
-			spinner.Fatalf(err, "Unable to create SBOM viewer file for image %s", tag.String())
-		}
-
-		defer sbomFile.Close()
-		defer sbomViewerFile.Close()
-
-		// Write the sbom json data to disk
-		enc := json.NewEncoder(sbomFile)
-		if err := enc.Encode(sbomAttestor); err != nil {
-			spinner.Fatalf(err, "Unable to write SBOM file for image %s", tag.String())
-		}
-
-		// Reset the file reader to the start of the file
-		if _, err = sbomFile.Seek(0, 0); err != nil {
-			spinner.Fatalf(err, "Unable to load generated SBOM file for image %s", tag.String())
-		}
-
-		// Read the sbom json data into memory (avoid JSON encoding large structs twice)
-		sbombViewerData, err := ioutil.ReadAll(sbomFile)
-		if err != nil {
-			spinner.Fatalf(err, "Unable to load generated SBOM file for image %s", tag.String())
-		}
-
-		// Create the sbomviewer template data
-		tplData := struct {
-			ThemeCSS  template.CSS
-			ViewerCSS template.CSS
-			ImageList template.JS
-			Data      template.JS
-			LibraryJS template.JS
-			ViewerJS  template.JS
-		}{
-			ThemeCSS:  loadFileCSS("theme.css"),
-			ViewerCSS: loadFileCSS("styles.css"),
-			ImageList: template.JS(jsonImageList),
-			Data:      template.JS(sbombViewerData),
-			LibraryJS: loadFileJS("library.js"),
-			ViewerJS:  loadFileJS("viewer.js"),
-		}
-
-		// Render the sbomviewer template
-		tpl, err := template.ParseFS(viewerAssets, "viewer/template.gohtml")
-		if err != nil {
-			spinner.Fatalf(err, "Unable to parse SBOM Viewer template file")
-		}
-
-		// Write the sbomviewer template to disk
-		if err := tpl.Execute(sbomViewerFile, tplData); err != nil {
-			spinner.Fatalf(err, "Unable to execute SBOM Viewer template file")
+		if err = builder.createSBOMViewerAsset(tag, jsonData); err != nil {
+			builder.spinner.Fatalf(err, "Unable to create SBOM viewer for image %s", tag)
 		}
 
 		currImage++
 	}
 
-	spinner.Success()
+	builder.spinner.Success()
 }
 
-func loadFileCSS(name string) template.CSS {
-	data, _ := viewerAssets.ReadFile("viewer/" + name)
-	return template.CSS(data)
-}
-
-func loadFileJS(name string) template.JS {
-	data, _ := viewerAssets.ReadFile("viewer/" + name)
-	return template.JS(data)
-}
-
-// This could be optimized, but loop over all the images to create an image tag list
-func generateImageListJSON(tagToImage map[name.Tag]v1.Image) ([]byte, error) {
-	var imageList []string
-
-	for tag := range tagToImage {
-		normalized := tranformRegex.ReplaceAllString(tag.String(), "_")
-		imageList = append(imageList, normalized)
+// uses syft to generate SBOM for an image,
+// some code/structure migrated from https://github.com/testifysec/go-witness/blob/v0.1.12/attestation/syft/syft.go
+func (builder *Builder) createImageSBOM(tag name.Tag) ([]byte, error) {
+	// Get the image
+	tarballImg, err := tarball.ImageFromPath(builder.tarPath, &tag)
+	if err != nil {
+		return nil, err
 	}
 
-	return json.Marshal(imageList)
+	// Create the sbom
+	syftImage := image.NewImage(tarballImg, builder.cachePath, image.WithTags(tag.String()))
+	if err := syftImage.Read(); err != nil {
+		return nil, err
+	}
+
+	syftSource, err := source.NewFromImage(syftImage, "")
+	if err != nil {
+		return nil, err
+	}
+
+	catalog, relationships, distro, err := syft.CatalogPackages(&syftSource, cataloger.DefaultConfig())
+	if err != nil {
+		return nil, err
+	}
+
+	artifact := sbom.SBOM{
+		Descriptor: sbom.Descriptor{
+			Name: "zarf",
+		},
+		Source: syftSource.Metadata,
+		Artifacts: sbom.Artifacts{
+			PackageCatalog:    catalog,
+			LinuxDistribution: distro,
+		},
+		Relationships: relationships,
+	}
+
+	jsonData, err := syft.Encode(artifact, syft.FormatByID(syft.JSONFormatID))
+	if err != nil {
+		return nil, err
+	}
+
+	// Write the sbom to disk using the image tag as the filename
+	sbomFile, err := builder.createSBOMFile("%s.json", tag)
+	if err != nil {
+		return nil, err
+	}
+	defer sbomFile.Close()
+
+	if _, err = sbomFile.Write(jsonData); err != nil {
+		return nil, err
+	}
+
+	// Return the json data
+	return jsonData, nil
+}
+
+func (builder *Builder) getNormalizedTag(tag name.Tag) string {
+	return transformRegex.ReplaceAllString(tag.String(), "_")
+}
+
+func (builder *Builder) createSBOMFile(name string, tag name.Tag) (*os.File, error) {
+	file := fmt.Sprintf(name, builder.getNormalizedTag(tag))
+	path := filepath.Join(builder.dir, file)
+	return os.Create(path)
 }
