95f14ef6d9b15c8b7a9aec39138541af7f26fdc7
razzle <harry@razzle.cloud>
2022-09-15 19:06:15 -0500

    🔥 commented code

--- src/ui/routes/+layout.svelte
+++ src/ui/routes/+layout.svelte
@@ -1,31 +1,8 @@
 <script lang="ts">
-	import type { propColor } from '@ui/Button/Button.types';
-	import { Button } from '@ui';
-	import { afterUpdate } from 'svelte';
-	import 'material-symbols';
 	import Header from '$lib/components/header.svelte';
-
-	let path = '';
-
-	// Lifecycle
-	afterUpdate(() => {
-		path = window.location.pathname;
-	});
-
-	// Functions
-	function getVariant(pathname: string, currentPath: string): propColor {
-		return pathname === currentPath ? 'secondary' : 'primary';
-	}
 </script>
 
 <Header />
-
-<!-- <section>
-	<Button href="/" variant="outlined" color={getVariant(path, '/')}>Home</Button>
-	<Button href="/zarf-state" variant="outlined" color={getVariant(path, '/zarf-state')}
-		>View Zarf State</Button
-	>
-</section> -->
 
 <main>
 	<slot />
