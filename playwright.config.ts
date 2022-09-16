c38d62a336cb2b5cca12e809edb59def0498a77b
razzle <harry@razzle.cloud>
2022-09-15 20:36:58 -0500

    disable parallel testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ const config: PlaywrightTestConfig = {
 		timeout: 5000
 	},
 	/* Run tests in files in parallel */
-	fullyParallel: true,
+	fullyParallel: false,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
