c479b9767cbea50f4c28d02f72535ee8bbadeece
Jon Perry <yrrepnoj@gmail.com>
2022-09-16 01:04:59 -0500

    move logic out of cmd package

--- src/cmd/destroy.go
+++ src/cmd/destroy.go
@@ -1,17 +1,7 @@
 package cmd
 
 import (
-	"context"
-	"errors"
-	"os"
-	"regexp"
-
-	"github.com/defenseunicorns/zarf/src/config"
-	"github.com/defenseunicorns/zarf/src/internal/helm"
-	"github.com/defenseunicorns/zarf/src/internal/message"
-	"github.com/defenseunicorns/zarf/src/internal/utils"
-
-	"github.com/defenseunicorns/zarf/src/internal/k8s"
+	"github.com/defenseunicorns/zarf/src/internal/cluster"
 
 	"github.com/spf13/cobra"
 )
@@ -34,49 +24,7 @@ var destroyCmd = &cobra.Command{
 		"the cluster. Since this is a cleanup operation, Zarf will not stop the uninstalls if one of the " +
 		"resources produce an error while being deleted.",
 	Run: func(cmd *cobra.Command, args []string) {
-		// NOTE: If 'zarf init' failed to deploy the k3s component (or if we're looking at the wrong kubeconfig)
-		//       there will be no zarf-state to load and the struct will be empty. In these cases, if we can find
-		//       the scripts to remove k3s, we will still try to remove a locally installed k3s cluster
-		state := k8s.LoadZarfState()
-
-		// If Zarf deployed the cluster, burn it all down
-		if state.ZarfAppliance || (state.Secret == "") {
-			// Check if we have the scripts to destory everything
-			fileInfo, err := os.Stat(config.ZarfCleanupScriptsPath)
-			if errors.Is(err, os.ErrNotExist) || !fileInfo.IsDir() {
-				message.Warnf("Unable to find the folder (%#v) which has the scripts to cleanup the cluster. Do you have the right kube-context?\n", config.ZarfCleanupScriptsPath)
-				return
-			}
-
-			// Run all the scripts!
-			pattern := regexp.MustCompile(`(?mi)zarf-clean-.+\.sh$`)
-			scripts := utils.RecursiveFileList(config.ZarfCleanupScriptsPath, pattern)
-			// Iterate over all matching zarf-clean scripts and exec them
-			for _, script := range scripts {
-				// Run the matched script
-				_, _, err := utils.ExecCommandWithContext(context.TODO(), true, script)
-				if errors.Is(err, os.ErrPermission) {
-					message.Warnf("Got a 'permission denied' when trying to execute the script (%s). Are you the right user and/or do you have the right kube-context?\n", script)
-
-					// Don't remove scripts we can't execute so the user can try to manually run
-					continue
-				} else if err != nil {
-					message.Debugf("Received error when trying to execute the script (%s): %#v", script, err)
-				}
-
-				// Try to remove the script, but ignore any errors
-				_ = os.Remove(script)
-			}
-		} else {
-			// Perform chart uninstallation
-			helm.Destroy(removeComponents)
-
-			// If Zarf didn't deploy the cluster, only delete the ZarfNamespace
-			k8s.DeleteZarfNamespace()
-
-			// Remove zarf agent labels and secrets from namespaces Zarf doesn't manage
-			k8s.StripZarfLabelsAndSecretsFromNamespaces()
-		}
+		cluster.DestroyZarfCluster(removeComponents)
 	},
 }
 
---
+++ src/internal/packager/initialize.go
@@ -0,0 +1,88 @@
+package packager
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/Masterminds/semver/v3"
+	"github.com/defenseunicorns/zarf/src/config"
+	"github.com/defenseunicorns/zarf/src/internal/message"
+	"github.com/defenseunicorns/zarf/src/internal/utils"
+)
+
+func InitializeCluster() {
+	zarfLogo := message.GetLogo()
+	_, _ = fmt.Fprintln(os.Stderr, zarfLogo)
+
+	// Continue running package deploy for all components like any other package
+	initPackageName := fmt.Sprintf("zarf-init-%s.tar.zst", config.GetArch())
+	config.DeployOptions.PackagePath = initPackageName
+
+	// Try to use an init-package in the executable directory if none exist in current working directory
+	if utils.InvalidPath(config.DeployOptions.PackagePath) {
+		executablePath, err := utils.GetFinalExecutablePath()
+		if err != nil {
+			message.Error(err, "Unable to get the directory where the zarf cli is located.")
+		}
+
+		executableDir := path.Dir(executablePath)
+		config.DeployOptions.PackagePath = filepath.Join(executableDir, initPackageName)
+
+		// If the init-package doesn't exist in the executable directory, suggest trying to download
+		if utils.InvalidPath(config.DeployOptions.PackagePath) {
+
+			if config.CommonOptions.Confirm {
+				message.Fatalf(nil, "This command requires a zarf-init package, but one was not found on the local system.")
+			}
+
+			// Parse the CLI version and extract its parts
+			initPackageVersion := strings.TrimLeft(config.CLIVersion, "v")
+			version, err := semver.StrictNewVersion(initPackageVersion)
+
+			if err != nil {
+				// If no CLI version exists (should only occur in dev or CI), try to get the latest release tag from Githhub
+				initPackageVersion, err = utils.GetLatestReleaseTag(config.GithubProject)
+				if err != nil {
+					message.Fatal(err, "No CLI version found and unable to get the latest release tag for the zarf cli.")
+				}
+			} else {
+				// If CLI version exists then get the latest init package for the matching major, minor and patch
+				initPackageVersion = fmt.Sprintf("v%d.%d.%d", version.Major(), version.Minor(), version.Patch())
+			}
+
+			var confirmDownload bool
+			url := fmt.Sprintf("https://github.com/%s/releases/download/%s/%s", config.GithubProject, initPackageVersion, initPackageName)
+
+			// Give the user the choice to download the init-package and note that this does require an internet connection
+			message.Question(fmt.Sprintf("It seems the init package could not be found locally, but can be downloaded from %s", url))
+
+			message.Note("Note: This will require an internet connection.")
+
+			// Prompt the user if --confirm not specified
+			if !confirmDownload {
+				prompt := &survey.Confirm{
+					Message: "Do you want to download this init package?",
+				}
+				if err := survey.AskOne(prompt, &confirmDownload); err != nil {
+					message.Fatalf(nil, "Confirm selection canceled: %s", err.Error())
+				}
+			}
+
+			// If the user wants to download the init-package, download it
+			if confirmDownload {
+				utils.DownloadToFile(url, config.DeployOptions.PackagePath, "")
+			} else {
+				// Otherwise, exit and tell the user to manually download the init-package
+				message.Warn("You must download the init package manually and place it in the current working directory")
+				os.Exit(0)
+			}
+		}
+	}
+
+	// Run everything
+	Deploy()
+}
