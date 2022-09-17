431aa879f6f20eca337c674870c0ae83e0542fc2
Jon Perry <yrrepnoj@gmail.com>
2022-09-16 19:58:42 -0500

    update common writer to take status code argument
    WriteJSONResponse() now takes a statuscode was the API can be more flexible with what codes get returned to the caller

--- src/internal/api/common/common.go
+++ src/internal/api/common/common.go
@@ -7,18 +7,26 @@ import (
 	"github.com/defenseunicorns/zarf/src/internal/message"
 )
 
+// WriteEmpty returns a 204 response with no body.
 func WriteEmpty(w http.ResponseWriter) {
 	message.Debug("api.WriteEmpty()")
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func WriteJSONResponse(w http.ResponseWriter, data any) {
+// WriteJSONResponse returns any data provided as a JSON body to the caller.
+func WriteJSONResponse(w http.ResponseWriter, data any, statusCode int) {
 	message.Debug("api.WriteJSONResponse()")
-	message.JsonValue(data)
-	encoded, err := json.Marshal(data)
-	if err != nil {
-		message.Error(err, "Error marshalling JSON")
-		panic(err)
+
+	var encoded []byte
+	var err error
+	if data == nil {
+		encoded, err = json.Marshal(data)
+		if err != nil {
+			message.Error(err, "Error marshalling JSON")
+			panic(err)
+		}
 	}
+
+	w.WriteHeader(statusCode)
 	w.Write(encoded)
 }
