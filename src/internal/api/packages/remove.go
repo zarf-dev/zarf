4110fd4cc24b654d7a711471fc3e91813647aee6
Jon Perry <yrrepnoj@gmail.com>
2022-09-17 00:56:23 -0500

    Get removeable components from the optional query params

--- src/internal/api/packages/remove.go
+++ src/internal/api/packages/remove.go
@@ -3,6 +3,7 @@ package packages
 import (
 	"net/http"
 
+	"github.com/defenseunicorns/zarf/src/config"
 	"github.com/defenseunicorns/zarf/src/internal/api/common"
 	"github.com/defenseunicorns/zarf/src/internal/message"
 	"github.com/defenseunicorns/zarf/src/internal/packager"
@@ -11,7 +12,14 @@ import (
 
 // RemovePackage removes a package that has been deployed to the cluster.
 func RemovePackage(w http.ResponseWriter, r *http.Request) {
+	// Get the components to remove from the (optional) query params
+	components := r.URL.Query().Get("components")
+	config.DeployOptions.Components = components
+
+	// Get the name of the package we're removing from the URL params
 	name := chi.URLParam(r, "name")
+
+	// Remove the package
 	err := packager.Remove(name)
 	if err != nil {
 		message.ErrorWebf(err, w, "Unable to remove the zarf package from the cluster")
