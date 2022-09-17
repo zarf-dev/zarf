e838659d541a16fada192d9228c0a9bd67fd176c
Jonathan Perry <YrrepNoj@gmail.com>
2022-09-17 14:04:44 -0500

    ugly (but functioning?) initialization via UI (#743)
    Co-authored-by: Megamind <882485+jeff-mccoy@users.noreply.github.com>

--- src/ui/lib/store.ts
+++ src/ui/lib/store.ts
@@ -1,7 +1,8 @@
 import { writable } from 'svelte/store';
 import type { ZarfPackage } from './api-types';
 
+const pkgPath = writable<string[]>();
 const pkgStore = writable<ZarfPackage>();
 const pkgComponentDeployStore = writable<number[]>([]);
 
-export { pkgStore, pkgComponentDeployStore };
+export { pkgStore, pkgComponentDeployStore, pkgPath };
