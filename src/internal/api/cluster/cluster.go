8e319b4e3f33c0b04370cf25674d69f7efec8416
Jon Perry <yrrepnoj@gmail.com>
2022-09-16 23:45:00 -0500

    add API endpoint for initializing the zarf cluster

--- src/internal/api/cluster/cluster.go
+++ src/internal/api/cluster/cluster.go
@@ -1,15 +1,19 @@
 package cluster
 
 import (
+	"encoding/json"
 	"net/http"
 	"time"
 
+	"github.com/defenseunicorns/zarf/src/config"
 	"github.com/defenseunicorns/zarf/src/internal/api/common"
 	"github.com/defenseunicorns/zarf/src/internal/k8s"
 	"github.com/defenseunicorns/zarf/src/internal/message"
+	"github.com/defenseunicorns/zarf/src/internal/packager"
 	"github.com/defenseunicorns/zarf/src/types"
 )
 
+// Summary returns a summary of cluster status.
 func Summary(w http.ResponseWriter, r *http.Request) {
 	message.Debug("cluster.Summary()")
 
@@ -22,7 +26,7 @@ func Summary(w http.ResponseWriter, r *http.Request) {
 	common.WriteJSONResponse(w, data, http.StatusOK)
 }
 
-// Reachable checks if we can connect to the cluster
+// Reachable checks if we can connect to the cluster.
 func Reachable(w http.ResponseWriter, r *http.Request) {
 	message.Debug("cluster.Reachable()")
 	common.WriteJSONResponse(w, reachable(), http.StatusOK)
@@ -52,4 +56,23 @@ func distro() string {
 	} else {
 		return distro
 	}
+}
+
+// InitializeCluster initializes the connected k8s cluster with Zarf
+// This is equivalent to running the `zarf init` command.
+func InitializeCluster(w http.ResponseWriter, r *http.Request) {
+	common.WriteJSONResponse(w, "Not Implemented", http.StatusNotImplemented)
+
+	var initializeClusterRequest = types.ZarfDeployOptions{}
+	err := json.NewDecoder(r.Body).Decode(&initializeClusterRequest)
+	if err != nil {
+		message.Errorf(err, "Unable to decode the request to initialize the cluster")
+		common.WriteJSONResponse(w, nil, http.StatusBadRequest)
+	}
+
+	config.DeployOptions = initializeClusterRequest
+	config.CommonOptions.Confirm = true
+	packager.Deploy()
+
+	common.WriteJSONResponse(w, true, http.StatusCreated)
 }
