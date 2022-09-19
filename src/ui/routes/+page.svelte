c489dcff68eb6ad284da69b737f07bfb479b409d
Jeff McCoy <code@jeffm.us>
2022-09-19 02:40:58 -0500

    update spinner for design changes, make hero component

--- src/ui/routes/+page.svelte
+++ src/ui/routes/+page.svelte
@@ -3,70 +3,47 @@
 
 	import { goto } from '$app/navigation';
 	import Spinner from '$lib/components/spinner.svelte';
-	import bigZarf from '@images/zarf-bubbles.png';
+	import bigZarf from '@images/zarf-bubbles-right.png';
 	import { Button } from '@ui';
+	import Hero from '$lib/components/hero.svelte';
 </script>
 
 {#if $clusterStore}
-	{#if $clusterStore.reachable}
-		{#if $clusterStore.hasZarf}
-			{goto(`/packages`, { replaceState: true })}
-		{:else}
-			<section class="hero">
-				<div class="hero-content">
-					<img src={bigZarf} alt="Zarf logo" id="zarf-logo" width="40%" />
+	{#if $clusterStore.hasZarf}
+		{goto(`/packages`, { replaceState: true })}
+	{:else}
+		<Hero>
+			<img src={bigZarf} alt="Zarf logo" id="zarf-logo" width="40%" />
 
-					<div class="hero-text">
-						<h1 class="hero-title">No Active Clusters</h1>
+			<div class="hero-text">
+				<h1 class="hero-title">No Active Zarf Clusters</h1>
 
-						<h2 class="hero-subtitle">
-							Click initialize cluster to install the Init Package and deploy a new cluster.
-						</h2>
-					</div>
+				{#if $clusterStore.reachable && $clusterStore.distro !== 'unknown'}
+					<h2 class="hero-subtitle">
+						A {$clusterStore.distro} cluster was found, click initialize cluster to initialize it now
+						with Zarf.
+					</h2>
+				{:else}
+					<h2 class="hero-subtitle">
+						Click initialize cluster to install the Init Package and deploy a new cluster.
+					</h2>
+				{/if}
+			</div>
 
-					<Button variant="raised" color="primary" href="/initialize/configure"
-						>Initialize Cluster</Button
-					>
-				</div>
-			</section>
-		{/if}
+			<Button variant="raised" color="primary" href="/initialize/configure"
+				>Initialize Cluster</Button
+			>
+		</Hero>
 	{/if}
 {:else}
-	<Spinner />
+	<Spinner
+		msg="Checking if a Kubernetes cluster is available and initialized by Zarf. This may take a seconds."
+	/>
 {/if}
 
 <style>
-	#zarf-logo {
-		-webkit-transform: scaleX(-1);
-		transform: scaleX(-1);
-	}
-	.hero {
-		position: relative;
-		width: 100vw;
-		height: 100vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.hero-content {
-		position: relative;
-		text-align: center;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		gap: 1rem;
-	}
 	h1,
 	h2 {
 		margin: 0;
-	}
-	.hero-subtitle {
-		font-size: large;
-	}
-	.hero-text {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-		margin: 1rem 0;
 	}
 </style>
