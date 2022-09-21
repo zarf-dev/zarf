30e9441f88432ceed4efc7311791c684ed1b2cf3
Jeff McCoy <code@jeffm.us>
2022-09-21 02:22:10 -0500

    Playwright things...

---
+++ src/test/ui/02_initialize_cluster.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+
+const checkbox = 'input[type=checkbox]';
+
+// todo: make series tests work here to validate the store persistence between views
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('/auth?token=insecure');
+});
+
+test.describe('initialize a zarf cluster', () => {
+	test('configure the init package', async ({ page }) => {
+		await page.goto('/initialize/configure');
+		await expect(page).toHaveTitle('Configure');
+
+		// Stepper
+		await expect(page.locator('.stepper :text("1 Configure") .step-icon')).toHaveClass(/primary/);
+		await expect(page.locator('.stepper :text("2 Review") .step-icon')).toHaveClass(/disabled/);
+		await expect(page.locator('.stepper :text("3 Deploy") .step-icon')).toHaveClass(/disabled/);
+
+		// Package details
+		await expect(page.locator('text=Package Type ZarfInitConfig')).toBeVisible();
+		await expect(
+			page.locator('text=Meta data Name: init Description: Used to establish a new Zarf cluster')
+		).toBeVisible();
+
+		// Components (check most functionaliy with k3s component)
+		let k3s = page.locator('.accordion:has-text("k3s (Optional)")');
+		await expect(k3s.locator(checkbox)).toBeEnabled();
+		await expect(
+			page.locator('.component-accordion-header:has-text("*** REQUIRES ROOT *** Install K3s")')
+		).toBeVisible();
+		await expect(k3s.locator('code')).toBeHidden();
+		await k3s.locator('button').click();
+		await expect(k3s.locator('code')).toBeVisible();
+		await expect(k3s.locator('code:has-text("name: k3s")')).toBeVisible();
+
+		// Check remaining components for deploy states
+		let injector = page.locator('.accordion:has-text("zarf-injector (Required)")');
+		await expect(injector.locator(checkbox)).toBeDisabled();
+		await expect(injector.locator(checkbox)).toBeChecked();
+
+		let seedRegistry = page.locator('.accordion:has-text("zarf-seed-registry (Required)")');
+		await expect(seedRegistry.locator(checkbox)).toBeDisabled();
+		await expect(seedRegistry.locator(checkbox)).toBeChecked();
+
+		let registry = page.locator('.accordion:has-text("zarf-registry (Required)")');
+		await expect(registry.locator(checkbox)).toBeDisabled();
+		await expect(registry.locator(checkbox)).toBeChecked();
+
+		let logging = page.locator('.accordion:has-text("logging (Optional)")');
+		await expect(logging.locator(checkbox)).toBeEnabled();
+
+		let gitServer = page.locator('.accordion:has-text("git-server (Optional)")');
+		await expect(gitServer.locator(checkbox)).toBeEnabled();
+
+		await page.locator('text=review deployment').click();
+		await expect(page).toHaveURL('/initialize/review');
+	});
+	
+	test('review the init package', async ({ page }) => {
+		await page.goto('/initialize/review');
+		await expect(page).toHaveTitle('Review');
+
+		// todo: finish verifying the components are read-only and only include the required ones since we didn't select any optional ones
+	});
+
+	test('perform init package deployment', async ({ page }) => {
+		// todo: finish deploy test
+	});
+});
