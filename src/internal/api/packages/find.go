03d27e57572e75d7c7a794afd253a536c5bf4861
Jeff McCoy <code@jeffm.us>
2022-09-22 00:01:20 -0500

    fix finding init packages in subdirectories with built binaries

--- src/internal/api/packages/find.go
+++ src/internal/api/packages/find.go
@@ -1,50 +1,45 @@
 package packages
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"regexp"
 
+	"github.com/defenseunicorns/zarf/src/config"
 	"github.com/defenseunicorns/zarf/src/internal/api/common"
 	"github.com/defenseunicorns/zarf/src/internal/message"
 	"github.com/defenseunicorns/zarf/src/internal/utils"
 )
 
 var packagePattern = regexp.MustCompile(`zarf-package-.*\.tar`)
-var initPattern = regexp.MustCompile(`zarf-init-.*\.tar\.zst`)
+var initPattern = regexp.MustCompile(fmt.Sprintf("zarf-init-%s.tar.zst", config.GetArch()))
 
+// Find returns all packages anywhere down the directory tree of the working directory.
 func Find(w http.ResponseWriter, r *http.Request) {
 	message.Debug("packages.Find()")
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		message.ErrorWebf(err, w, "Error getting current working directory")
-	}
-
-	files, _ := utils.RecursiveFileList(cwd, packagePattern)
-	common.WriteJSONResponse(w, files, http.StatusOK)
+	findPackage(packagePattern, w, os.Getwd)
 }
 
+// FindInHome returns all packages in the user's home directory.
 func FindInHome(w http.ResponseWriter, r *http.Request) {
 	message.Debug("packages.FindInHome()")
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		message.ErrorWebf(err, w, "Error getting user home directory")
-	}
-
-	files, _ := utils.RecursiveFileList(home, packagePattern)
-	common.WriteJSONResponse(w, files, http.StatusOK)
+	findPackage(packagePattern, w, os.UserHomeDir)
 }
 
 // FindInitPackage returns all init packages anywhere down the directory tree of the working directory.
 func FindInitPackage(w http.ResponseWriter, r *http.Request) {
 	message.Debug("packages.FindInitPackage()")
-	cwd, err := os.Getwd()
+	findPackage(initPattern, w, os.Getwd)
+}
+
+func findPackage(pattern *regexp.Regexp, w http.ResponseWriter, setDir func() (string, error)) {
+	targetDir, err := setDir()
 	if err != nil {
-		message.ErrorWebf(err, w, "Error getting current working directory")
+		message.ErrorWebf(err, w, "Error getting directory")
 	}
 
-	files, _ := utils.RecursiveFileList(cwd, initPattern)
+	// Intentionally ignore errors
+	files, _ := utils.RecursiveFileList(targetDir, pattern)
 	common.WriteJSONResponse(w, files, http.StatusOK)
 }
