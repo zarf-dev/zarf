a79bcded88b8e7ac95c2578c8d2502f76c140179
Jonathan Perry <YrrepNoj@gmail.com>
2022-09-29 17:28:13 -0400

    Feature: Support Using Zarf With an External Registry and Repository (#754)
    ## Description
    This PR introduces the ability to connect to an already existing (and
    reachable) Container Registry and/or Git Repository during the `zarf
    init` command.
    
    Closes #570 (Support using an external git server)
    Closes #560 (Support using an external registry)
    This implementation will serve as a good midway point on having a fully
    HA in-cluster registry #375.
    
    
    
    ## PR Feature List
    - Added several flags to the `init` command to support using an external
    git repository
    - Added several flags to the `init` command to support using an external
    container registry
    - Update `zarf connect registry` to direct to `{HOST}/v2/_catalog` (this
    was confusing some other people since it would originally seem like the
    registry was returning an empty page)
    - Add utility function to create a tunnel to a service URL
    - Created slightly better regexp for replacing the host from a
    `containerImage` url
    - semi-refactored the `zarf package deploy` logic
    
    
    ## Breaking Changes List
    - We are changing the structure of the names of repos & containers we
    are pushing (we are simplifying the name and adding a sha1 hash of the
    original name to the end of the name)
    
    Co-authored-by: Wayne Starr <Racer159@users.noreply.github.com>
    Co-authored-by: Megamind <882485+jeff-mccoy@users.noreply.github.com>

--- src/types/k8s.go
+++ src/types/k8s.go
@@ -6,11 +6,15 @@ type ZarfState struct {
 	Distro        string       `json:"distro" jsonschema:"description=K8s distribution of the cluster Zarf was deployed to"`
 	Architecture  string       `json:"architecture" jsonschema:"description=Machine architecture of the k8s node(s)"`
 	StorageClass  string       `json:"storageClass" jsonschema:"Default StorageClass value Zarf uses for variable templating"`
-	Secret        string       `json:"secret"`
-	NodePort      string       `json:"nodePort"`
 	AgentTLS      GeneratedPKI `json:"agentTLS" jsonschema:"PKI certificate information for the agent pods Zarf manages"`
+
+	GitServer     GitServerInfo `json:"gitServer" jsonschema:"description=Information about the repository Zarf is configured to use"`
+	RegistryInfo  RegistryInfo  `json:"registryInfo" jsonschema:"description=Information about the registry Zarf is configured to use"`
+	LoggingSecret string        `json:"loggingSecret" jsonschema:"description=Secret value that the internal Grafana server was seeded with"`
 }
 
+// DeployedPackage contains information about a Zarf Package that has been deployed to a cluster
+// This object is saved as the data of a k8s secret within the 'zarf' namespace (not as part of the ZarfState secret).
 type DeployedPackage struct {
 	Name       string      `json:"name"`
 	Data       ZarfPackage `json:"data"`
@@ -19,16 +23,43 @@ type DeployedPackage struct {
 	DeployedComponents []DeployedComponent `json:"deployedComponents"`
 }
 
+// DeployedComponent contains information about a Zarf Package Component that has been deployed to a cluster.
 type DeployedComponent struct {
-	Name            string            `json:"name"`
-	InstalledCharts []InstalledCharts `json:"installedCharts"`
+	Name            string           `json:"name"`
+	InstalledCharts []InstalledChart `json:"installedCharts"`
 }
 
-type InstalledCharts struct {
+type InstalledChart struct {
 	Namespace string `json:"namespace"`
 	ChartName string `json:"chartName"`
 }
 
+// GitServerInfo contains information Zarf uses to communicate with a git repository to push/pull repositories to.
+type GitServerInfo struct {
+	PushUsername string `json:"pushUsername" jsonschema:"description=Username of a user with push access to the git repository"`
+	PushPassword string `json:"pushPassword" jsonschema:"description=Password of a user with push access to the git repository"`
+	PullUsername string `json:"pullUsername" jsonschema:"description=Username of a user with pull-only access to the git repository. If not provided for an external repository than the push-user is used"`
+	PullPassword string `json:"pullPassword" jsonschema:"description=Password of a user with pull-only access to the git repository. If not provided for an external repository than the push-user is used"`
+
+	Address        string `json:"address" jsonschema:"description=URL address of the git server"`
+	InternalServer bool   `json:"internalServer" jsonschema:"description=Indicates if we are using a git server that Zarf is directly managing"`
+}
+
+// RegistryInfo contains information Zarf uses to communicate with a container registry to push/pull images.
+type RegistryInfo struct {
+	PushUsername string `json:"pushUsername" jsonschema:"description=Username of a user with push access to the registry"`
+	PushPassword string `json:"pushPassword" jsonschema:"description=Password of a user with push access to the registry"`
+	PullUsername string `json:"pullUsername" jsonschema:"description=Username of a user with pull-only access to the registry. If not provided for an external registry than the push-user is used"`
+	PullPassword string `json:"pullPassword" jsonschema:"description=Password of a user with pull-only access to the registry. If not provided for an external registry than the push-user is used"`
+
+	Address          string `json:"address" jsonschema:"description=URL address of the registry"`
+	NodePort         int    `json:"nodePort" jsonschema:"description=Nodeport of the registry. Only needed if the registry is running inside the kubernetes cluster"`
+	InternalRegistry bool   `json:"internalRegistry" jsonschema:"description=Indicates if we are using a registry that Zarf is directly managing"`
+
+	Secret string `json:"secret" jsonschema:"description=Secret value that the registry was seeded with"`
+}
+
+// GeneratedPKI
 type GeneratedPKI struct {
 	CA   []byte `json:"ca"`
 	Cert []byte `json:"cert"`
