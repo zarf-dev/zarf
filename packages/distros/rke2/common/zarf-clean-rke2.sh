d0a1b38e0b3f0923160f37a149feb2036b4fa8ff
Jason L. van Brackel <jason@vanbrackel.net>
2022-10-03 16:33:53 -0400

    WIP: Experiment to see if RKE2 works

---
+++ packages/distros/rke2/common/zarf-clean-rke2.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+echo -e '\033[0;31m'
+
+for bin in /var/lib/rancher/rke2/data/**/bin/; do
+	[ -d $bin ] && export PATH=$PATH:$bin:$bin/aux
+done
+
+set -x
+
+for service in /etc/systemd/system/rke2*.service; do
+	[ -s $service ] && systemctl stop $(basename $service)
+done
+
+for service in /etc/init.d/rke2*; do
+	[ -x $service ] && $service stop
+done
+
+pschildren() {
+	ps -e -o ppid= -o pid= | \
+	sed -e 's/^\s*//g; s/\s\s*/\t/g;' | \
+	grep -w "^$1" | \
+	cut -f2
+}
+
+pstree() {
+	for pid in $@; do
+		echo $pid
+		for child in $(pschildren $pid); do
+			pstree $child
+		done
+	done
+}
+
+killtree() {
+	kill -9 $(
+		{ set +x; } 2>/dev/null;
+		pstree $@;
+		set -x;
+	) 2>/dev/null
+}
+
+getshims() {
+	ps -e -o pid= -o args= | sed -e 's/^ *//; s/\s\s*/\t/;' | grep -w 'k3s/data/[^/]*/bin/containerd-shim' | cut -f1
+}
+
+killtree $({ set +x; } 2>/dev/null; getshims; set -x)
+
+do_unmount_and_remove() {
+	awk -v path="$1" '$2 ~ ("^" path) { print $2 }' /proc/self/mounts | sort -r | xargs -r -t -n 1 sh -c 'umount "$0" && rm -rf "$0"'
+}
+
+do_unmount_and_remove '/run/rke2'
+do_unmount_and_remove '/var/lib/rancher/rke2'
+do_unmount_and_remove '/var/lib/kubelet/pods'
+do_unmount_and_remove '/var/lib/kubelet/plugins'
+do_unmount_and_remove '/run/netns/cni-'
+
+# Remove CNI namespaces
+ip netns show 2>/dev/null | grep cni- | xargs -r -t -n 1 ip netns delete
+
+# Delete network interface(s) that match 'master cni0'
+ip link show 2>/dev/null | grep 'master cni0' | while read ignore iface ignore; do
+	iface=${iface%%@*}
+	[ -z "$iface" ] || ip link delete $iface
+done
+ip link delete cni0
+ip link delete flannel.1
+rm -rf /var/lib/cni/
+iptables-save | grep -v KUBE- | grep -v CNI- | iptables-restore
+
+if command -v systemctl; then
+	systemctl disable rke2
+	systemctl reset-failed rke2
+	systemctl daemon-reload
+fi
+
+rm -f /etc/systemd/system/rke2.service
+
+for cmd in kubectl crictl ctr; do
+	if [ -L /usr/sbin/$cmd ]; then
+		rm -f /usr/sbin/$cmd
+	fi
+done
+
+rm -rf /etc/rancher/rke2
+rm -rf /run/rke2
+rm -rf /run/flannel
+rm -rf /var/lib/rancher/rke2
+rm -rf /var/lib/kubelet
+rm -f /usr/sbin/rke2
+rm -f /usr/sbin/ctr
+rm -f /usr/sbin/crictl
+rm -f /usr/sbin/kubectl
+rm -f /opt/zarf/rke2-remove.sh
+rm -fr zarf-pki
+
+echo -e '\033[0m'
