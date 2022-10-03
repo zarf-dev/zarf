a3cacc3347d8c1b708ef519140fc785fbf1eb394
Jeff McCoy <code@jeffm.us>
2022-10-03 12:09:44 -0500

    little cleanup

--- src/cmd/internal.go
+++ src/cmd/internal.go
@@ -29,8 +29,6 @@ var agentCmd = &cobra.Command{
 	Long: "NOTE: This command is a hidden command and generally shouldn't be run by a human.\n" +
 		"This command starts up a http webhook that Zarf deployments use to mutate pods to conform " +
 		"with the Zarf container registry and Gitea server URLs.",
-	// this command should not be advertised on the cli as it has no value outside the k8s env
-	Hidden: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		agent.StartWebhook()
 	},
