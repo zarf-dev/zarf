d4576086a75702649772eb64b6a8f93a6472b69b
Wayne Starr <me@racer159.com>
2022-10-05 18:47:51 -0500

    Add a few more files

--- .hooks/print-ui-diff.sh
+++ .hooks/print-ui-diff.sh
@@ -4,6 +4,13 @@
 git diff src/ui
 git diff package.json
 git diff package-lock.json
+git diff .npmrc
+git diff .eslint*
+git diff ts*
+git diff prettier*
+git diff svelte*
+git diff vite*
+git diff playwright*
 
 # Get the current commit, branch and other information
 git show --oneline -s
