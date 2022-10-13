cc90093e80a281fa4bbc39c8482ac4ff394f6531
razzle <harry@razzle.cloud>
2022-10-13 05:37:34 -0500

    initial fixes on differentials between init and standard package deploy flow via UI

--- src/internal/api/packages/deploy.go
+++ src/internal/api/packages/deploy.go
@@ -3,27 +3,63 @@ package packages
 import (
 	"encoding/json"
 	"net/http"
+	"path"
+	"path/filepath"
 
 	"github.com/defenseunicorns/zarf/src/config"
 	"github.com/defenseunicorns/zarf/src/internal/api/common"
 	"github.com/defenseunicorns/zarf/src/internal/message"
 	"github.com/defenseunicorns/zarf/src/internal/packager"
+	"github.com/defenseunicorns/zarf/src/internal/utils"
 	"github.com/defenseunicorns/zarf/src/types"
 )
 
 // DeployPackage deploys a package to the Zarf cluster.
 func DeployPackage(w http.ResponseWriter, r *http.Request) {
-	// Decode the body of the request
-	var deployClusterRequest = types.ZarfDeployOptions{}
-	err := json.NewDecoder(r.Body).Decode(&deployClusterRequest)
-	if err != nil {
-		message.ErrorWebf(err, w, "Unable to decode the request to deploy the cluster")
+	isInitPkg := r.URL.Query().Get("isInitPkg") == "true"
 
-		return
+	if isInitPkg {
+		var body = types.ZarfInitOptions{}
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			message.ErrorWebf(err, w, "Unable to decode the request to deploy the cluster")
+			return
+		}
+		config.InitOptions = body
+		initPackageName := config.GetInitPackageName()
+		config.DeployOptions.PackagePath = initPackageName
+
+		// Try to use an init-package in the executable directory if none exist in current working directory
+		if utils.InvalidPath(config.DeployOptions.PackagePath) {
+			// Get the path to the executable
+			if executablePath, err := utils.GetFinalExecutablePath(); err != nil {
+				message.Errorf(err, "Unable to get the path to the executable")
+			} else {
+				executableDir := path.Dir(executablePath)
+				config.DeployOptions.PackagePath = filepath.Join(executableDir, initPackageName)
+			}
+
+			// If the init-package doesn't exist in the executable directory, try the cache directory
+			if err != nil || utils.InvalidPath(config.DeployOptions.PackagePath) {
+				config.DeployOptions.PackagePath = filepath.Join(config.GetAbsCachePath(), initPackageName)
+
+				// If the init-package doesn't exist in the cache directory, return an error
+				if utils.InvalidPath(config.DeployOptions.PackagePath) {
+					common.WriteJSONResponse(w, false, http.StatusBadRequest)
+					return
+				}
+			}
+		}
+	} else {
+		var body = types.ZarfDeployOptions{}
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			message.ErrorWebf(err, w, "Unable to decode the request to deploy the cluster")
+			return
+		}
+		config.DeployOptions = body
 	}
 
-	// Set the deploy options and deploy!
-	config.DeployOptions = deployClusterRequest
 	config.CommonOptions.Confirm = true
 	packager.Deploy()
 
--- src/ui/routes/initialize/deploy/+page.svelte
+++ src/ui/routes/initialize/deploy/+page.svelte
@@ -11,7 +11,7 @@
 	import { Packages } from '$lib/api';
 	import { Dialog, Stepper, Typography } from '@ui';
 	import bigZarf from '@images/zarf-bubbles-right.png';
-	import type { ZarfDeployOptions } from '$lib/api-types';
+	import type { ZarfDeployOptions, ZarfInitOptions } from '$lib/api-types';
 	import { pkgComponentDeployStore, pkgStore } from '$lib/store';
 	import type { StepProps } from '@defense-unicorns/unicorn-ui/Stepper/Step.svelte';
 
@@ -21,16 +21,46 @@
 		$pkgStore.zarfPackage.components,
 		$pkgComponentDeployStore
 	);
-	console.log(Array.from(components.keys()).join(','));
-	const deployOptions: ZarfDeployOptions = {
-		applianceMode: false,
-		components: Array.from(components.keys()).join(','),
-		nodePort: '',
-		storageClass: '',
-		sGetKeyPath: '',
-		secret: '',
-		packagePath: $pkgStore.path
-	};
+	const requestedComponents: string = $pkgStore.zarfPackage.components
+		.filter((c, idx) => $pkgComponentDeployStore.includes(idx) && !c.required)
+		.map((c) => c.name)
+		.join(',');
+
+	const isInitPkg = $pkgStore.zarfPackage.kind === 'ZarfInitConfig';
+	let options: ZarfDeployOptions | ZarfInitOptions;
+
+	if (isInitPkg) {
+		options = {
+			applianceMode: false,
+			components: requestedComponents,
+			gitServer: {
+				address: '',
+				pushUsername: '',
+				pushPassword: '',
+				pullUsername: '',
+				pullPassword: '',
+				internalServer: true
+			},
+			storageClass: '',
+			registryInfo: {
+				address: '',
+				internalRegistry: true,
+				nodePort: 0,
+				pullPassword: '',
+				pullUsername: '',
+				pushPassword: '',
+				pushUsername: '',
+				secret: ''
+			}
+		};
+	} else {
+		options = {
+			components: requestedComponents,
+			sGetKeyPath: '',
+			packagePath: $pkgStore.path,
+			setVariables: {}
+		};
+	}
 
 	let successful = false;
 	let finishedDeploying = false;
@@ -46,7 +76,7 @@
 	}
 
 	onMount(() => {
-		Packages.deploy(deployOptions).then(
+		Packages.deploy(options, isInitPkg).then(
 			(value: boolean) => {
 				finishedDeploying = true;
 				successful = value;
