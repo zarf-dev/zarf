0eabbbc26d90a485b22c3b5d4b50b2b1876d1e86
razzle <harry@razzle.cloud>
2022-10-27 16:16:41 -0500

    build for aarch64

--- src/injector/stage1/release.sh
+++ src/injector/stage1/release.sh
@@ -9,7 +9,12 @@ cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_im
 cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort \
     --target aarch64-apple-darwin --release
 
+cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort \
+    --target aarch64-unknown-linux-musl --release
+
 size_linux=$(du --si target/x86_64-unknown-linux-musl/release/zarf-injector | cut -f1)
 echo "Linux binary size: $size_linux"
-size_aarch64=$(du --si target/aarch64-apple-darwin/release/zarf-injector | cut -f1)
-echo "Aarch64 binary size: $size_aarch64"
+size_m1=$(du --si target/aarch64-apple-darwin/release/zarf-injector | cut -f1)
+echo "M1 Mac binary size: $size_m1"
+size_aarch64=$(du --si target/aarch64-unknown-linux-musl/release/zarf-injector | cut -f1)
+echo "aarch64 binary size: $size_aarch64"
