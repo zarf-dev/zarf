3ab94aa2b52ca69bed2dd29d02560d34dcf59972
razzle <harry@razzle.cloud>
2022-10-28 01:48:37 -0500

    rework release script to only use stable rust

--- src/injector/stage1/release.sh
+++ src/injector/stage1/release.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-# first follow steps from: https://github.com/johnthagen/min-sized-rust
-# and: https://github.com/johnthagen/min-sized-rust/issues/45
+cargo build --target x86_64-unknown-linux-musl --release
 
-cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort \
-    --target x86_64-unknown-linux-musl --release
-
-cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort \
-    --target aarch64-apple-darwin --release
-
-cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort \
-    --target aarch64-unknown-linux-musl --release
+cargo build --target aarch64-unknown-linux-musl --release
 
 size_linux=$(du --si target/x86_64-unknown-linux-musl/release/zarf-injector | cut -f1)
 echo "Linux binary size: $size_linux"
-size_m1=$(du --si target/aarch64-apple-darwin/release/zarf-injector | cut -f1)
-echo "M1 Mac binary size: $size_m1"
 size_aarch64=$(du --si target/aarch64-unknown-linux-musl/release/zarf-injector | cut -f1)
 echo "aarch64 binary size: $size_aarch64"
