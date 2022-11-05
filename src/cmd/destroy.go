9d0e0d45e8ad2e167ec7df3aa9a2d354839f3d25
Jeff McCoy <code@jeffm.us>
2022-11-04 23:28:56 -0500

    add cmd connect/destroy to language file

--- src/cmd/destroy.go
+++ src/cmd/destroy.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/defenseunicorns/zarf/src/config"
+	"github.com/defenseunicorns/zarf/src/config/lang"
 	"github.com/defenseunicorns/zarf/src/internal/cluster"
 	"github.com/defenseunicorns/zarf/src/internal/packager/helm"
 	"github.com/defenseunicorns/zarf/src/pkg/message"
@@ -24,23 +25,14 @@ var confirmDestroy bool
 var removeComponents bool
 
 var destroyCmd = &cobra.Command{
-	Use:     "destroy",
+	Use:     lang.CmdDestroy,
 	Aliases: []string{"d"},
-	Short:   "Tear it all down, we'll miss you Zarf...",
-	Long: "Tear down Zarf.\n\n" +
-		"Deletes everything in the 'zarf' namespace within your connected k8s cluster.\n\n" +
-		"If Zarf deployed your k8s cluster, this command will also tear your cluster down by " +
-		"searching through /opt/zarf for any scripts that start with 'zarf-clean-' and executing them. " +
-		"Since this is a cleanup operation, Zarf will not stop the teardown if one of the scripts produce " +
-		"an error.\n\n" +
-		"If Zarf did not deploy your k8s cluster, this command will delete the Zarf namespace, delete secrets " +
-		"and labels that only Zarf cares about, and optionally uninstall components that Zarf deployed onto " +
-		"the cluster. Since this is a cleanup operation, Zarf will not stop the uninstalls if one of the " +
-		"resources produce an error while being deleted.",
+	Short:   lang.CmdDestroyShort,
+	Long:    lang.CmdDestroyLong,
 	Run: func(cmd *cobra.Command, args []string) {
 		c, err := cluster.NewClusterWithWait(30 * time.Second)
 		if err != nil {
-			message.Fatalf(err, "Failed to connect to cluster")
+			message.Fatalf(err, lang.ErrNoClusterConnection)
 		}
 
 		// NOTE: If 'zarf init' failed to deploy the k3s component (or if we're looking at the wrong kubeconfig)
@@ -48,7 +40,7 @@ var destroyCmd = &cobra.Command{
 		//       the scripts to remove k3s, we will still try to remove a locally installed k3s cluster
 		state, err := c.LoadZarfState()
 		if err != nil {
-			message.Error(err, "Failed to load Zarf state from cluster")
+			message.Error(err, lang.ErrLoadState)
 		}
 
 		// If Zarf deployed the cluster, burn it all down
@@ -56,8 +48,7 @@ var destroyCmd = &cobra.Command{
 			// Check if we have the scripts to destory everything
 			fileInfo, err := os.Stat(config.ZarfCleanupScriptsPath)
 			if errors.Is(err, os.ErrNotExist) || !fileInfo.IsDir() {
-				message.Warnf("Unable to find the folder (%#v) which has the scripts to cleanup the cluster. Do you have the right kube-context?\n", config.ZarfCleanupScriptsPath)
-				return
+				message.Fatalf(lang.CmdDestroyErrNoScriptPath, config.ZarfCleanupScriptsPath)
 			}
 
 			// Run all the scripts!
@@ -68,7 +59,7 @@ var destroyCmd = &cobra.Command{
 				// Run the matched script
 				_, _, err := utils.ExecCommandWithContext(context.TODO(), true, script)
 				if errors.Is(err, os.ErrPermission) {
-					message.Warnf("Got a 'permission denied' when trying to execute the script (%s). Are you the right user and/or do you have the right kube-context?\n", script)
+					message.Warnf(lang.CmdDestroyErrScriptPermissionDenied, script)
 
 					// Don't remove scripts we can't execute so the user can try to manually run
 					continue
@@ -97,7 +88,7 @@ func init() {
 	rootCmd.AddCommand(destroyCmd)
 
 	// Still going to require a flag for destroy confirm, no viper oopsies here
-	destroyCmd.Flags().BoolVar(&confirmDestroy, "confirm", false, "REQUIRED. Confirm the destroy action to prevent accidental deletions")
-	destroyCmd.Flags().BoolVar(&removeComponents, "remove-components", false, "Also remove any installed components outside the zarf namespace")
-	_ = destroyCmd.MarkFlagRequired("confirm")
+	destroyCmd.Flags().BoolVar(&confirmDestroy, lang.CmdDestroyFlagConfirm, false, lang.CmdDestroyFlagConfirmHelp)
+	destroyCmd.Flags().BoolVar(&removeComponents, lang.CmdDestroyFlagRemoveComponents, false, lang.CmdDestroyFlagRemoveComponentsHelp)
+	_ = destroyCmd.MarkFlagRequired(lang.CmdDestroyFlagConfirm)
 }
