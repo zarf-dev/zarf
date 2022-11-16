524808ef5d2d1bd8c5429f6d67990b2b0eefa428
Jeff McCoy <code@jeffm.us>
2022-11-15 20:40:10 -0600

    test fix

--- src/test/e2e/30_config_file_test.go
+++ src/test/e2e/30_config_file_test.go
@@ -89,7 +89,7 @@ func configFileDefaultTests(t *testing.T) {
 		"create.output_directory: 52d061d5",
 		"Skip generating SBOM for this package (default true)",
 		"[thing1=1a2b3c4d]",
-		"Specify the maximum size of the package in MB, larger packages will be split into multiple files (default 42)",
+		"Specify the maximum size of the package in megabytes, packages larger than this will be split into multiple packages. Use 0 to disable splitting. (default 42)",
 	}
 
 	packageDeployFlags := []string{
