f5d398f9499f2cb816cb0d532941071df394f029
razzle <harry@razzle.cloud>
2022-12-12 13:03:19 -0600

    further update configure page

--- src/ui/routes/initialize/configure/+page.svelte
+++ src/ui/routes/initialize/configure/+page.svelte
@@ -8,13 +8,13 @@
 	import Icon from '$lib/components/icon.svelte';
 	import PackageComponent from '$lib/components/package-component-accordion.svelte';
 	import { pkgStore } from '$lib/store';
-	import { Button, Chip, Dialog, Typography } from '@ui';
+	import { Button, Chip, Typography } from '@ui';
 	import SectionHeader from '$lib/components/pkg/section-header.svelte';
 	let showRaw: boolean = false;
 	let toggleShowRaw = () => (showRaw = !showRaw);
 	import CodeBlock from '$lib/components/code-block.svelte';
 	import { stringify } from 'yaml';
-  	import Drawer from '$lib/components/drawer.svelte';
+	import Drawer from '$lib/components/drawer.svelte';
 </script>
 
 <svelte:head>
@@ -31,7 +31,12 @@
 		<Button on:click={toggleShowRaw} variant="text" color="primary" slot="actions">view yaml</Button
 		>
 	</SectionHeader>
-	<Drawer placement="right" size="fit-content" open={showRaw} on:clickAway={() => showRaw = false}>
+	<Drawer
+		placement="right"
+		size="fit-content"
+		open={showRaw}
+		on:clickAway={() => (showRaw = false)}
+	>
 		<CodeBlock language="yaml">{stringify($pkgStore.zarfPackage)}</CodeBlock>
 	</Drawer>
 	<div class="pkg-details">
@@ -54,21 +59,38 @@
 		<Typography variant="h2" slot="title">Supply Chain</Typography>
 	</SectionHeader>
 	<div style="margin-left: 2rem;">
-		<Typography variant="h3">Build Providence</Typography>
-		<table>
-			<tr>
-				<Typography variant="caption" element="td">User:</Typography>
-				<Typography variant="th" element="td">{$pkgStore.zarfPackage.build?.user}</Typography>
-			</tr>
-			<tr>
-				<Typography variant="caption" element="td">Terminal:</Typography>
-				<Typography variant="th" element="td">{$pkgStore.zarfPackage.build?.terminal}</Typography>
-			</tr>
-			<tr>
-				<Typography variant="caption" element="td">Timestamp:</Typography>
-				<Typography variant="th" element="td">{$pkgStore.zarfPackage.build?.timestamp}</Typography>
-			</tr>
-		</table>
+		<Typography variant="subtitle1">Build Providence</Typography>
+		<div
+			style="display: grid; grid-template-columns: 22% 78%; max-width: 400px; margin: 0.66rem 0;"
+		>
+			<div class="align-center">
+				<Typography variant="caption">User:</Typography>
+			</div>
+			<div class="align-center">
+				<Typography variant="body2">{$pkgStore.zarfPackage.build?.user}</Typography>
+			</div>
+			<div class="align-center"><Typography variant="caption">Terminal:</Typography></div>
+			<div class="align-center">
+				<Typography variant="body2">{$pkgStore.zarfPackage.build?.terminal}</Typography>
+			</div>
+			<div class="align-center"><Typography variant="caption">Timestamp:</Typography></div>
+			<div class="align-center">
+				<Typography variant="body2">{$pkgStore.zarfPackage.build?.timestamp}</Typography>
+			</div>
+			<div class="align-center"><Typography variant="caption">Signed by:</Typography></div>
+			<div class="align-center">
+				<Typography variant="body2" class="todo">{$pkgStore.zarfPackage.build?.user}</Typography>
+			</div>
+		</div>
+
+		<Typography variant="subtitle1">Software Bill of Materials (SBOM)</Typography>
+		<Typography variant="caption" element="div">
+			This package has 7 images with software bill-of-materials (SBOM) included. This button opens
+			the SBOM viewer in your browser.
+		</Typography>
+		<Typography variant="caption">
+			* This directory will removed after package deployment.
+		</Typography>
 	</div>
 </section>
 
@@ -105,5 +127,10 @@
 		display: flex;
 		flex-direction: column;
 		gap: 1rem;
+	}
+
+	.align-center {
+		display: flex;
+		align-items: center;
 	}
 </style>
