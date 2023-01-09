1046c4fe475a891322f785c650936378689a980d
razzle <harry@razzle.cloud>
2023-01-09 17:51:15 -0500

    add empty line between yaml and description

--- src/pkg/packager/components.go
+++ src/pkg/packager/components.go
@@ -193,6 +193,7 @@ func (p *Packager) confirmOptionalComponent(component types.ZarfComponent) (conf
 	displayComponent.Description = ""
 	utils.ColorPrintYAML(displayComponent)
 	if component.Description != "" {
+		pterm.Println()
 		message.Question(component.Description)
 	}
 
