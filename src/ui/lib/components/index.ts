dc7a22d37c5d1c7ad85950cce0e815d2a4a28af7
razzle <harry@razzle.cloud>
2023-01-11 15:44:19 -0500

    cleanup imports

---
+++ src/ui/lib/components/index.ts
@@ -0,0 +1,27 @@
+import AccordionGroup from './accordion-group.svelte';
+import Container from './container.svelte';
+import Divider from './divider.svelte';
+import Header from './header.svelte';
+import Hero from './hero.svelte';
+import Icon from './icon.svelte';
+import Modal from './modal.svelte';
+import PackageComponentAccordion from './package-component-accordion.svelte';
+import PackageDetailsCard from './package-details-card.svelte';
+import Spinner from './spinner.svelte';
+import ThemeToggle from './theme-toggle.svelte';
+import YamlCode from './yaml-code.svelte';
+
+export {
+	AccordionGroup,
+	Container,
+	Divider,
+	Header,
+	Hero,
+	Icon,
+	Modal,
+	PackageComponentAccordion,
+	PackageDetailsCard,
+	Spinner,
+	ThemeToggle,
+	YamlCode
+};
