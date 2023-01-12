e906eed2adfe4a1f013e71b873b3a8d8aa46942b
Wayne Starr <me@racer159.com>
2023-01-12 10:54:52 -0600

    Fix YOLO mode deploys

--- src/pkg/packager/deploy.go
+++ src/pkg/packager/deploy.go
@@ -325,13 +325,19 @@ func (p *Packager) getUpdatedValueTemplate(component types.ZarfComponent) (value
 
 	// Check if the state is empty
 	if state.Distro == "" {
-		// If we are in YOLO mode, return an error
+		// If this is not a YOLO mode package, return an error
 		if !p.cfg.Pkg.Metadata.YOLO {
 			return values, fmt.Errorf("unable to load the Zarf State from the Kubernetes cluster: %w", err)
 		}
 
-		// YOLO mode, so no state needed
+		// YOLO mode, so minimal state needed
 		state.Distro = "YOLO"
+
+		// Try to create the zarf namespace
+		spinner.Updatef("Creating the Zarf namespace")
+		if _, err := p.cluster.Kube.CreateNamespace(cluster.ZarfNamespace, nil); err != nil {
+			spinner.Fatalf(err, "Unable to create the zarf namespace")
+		}
 	}
 
 	if p.cfg.Pkg.Metadata.YOLO && state.Distro != "YOLO" {
