ede3504957ddd04335025a5bef488890c83b9c40
Wayne Starr <Racer159@users.noreply.github.com>
2023-01-17 11:01:09 -0600

    Wrap the crane command to make it easier to use with Zarf (#1205)
    ## Description
    
    This wraps the crane command so that it is easier to use more directly
    with Zarf
    
    ## Related Issue
    
    Fixes #1186 
    
    ## Type of change
    
    - [X] New feature (non-breaking change which adds functionality)
    
    ## Checklist before merging
    
    - [X] Documentation has been updated as necessary (add the `needs-docs`
    label)

--- src/test/e2e/21_connect_test.go
+++ src/test/e2e/21_connect_test.go
@@ -5,10 +5,8 @@
 package test
 
 import (
-	"encoding/base64"
-	"encoding/json"
-	"io"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/defenseunicorns/zarf/src/internal/cluster"
@@ -25,38 +23,13 @@ func TestConnect(t *testing.T) {
 	e2e.setupWithCluster(t)
 	defer e2e.teardown(t)
 
-	// Get the state from the cluster
-	zarfState, err := cluster.NewClusterOrDie().LoadZarfState()
-	require.NoError(t, err)
-
-	// Connect to the Registry
-	tunnelReg, err := cluster.NewZarfTunnel()
-	require.NoError(t, err)
-	tunnelReg.Connect(cluster.ZarfRegistry, false)
-	defer tunnelReg.Close()
-
 	// Make the Registry contains the images we expect
-	reqReg, err := http.NewRequest("GET", tunnelReg.HTTPEndpoint()+"/v2/_catalog", nil)
-	assert.NoError(t, err)
-
-	authReg := zarfState.RegistryInfo.PullUsername + ":" + zarfState.RegistryInfo.PullPassword
-	authRegB64 := base64.StdEncoding.EncodeToString([]byte(authReg))
-	reqReg.Header.Add("Authorization", "Basic "+authRegB64)
-
-	respReg, err := http.DefaultClient.Do(reqReg)
-	assert.NoError(t, err)
-	assert.Equal(t, 200, respReg.StatusCode)
-
-	bodyReg, err := io.ReadAll(respReg.Body)
-	defer respReg.Body.Close()
-	assert.NoError(t, err)
-
-	registries := RegistryResponse{}
-	err = json.Unmarshal(bodyReg, &registries)
-	assert.NoError(t, err)
-	assert.Equal(t, 12, len(registries.Repositories))
-	assert.Contains(t, registries.Repositories, "gitea/gitea")
-	assert.Contains(t, registries.Repositories, "gitea/gitea-3431384023")
+	stdOut, stdErr, err := e2e.execZarfCommand("tools", "registry", "catalog")
+	assert.NoError(t, err, stdOut, stdErr)
+	registryList := strings.Split(strings.Trim(stdOut, "\n "), "\n")
+	assert.Equal(t, 12, len(registryList))
+	assert.Contains(t, stdOut, "gitea/gitea")
+	assert.Contains(t, stdOut, "gitea/gitea-3431384023")
 
 	// Connect to Gitea
 	tunnelGit, err := cluster.NewZarfTunnel()
@@ -80,6 +53,6 @@ func TestConnect(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 200, respLog.StatusCode)
 
-	stdOut, stdErr, err := e2e.execZarfCommand("package", "remove", "init", "--components=logging", "--confirm")
+	stdOut, stdErr, err = e2e.execZarfCommand("package", "remove", "init", "--components=logging", "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 }
