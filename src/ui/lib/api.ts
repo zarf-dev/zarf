a80dffe8c56303045b4186102ebe126b7c83dc7c
razzle <harry@razzle.cloud>
2023-01-18 22:46:49 -0500

    update type on api.ts

--- src/ui/lib/api.ts
+++ src/ui/lib/api.ts
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2021-Present The Zarf Authors
 
 import type {
+	APIZarfDeployPayload,
 	APIZarfPackage,
 	ClusterSummary,
 	DeployedComponent,
@@ -39,7 +40,7 @@ const Packages = {
 	findInit: () => http.get<string[]>('/packages/find-init'),
 	read: (name: string) => http.get<APIZarfPackage>(`/packages/read/${encodeURIComponent(name)}`),
 	getDeployedPackages: () => http.get<DeployedPackage[]>('/packages/list'),
-	deploy: (body: any) => http.put<boolean>(`/packages/deploy`, body),
+	deploy: (options: APIZarfDeployPayload) => http.put<boolean>(`/packages/deploy`, options),
 	remove: (name: string) => http.del(`/packages/remove/${encodeURIComponent(name)}`)
 };
 
