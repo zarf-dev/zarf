ef8a2f63cc68a57ec8e967e8a945c1ad10ad947a
Jeff McCoy <code@jeffm.us>
2023-01-23 11:11:32 -0600

    support TF_VAR on windoze

--- src/pkg/packager/actions.go
+++ src/pkg/packager/actions.go
@@ -130,9 +130,9 @@ func actionCmdMutation(cmd string) (string, error) {
 		// http://web.cs.ucla.edu/~miryung/teaching/EE461L-Spring2012/labs/posix.html for more details.
 		cmd = regexp.MustCompile(`^touch `).ReplaceAllString(cmd, `New-Item `)
 
-		// Convert any ${ZARF_VAR_*} or $ZARF_VAR_* to ${env:ZARF_VAR_*} or $env:ZARF_VAR_* respectively
-		// https://regex101.com/r/BdtUfO/1
-		envVarRegex := regexp.MustCompile(`(?P<envIndicator>\${?(?P<varName>ZARF_VAR_([a-zA-Z0-9_-])+)}?)`)
+		// Convert any ${ZARF_VAR_*} or $ZARF_VAR_* to ${env:ZARF_VAR_*} or $env:ZARF_VAR_* respectively (also TF_VAR_*).
+		// https://regex101.com/r/xk1rkw/1
+		envVarRegex := regexp.MustCompile(`(?P<envIndicator>\${?(?P<varName>(ZARF|TF)_VAR_([a-zA-Z0-9_-])+)}?)`)
 		matches := envVarRegex.FindStringSubmatch(cmd)
 		matchIndex := envVarRegex.SubexpIndex
 		if len(matches) > 0 {
