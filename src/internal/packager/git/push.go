6ad468b6f4cd5869025a8f7f56ace7246fc997a6
Yash Chaudhari <yash.official.990@gmail.com>
2023-01-28 11:19:11 +0530

    Signed-off-by: Yash Chaudhari <yash.official.990@gmail.com>

--- src/internal/packager/git/push.go
+++ src/internal/packager/git/push.go
@@ -32,7 +32,7 @@ func (g *Git) PushRepo(localPath string) error {
 	}
 
 	if err := g.push(repo, spinner); err != nil {
-		spinner.Warnf("unable to push git repo %s retrying...", basename)
+		spinner.Warnf("unable to push the git repo %s retrying...", basename)
 		return err
 	}
 
