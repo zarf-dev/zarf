0097bc3562582183e323a6a42b51df19c37f29c2
Wayne Starr <me@racer159.com>
2023-02-05 10:20:17 -0600

    Add a period to a comment

--- src/internal/agent/proxy/transform.go
+++ src/internal/agent/proxy/transform.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2021-Present The Zarf Authors
 
-// Package proxy provides helper functions for the agent proxy
+// Package proxy provides helper functions for the agent proxy.
 package proxy
 
 import (
