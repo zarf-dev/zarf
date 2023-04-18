5666ae237cb088628d40af0b597e8fe4f38933af
Wayne Starr <Racer159@users.noreply.github.com>
2023-04-18 18:38:25 -0500

    Switch to per-image tunnels, CRC on tags, and progressbar for image pushes (#1590)
    ## Description
    
    This PR creates a tunnel per image push (making it easier to implement
    concurrency - may do that in this PR if we can confirm that issues are
    mitigated) moves the CRC from the image name to the tag and changes the
    UI to use a progressbar instead of a spinner for better user feedback.
    
    ## Related Issue
    
    Relates to #1568 , #1433, #1218, #1364
    
    This also will make #1594 slightly easier.
    
    (See https://github.com/aws/containers-roadmap/issues/853)
    
    Fixes: #1541
    
    ## Type of change
    
    - [X] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [X] Test, docs, adr added or updated as needed
    - [X] [Contributor Guide
    Steps](https://github.com/defenseunicorns/zarf/blob/main/CONTRIBUTING.md#developer-workflow)
    followed

--- src/cmd/package.go
+++ src/cmd/package.go
@@ -89,6 +89,8 @@ var packageDeployCmd = &cobra.Command{
 		pkgClient := packager.NewOrDie(&pkgConfig)
 		defer pkgClient.ClearTempPaths()
 
+		pterm.Println()
+
 		// Deploy the package
 		if err := pkgClient.Deploy(); err != nil {
 			message.Fatalf(err, "Failed to deploy package: %s", err.Error())
