80d4758bf58e3bf8ff3f4ee739a3a7f43817c472
Michael Winberry <mike@defenseunicorns.com>
2023-05-01 16:54:00 -0700

    1570 Fix api tunnels.go package comment

--- src/internal/api/tunnels/tunnels.go
+++ src/internal/api/tunnels/tunnels.go
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2021-Present The Zarf Authors
+
+// Package tunnels contains the handlers for the tunnels API.
 package tunnels
 
 import (
