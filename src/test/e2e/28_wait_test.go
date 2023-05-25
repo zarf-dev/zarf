490c0214569ac1ebbb73a211100a54429dc49b2b
razzle <harry@razzle.cloud>
2023-05-24 23:53:35 -0500

    cleaner 28
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/test/e2e/28_wait_test.go
+++ src/test/e2e/28_wait_test.go
@@ -5,33 +5,21 @@
 package test
 
 import (
-	// "context"
 	"fmt"
-	"os/exec"
-	"time"
 
-	// "os/exec"
 	"testing"
 
-	// "github.com/stretchr/testify/assert"
-	"github.com/defenseunicorns/zarf/src/test"
 	"github.com/stretchr/testify/require"
 )
 
-type zarfCommandResult struct {
-	stdOut string
-	stdErr string
-	err    error
-}
-
-func zarfCommandWStruct(e2e test.ZarfE2ETest, path string) (result zarfCommandResult) {
-	result.stdOut, result.stdErr, result.err = e2e.Zarf("package", "deploy", path, "--confirm")
-	return result
+func cleanupNoWaitNS() {
+	_, _, _ = e2e.Kubectl("delete", "namespace", "no-wait", "--force=true", "--wait=false", "--grace-period=0")
 }
 
 func TestWait(t *testing.T) {
 	t.Log("E2E: Helm Wait")
 	e2e.SetupWithCluster(t)
+	t.Cleanup(cleanupNoWaitNS)
 
 	// Create the package.
 	stdOut, stdErr, err := e2e.Zarf("package", "create", "src/test/packages/28-helm-no-wait/", "-o=build", "--confirm")
@@ -39,26 +27,8 @@ func TestWait(t *testing.T) {
 
 	path := fmt.Sprintf("build/zarf-package-helm-no-wait-%s.tar.zst", e2e.Arch)
 
-	zarfChannel := make(chan zarfCommandResult, 1)
-	go func() {
-		zarfChannel <- zarfCommandWStruct(e2e, path)
-	}()
-
-	select {
-	case res := <-zarfChannel:
-		stdOut = res.stdOut
-		stdErr = res.stdErr
-		err = res.err
-	case <-time.After(30 * time.Second):
-		t.Error("Timeout waiting for zarf deploy (it tried to wait)")
-		t.Log("Removing hanging namespace...")
-		kubectlOut, err := exec.Command("kubectl", "delete", "namespace", "no-wait", "--force=true", "--wait=false", "--grace-period=0").Output()
-		if err != nil {
-			t.Log(kubectlOut)
-		} else {
-			panic(err)
-		}
-	}
+	deployNoWaitArgs := []string{"package", "deploy", path, "--confirm"}
+	stdOut, stdErr, err = e2e.Zarf(deployNoWaitArgs...)
 	require.NoError(t, err, stdOut, stdErr)
 
 	stdOut, stdErr, err = e2e.Zarf("package", "remove", "helm-no-wait", "--confirm")
