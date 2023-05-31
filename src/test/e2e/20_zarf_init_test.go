94e733f85d32dbd54625c672c13e935f1307d552
razzle <harry@razzle.cloud>
2023-05-31 15:31:01 -0500

    changes from review
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/test/e2e/20_zarf_init_test.go
+++ src/test/e2e/20_zarf_init_test.go
@@ -6,8 +6,6 @@ package test
 
 import (
 	"encoding/base64"
-	"fmt"
-	"strings"
 	"testing"
 
 	"encoding/json"
@@ -26,77 +24,43 @@ func TestZarfInit(t *testing.T) {
 		initComponents = "k3s,logging,git-server"
 	}
 
-	t.Run("init mismatched arch", func(t *testing.T) {
-		t.Parallel()
-		// Get the version of the CLI
-		stdOut, stdErr, err := e2e.Zarf("version")
-		require.NoError(t, err, stdOut, stdErr)
-		initPackageVersion := strings.Trim(stdOut, "\n")
+	// run `zarf init`
+	_, initStdErr, err := e2e.Zarf("init", "--components="+initComponents, "--nodeport", "31337", "-l", "trace", "--confirm")
+	require.NoError(t, err)
+	require.Contains(t, initStdErr, "artifacts with software bill-of-materials (SBOM) included")
 
-		var (
-			mismatchedArch        = e2e.GetMismatchedArch()
-			mismatchedInitPackage = fmt.Sprintf("zarf-init-%s-%s.tar.zst", mismatchedArch, initPackageVersion)
-			expectedErrorMessage  = fmt.Sprintf("this package architecture is %s", mismatchedArch)
-		)
-		t.Cleanup(func() {
-			e2e.CleanFiles(mismatchedInitPackage)
-		})
+	logText := e2e.GetLogFileContents(t, initStdErr)
 
-		// Build init package with different arch than the cluster arch.
-		stdOut, stdErr, err = e2e.Zarf("package", "create", ".", "--architecture", mismatchedArch, "--confirm")
-		require.NoError(t, err, stdOut, stdErr)
-		// Check that `zarf init` fails in appliance mode when we try to initialize a k3s cluster
-		// on a machine with a different architecture than the package architecture.
-		// We need to use the --architecture flag here to force zarf to find the package.
-		componentsFlag := ""
-		if e2e.ApplianceMode {
-			componentsFlag = "--components=k3s"
-		}
-		_, stdErr, err = e2e.Zarf("init", "--architecture", mismatchedArch, componentsFlag, "--confirm")
-		require.Error(t, err, stdErr)
-		require.Contains(t, stdErr, expectedErrorMessage)
-	})
+	// Verify that any state secrets were not included in the log
+	base64State, _, err := e2e.Kubectl("get", "secret", "zarf-state", "-n", "zarf", "-o", "jsonpath={.data.state}")
+	require.NoError(t, err)
+	stateJSON, err := base64.StdEncoding.DecodeString(base64State)
+	require.NoError(t, err)
+	state := types.ZarfState{}
+	err = json.Unmarshal(stateJSON, &state)
+	require.NoError(t, err)
+	require.NotContains(t, logText, state.AgentTLS.CA)
+	require.NotContains(t, logText, state.AgentTLS.Cert)
+	require.NotContains(t, logText, state.AgentTLS.Key)
+	require.NotContains(t, logText, state.ArtifactServer.PushToken)
+	require.NotContains(t, logText, state.GitServer.PullPassword)
+	require.NotContains(t, logText, state.GitServer.PushPassword)
+	require.NotContains(t, logText, state.RegistryInfo.PullPassword)
+	require.NotContains(t, logText, state.RegistryInfo.PushPassword)
+	require.NotContains(t, logText, state.RegistryInfo.Secret)
+	require.NotContains(t, logText, state.LoggingSecret)
 
-	t.Run("init with components and verify state secrets are sanitized", func(t *testing.T) {
-		t.Parallel()
-		// run `zarf init`
-		_, initStdErr, err := e2e.Zarf("init", "--components="+initComponents, "--nodeport", "31337", "-l", "trace", "--confirm")
-		require.NoError(t, err)
-		require.Contains(t, initStdErr, "artifacts with software bill-of-materials (SBOM) included")
+	// Check that the registry is running on the correct NodePort
+	stdOut, _, err := e2e.Kubectl("get", "service", "-n", "zarf", "zarf-docker-registry", "-o=jsonpath='{.spec.ports[*].nodePort}'")
+	require.NoError(t, err)
+	require.Contains(t, stdOut, "31337")
 
-		logText := e2e.GetLogFileContents(t, initStdErr)
+	// Check that the registry is running with the correct scale down policy
+	stdOut, _, err = e2e.Kubectl("get", "hpa", "-n", "zarf", "zarf-docker-registry", "-o=jsonpath='{.spec.behavior.scaleDown.selectPolicy}'")
+	require.NoError(t, err)
+	require.Contains(t, stdOut, "Min")
 
-		// Verify that any state secrets were not included in the log
-		base64State, _, err := e2e.Kubectl("get", "secret", "zarf-state", "-n", "zarf", "-o", "jsonpath={.data.state}")
-		require.NoError(t, err)
-		stateJSON, err := base64.StdEncoding.DecodeString(base64State)
-		require.NoError(t, err)
-		state := types.ZarfState{}
-		err = json.Unmarshal(stateJSON, &state)
-		require.NoError(t, err)
-		require.NotContains(t, logText, state.AgentTLS.CA)
-		require.NotContains(t, logText, state.AgentTLS.Cert)
-		require.NotContains(t, logText, state.AgentTLS.Key)
-		require.NotContains(t, logText, state.ArtifactServer.PushToken)
-		require.NotContains(t, logText, state.GitServer.PullPassword)
-		require.NotContains(t, logText, state.GitServer.PushPassword)
-		require.NotContains(t, logText, state.RegistryInfo.PullPassword)
-		require.NotContains(t, logText, state.RegistryInfo.PushPassword)
-		require.NotContains(t, logText, state.RegistryInfo.Secret)
-		require.NotContains(t, logText, state.LoggingSecret)
-
-		// Check that the registry is running on the correct NodePort
-		stdOut, _, err := e2e.Kubectl("get", "service", "-n", "zarf", "zarf-docker-registry", "-o=jsonpath='{.spec.ports[*].nodePort}'")
-		require.NoError(t, err)
-		require.Contains(t, stdOut, "31337")
-
-		// Check that the registry is running with the correct scale down policy
-		stdOut, _, err = e2e.Kubectl("get", "hpa", "-n", "zarf", "zarf-docker-registry", "-o=jsonpath='{.spec.behavior.scaleDown.selectPolicy}'")
-		require.NoError(t, err)
-		require.Contains(t, stdOut, "Min")
-
-		// Special sizing-hacking for reducing resources where Kind + CI eats a lot of free cycles (ignore errors)
-		_, _, _ = e2e.Kubectl("scale", "deploy", "-n", "kube-system", "coredns", "--replicas=1")
-		_, _, _ = e2e.Kubectl("scale", "deploy", "-n", "zarf", "agent-hook", "--replicas=1")
-	})
+	// Special sizing-hacking for reducing resources where Kind + CI eats a lot of free cycles (ignore errors)
+	_, _, _ = e2e.Kubectl("scale", "deploy", "-n", "kube-system", "coredns", "--replicas=1")
+	_, _, _ = e2e.Kubectl("scale", "deploy", "-n", "zarf", "agent-hook", "--replicas=1")
 }
--- src/test/e2e/29_mismatched_architectures_test.go
+++ src/test/e2e/29_mismatched_architectures_test.go
@@ -6,6 +6,7 @@ package test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -17,19 +18,53 @@ func TestMismatchedArchitectures(t *testing.T) {
 	t.Log("E2E: Mismatched architectures")
 	e2e.SetupWithCluster(t)
 
-	var (
-		mismatchedArch         = e2e.GetMismatchedArch()
-		mismatchedGamesPackage = fmt.Sprintf("zarf-package-dos-games-%s.tar.zst", mismatchedArch)
-		expectedErrorMessage   = fmt.Sprintf("this package architecture is %s", mismatchedArch)
-	)
+	t.Run("package deploy mismatched arch", func(t *testing.T) {
+		t.Parallel()
+		var (
+			mismatchedArch         = e2e.GetMismatchedArch()
+			mismatchedGamesPackage = fmt.Sprintf("zarf-package-dos-games-%s.tar.zst", mismatchedArch)
+			expectedErrorMessage   = fmt.Sprintf("this package architecture is %s", mismatchedArch)
+		)
 
-	// Build dos-games package with different arch than the cluster arch.
-	stdOut, stdErr, err := e2e.Zarf("package", "create", "examples/dos-games/", "--architecture", mismatchedArch, "--confirm")
-	require.NoError(t, err, stdOut, stdErr)
-	defer e2e.CleanFiles(mismatchedGamesPackage)
+		// Build dos-games package with different arch than the cluster arch.
+		stdOut, stdErr, err := e2e.Zarf("package", "create", "examples/dos-games/", "--architecture", mismatchedArch, "--confirm")
+		require.NoError(t, err, stdOut, stdErr)
+		defer e2e.CleanFiles(mismatchedGamesPackage)
 
-	// Ensure zarf package deploy returns an error because of the mismatched architectures.
-	_, stdErr, err = e2e.Zarf("package", "deploy", mismatchedGamesPackage, "--confirm")
-	require.Error(t, err, stdErr)
-	require.Contains(t, stdErr, expectedErrorMessage)
+		// Ensure zarf package deploy returns an error because of the mismatched architectures.
+		_, stdErr, err = e2e.Zarf("package", "deploy", mismatchedGamesPackage, "--confirm")
+		require.Error(t, err, stdErr)
+		require.Contains(t, stdErr, expectedErrorMessage)
+	})
+
+	t.Run("init mismatched arch", func(t *testing.T) {
+		t.Parallel()
+		// Get the version of the CLI
+		stdOut, stdErr, err := e2e.Zarf("version")
+		require.NoError(t, err, stdOut, stdErr)
+		initPackageVersion := strings.Trim(stdOut, "\n")
+
+		var (
+			mismatchedArch        = e2e.GetMismatchedArch()
+			mismatchedInitPackage = fmt.Sprintf("zarf-init-%s-%s.tar.zst", mismatchedArch, initPackageVersion)
+			expectedErrorMessage  = fmt.Sprintf("this package architecture is %s", mismatchedArch)
+		)
+		t.Cleanup(func() {
+			e2e.CleanFiles(mismatchedInitPackage)
+		})
+
+		// Build init package with different arch than the cluster arch.
+		stdOut, stdErr, err = e2e.Zarf("package", "create", ".", "--architecture", mismatchedArch, "--confirm")
+		require.NoError(t, err, stdOut, stdErr)
+		// Check that `zarf init` fails in appliance mode when we try to initialize a k3s cluster
+		// on a machine with a different architecture than the package architecture.
+		// We need to use the --architecture flag here to force zarf to find the package.
+		componentsFlag := ""
+		if e2e.ApplianceMode {
+			componentsFlag = "--components=k3s"
+		}
+		_, stdErr, err = e2e.Zarf("init", "--architecture", mismatchedArch, componentsFlag, "--confirm")
+		require.Error(t, err, stdErr)
+		require.Contains(t, stdErr, expectedErrorMessage)
+	})
 }
