d847c547d83e772cf37c92606df5514800d12f7e
razzle <harry@razzle.cloud>
2023-05-31 15:33:57 -0500

    changes from review
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/test/e2e/28_wait_test.go
+++ src/test/e2e/28_wait_test.go
@@ -6,29 +6,49 @@ package test
 
 import (
 	"fmt"
+	"time"
 
 	"testing"
 
+	"github.com/defenseunicorns/zarf/src/test"
 	"github.com/stretchr/testify/require"
 )
 
-func cleanupNoWaitNS() {
-	_, _, _ = e2e.Kubectl("delete", "namespace", "no-wait", "--force=true", "--wait=false", "--grace-period=0")
+type zarfCommandResult struct {
+	stdOut string
+	stdErr string
+	err    error
+}
+
+func zarfCommandWStruct(e2e test.ZarfE2ETest, path string) (result zarfCommandResult) {
+	result.stdOut, result.stdErr, result.err = e2e.Zarf("package", "deploy", path, "--confirm")
+	return result
 }
 
 func TestWait(t *testing.T) {
 	t.Log("E2E: Helm Wait")
-	e2e.SetupWithCluster(t)
-	t.Cleanup(cleanupNoWaitNS)
-
-	// Create the package.
-	stdOut, stdErr, err := e2e.Zarf("package", "create", "src/test/packages/28-helm-no-wait/", "-o=build", "--confirm")
-	require.NoError(t, err, stdOut, stdErr)
 
 	path := fmt.Sprintf("build/zarf-package-helm-no-wait-%s.tar.zst", e2e.Arch)
 
-	deployNoWaitArgs := []string{"package", "deploy", path, "--confirm"}
-	stdOut, stdErr, err = e2e.Zarf(deployNoWaitArgs...)
+	zarfChannel := make(chan zarfCommandResult, 1)
+	go func() {
+		zarfChannel <- zarfCommandWStruct(e2e, path)
+	}()
+
+	var stdOut string
+	var stdErr string
+	var err error
+
+	select {
+	case res := <-zarfChannel:
+		stdOut = res.stdOut
+		stdErr = res.stdErr
+		err = res.err
+	case <-time.After(30 * time.Second):
+		t.Error("Timeout waiting for zarf deploy (it tried to wait)")
+		t.Log("Removing hanging namespace...")
+		_, _, _ = e2e.Kubectl("delete", "namespace", "no-wait", "--force=true", "--wait=false", "--grace-period=0")
+	}
 	require.NoError(t, err, stdOut, stdErr)
 
 	stdOut, stdErr, err = e2e.Zarf("package", "remove", "helm-no-wait", "--confirm")
