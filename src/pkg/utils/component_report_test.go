819d32a58fe543b81f17874657af155c9868e1a8
Case Wylie <cmwylie19@defenseunicorns.com>
2023-06-12 09:33:09 -0400

    [TASK] refactor tests to conform w/ testify require
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/pkg/utils/component_report_test.go
+++ src/pkg/utils/component_report_test.go
@@ -7,161 +7,291 @@ package utils
 import (
 	"testing"
 
+	"github.com/defenseunicorns/zarf/src/pkg/message"
 	"github.com/defenseunicorns/zarf/src/types"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestValidateReportSource(t *testing.T) {
-	// Case 1: Valid VEX file path
-	report := &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "Test Name",
-			Source: "../../test/packages/53-component-reports/vex/test-component.vex-1.json",
-			Type:   "vex",
+// ZarfTestReport tests src/pkg/utils/component_report.go
+type ZarfTestReport struct {
+	success bool       // whether the test should succeed or fail
+	report  ZarfReport // component report to test
+}
+
+// TestReportSuite tests src/pkg/utils/component_report.go
+type TestReportSuite struct {
+	suite.Suite
+	*require.Assertions
+	// test paths
+	validPaths   []ZarfTestReport
+	invalidPaths []ZarfTestReport
+	// test fields
+	validFields   []ZarfTestReport
+	invalidFields []ZarfTestReport
+	// test types
+	validTypes   []ZarfTestReport
+	invalidTypes []ZarfTestReport
+	// test sources
+	validSources   []ZarfTestReport
+	invalidSources []ZarfTestReport
+}
+
+func (suite *TestReportSuite) SetupSuite() {
+	suite.Assertions = require.New(suite.T())
+
+	suite.validPaths = []ZarfTestReport{
+		{
+			success: true,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "valid file",
+					Source: "../../test/packages/53-component-reports/vex/test-component.vex-1.json",
+					Type:   "vex",
+				},
+			},
 		},
-	}
-	err := report.ValidateVexReport("")
-	if err != nil {
-		t.Errorf("Expected ValidateReportSource to return nil, but got error: %v", err)
+		{
+			success: true,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "valid directory",
+					Source: "../../test/packages/53-component-reports/vex/",
+					Type:   "vex",
+				},
+			},
+		},
 	}
 
-	// Case 2: Valid VEX directory path with valid files
-	report = &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "Test Name",
-			Source: "../../test/packages/53-component-reports/vex/",
-			Type:   "vex",
+	suite.invalidPaths = []ZarfTestReport{
+		{
+			success: false,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "invalid file",
+					Source: "/somewhere/over/the/rainbow/file.vex",
+					Type:   "vex",
+				},
+			},
 		},
-	}
-	err = report.ValidateVexReport("")
-	if err != nil {
-		t.Errorf("Expected ValidateReportSource to return nil, but got error: %v", err)
+		{
+			success: false,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "invalid directory",
+					Source: "/somewhere/over/the/rainbow/",
+					Type:   "vex",
+				},
+			},
+		},
 	}
 
-	// Case 3: invalid VEX directory path
-	report = &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "Test Name",
-			Source: "/somewhere/over/the/rainbow/",
-			Type:   "vex",
+	suite.validTypes = []ZarfTestReport{
+		{
+			// we expect ValidateType to throw an error b/c the vexReport is invalid
+			// but it SHOULD validate the type
+			success: false,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name: "should-not-be-ignoreed",
+					Type: "vex",
+				},
+			},
 		},
-	}
-	err = report.ValidateVexReport("")
-	if err == nil {
-		t.Error("Expected ValidateReportSource to return an error, but got nil")
 	}
 
-	// // Case 4: Invalid file path
-	report = &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "Test Name",
-			Source: "invalid/file.vex",
-			Type:   "vex",
+	suite.invalidTypes = []ZarfTestReport{
+		{
+			// we expect no error b/c the vexReport is invalid and therefore ignored
+			success: true,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name: "should-be-ignoreed",
+					Type: "should-ignore",
+				},
+			},
+		},
+		{
+			success: true,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name: "should-be-ignored",
+					Type: "yaml",
+				},
+			},
 		},
 	}
-	err = report.ValidateVexReport("")
-	if err == nil {
-		t.Error("Expected ValidateReportSource to return an error, but got nil")
+
+	suite.validFields = []ZarfTestReport{
+		{
+			success: true,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "valid fields",
+					Source: "../../test/packages/53-component-reports/vex/test-component.vex-1.json",
+					Type:   "vex",
+				},
+			},
+		},
+		{
+			success: true,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "valid fields 2",
+					Source: "just make sure fields are there",
+					Type:   "vex",
+				},
+			},
+		},
+	}
+
+	suite.invalidFields = []ZarfTestReport{
+		{
+			success: false,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name: "missing source and type",
+				},
+			},
+		},
+		{
+			success: false,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "invalid fields",
+					Source: "Missing type",
+				},
+			},
+		},
+	}
+
+	suite.validSources = []ZarfTestReport{
+		{
+			// this is just a wrapper function with a message statement around isURL
+			// expect true if isURL
+			success: true,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "valid source",
+					Source: "https://zarf.dev",
+					Type:   "vex",
+				},
+			},
+		},
+		{
+			success: true,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "valid source 2",
+					Source: "https://defenseunicorns.com",
+					Type:   "vex",
+				},
+			},
+		},
+	}
+
+	suite.invalidSources = []ZarfTestReport{
+		{
+			success: false,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "invalid source",
+					Source: "not-url",
+					Type:   "vex",
+				},
+			},
+		},
+		{
+			success: false,
+			report: ZarfReport{
+				types.ZarfComponentReport{
+					Name:   "invalid source 2",
+					Source: "@defenseunicorns",
+					Type:   "vex",
+				},
+			},
+		},
+	}
+
+}
+
+func (suite *TestReportSuite) Test_0_ValidateReportPath() {
+
+	for _, test := range suite.validPaths {
+		if test.success {
+			suite.NoError(test.report.ValidateVexReport(""))
+		} else {
+			suite.NoError(test.report.ValidateVexReport(""))
+		}
+	}
+
+	for _, test := range suite.invalidPaths {
+		if test.success {
+			suite.Error(test.report.ValidateVexReport(""))
+		} else {
+			suite.Error(test.report.ValidateVexReport(""))
+		}
 	}
 }
 
-func TestValidateType(t *testing.T) {
-	report1 := &ZarfReport{
-		types.ZarfComponentReport{
-			Type: "should-ignore",
-		},
+func (suite *TestReportSuite) Test_1_ValidateType() {
+	// on valid types, we want errors b/c the vex report is invalid
+	for _, test := range suite.validTypes {
+		if test.success {
+			suite.Error(test.report.ValidateType())
+		} else {
+			suite.Error(test.report.ValidateType())
+		}
 	}
-
-	if err := report1.ValidateType(); err != nil {
-		t.Errorf("Expected TestValidateType to return nil, but it returned error %s", err)
-	}
-
-	report2 := &ZarfReport{
-		types.ZarfComponentReport{
-			Type: "vex",
-		},
-	}
-
-	if err := report2.ValidateType(); err == nil {
-		t.Errorf("Expected TestValidateType to return nil, but it returned error %s", err)
+	// invalid types, no errors b/c the vex report is ignored
+	for _, test := range suite.invalidTypes {
+		if test.success {
+			suite.NoError(test.report.ValidateType())
+		} else {
+			suite.NoError(test.report.ValidateType())
+		}
 	}
 }
 
-func TestHasCorrectFields(t *testing.T) {
-	report := &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "Test Name",
-			Source: "Test Source",
-			Type:   "Test Type",
-		},
+func (suite *TestReportSuite) Test_2_HasCorrectFields() {
+	for _, test := range suite.validFields {
+		if test.success {
+			suite.True(test.report.HasCorrectFields())
+		} else {
+			suite.True(test.report.HasCorrectFields())
+		}
 	}
 
-	if !report.HasCorrectFields() {
-		t.Error("Expected HasCorrectFields to return true, but it returned false")
-	}
-}
-
-func TestHasIncorrectFields(t *testing.T) {
-	report := &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "",
-			Source: "Test Source",
-			Type:   "Test Type",
-		},
-	}
-
-	if report.HasCorrectFields() {
-		t.Error("Expected HasCorrectFields to return false, but it returned true")
-	}
-
-	report = &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "Test Name",
-			Source: "",
-			Type:   "Test Type",
-		},
-	}
-
-	if report.HasCorrectFields() {
-		t.Error("Expected HasCorrectFields to return false, but it returned true")
-	}
-
-	report = &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "Test Name",
-			Source: "Test Source",
-			Type:   "",
-		},
-	}
-
-	if report.HasCorrectFields() {
-		t.Error("Expected HasCorrectFields to return false, but it returned true")
+	for _, test := range suite.invalidFields {
+		if test.success {
+			suite.False(test.report.HasCorrectFields())
+		} else {
+			suite.False(test.report.HasCorrectFields())
+		}
 	}
 }
 
-func TestValidateSource(t *testing.T) {
-
-	report := &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "",
-			Source: "not-url",
-			Type:   "Test Type",
-		},
+func (suite *TestReportSuite) Test_3_ValidateSource() {
+	// this is just a wrapper function with a message statement around isURL
+	// expect true if isURL
+	for _, test := range suite.validSources {
+		if test.success {
+			suite.True(test.report.ValidateSource())
+		} else {
+			suite.True(test.report.ValidateSource())
+		}
 	}
 
-	if notURL := report.ValidateSource(); notURL {
-		t.Error("Expected TestValidateSource to return false, but it returned true")
+	for _, test := range suite.invalidSources {
+		if test.success {
+			suite.False(test.report.ValidateSource())
+		} else {
+			suite.False(test.report.ValidateSource())
+		}
 	}
+}
 
-	report = &ZarfReport{
-		types.ZarfComponentReport{
-			Name:   "Test Name",
-			Source: "https://www.defenseunicorns.com",
-			Type:   "Test Type",
-		},
-	}
-
-	if url := report.ValidateSource(); !url {
-		t.Error("Expected TestValidateSource to return true, but it returned false")
-	}
-
+func TestReport(t *testing.T) {
+	message.SetLogLevel(message.DebugLevel)
+	suite.Run(t, new(TestReportSuite))
 }
