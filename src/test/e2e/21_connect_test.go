f6d0aa33b085126c2deaf70a408b08112c66340e
Case Wylie <cmwylie19@defenseunicorns.com>
2023-06-29 19:05:37 -0400

    [TASK] fix tests
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/test/e2e/21_connect_test.go
+++ src/test/e2e/21_connect_test.go
@@ -55,13 +55,14 @@ func TestConnect(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 200, respLog.StatusCode)
 
-	stdOut, stdErr, err = e2e.Zarf("package", "remove", "init", "--components=logging", "--confirm")
-	require.NoError(t, err, stdOut, stdErr)
-
 	// Make sure Prometheus comes up cleanly
 	tunnelProm, err := cluster.NewPrometheusTunnel()
 	require.NoError(t, err)
 	err = tunnelProm.Connect(cluster.ZarfPrometheus, false)
 	require.NoError(t, err)
 	defer tunnelProm.Close()
+
+	stdOut, stdErr, err = e2e.Zarf("package", "remove", "init", "--components=logging", "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+
 }
