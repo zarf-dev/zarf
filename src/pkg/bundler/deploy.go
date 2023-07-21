efb5b400c01ef16d0d2a021e5cd0e99a1051ee34
razzle <harry@razzle.cloud>
2023-07-20 23:30:47 -0500

    scaffold bundler deploy
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/pkg/bundler/deploy.go
+++ src/pkg/bundler/deploy.go
@@ -4,6 +4,17 @@
 // Package bundler contains functions for interacting with, managing and deploying Zarf bundles.
 package bundler
 
+import (
+	"context"
+	"os"
+	"strings"
+
+	"github.com/defenseunicorns/zarf/src/config"
+	"github.com/defenseunicorns/zarf/src/pkg/packager"
+	"github.com/defenseunicorns/zarf/src/pkg/utils"
+	"github.com/defenseunicorns/zarf/src/pkg/utils/helpers"
+)
+
 // Deploy is different based on source
 // depending on if source is a tarball or a OCI ref
 // : if tarball
@@ -16,5 +27,56 @@ package bundler
 // : : pull the package's layers into temp, making it look like packager's temp dir
 // : : use b.p.Deploy() to deploy it
 func (b *Bundler) Deploy() error {
+	ctx := context.TODO()
+
+	// create a new provider
+	provider, err := NewProvider(ctx, b.cfg.DeployOpts.Source, b.tmp)
+	if err != nil {
+		return err
+	}
+
+	// pull the zarf-bundle.yaml + sig
+	loaded, err := provider.LoadBundleMetadata()
+	if err != nil {
+		return err
+	}
+
+	// read the zarf-bundle.yaml into memory
+	if err := b.ReadBundleYaml(loaded[config.ZarfBundleYAML], &b.bundle); err != nil {
+		return err
+	}
+
+	// validate the sig (if present)
+	if err := ValidateBundleSignature(b.tmp); err != nil {
+		return err
+	}
+
+	// TODO: state sharing? variable scoping?
+
+	// deploy each package
+	for _, pkg := range b.bundle.Packages {
+		sha := strings.Split(pkg.Ref, "@sha256:")[1]
+		// TODO: figure out how we want to handle passing --packages to deploy
+		// TODO: add a `name` field to the package struct
+		if len(b.cfg.DeployOpts.Packages) == 0 || helpers.SliceContains(b.cfg.DeployOpts.Packages, sha) {
+			pkgTmp, err := utils.MakeTempDir("") // change this to pkg.Name
+			if err != nil {
+				return err
+			}
+			defer os.RemoveAll(pkgTmp)
+			_, err = provider.LoadPackage(sha, pkgTmp)
+			if err != nil {
+				return err
+			}
+			if err := packager.ValidatePackageSignature(pkgTmp, pkg.PublicKey); err != nil {
+				return err
+			}
+			// TODO: this is where we break packager
+			// check if is init-package
+			// confirm deployment interactively
+			// set variables
+			// deploy all the components
+		}
+	}
 	return nil
 }
