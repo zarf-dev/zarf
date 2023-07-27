04d191c4609d20fa9150fd24f8f5feb06e5f65e5
razzle <harry@razzle.cloud>
2023-07-27 15:58:32 -0500

    fix nil pointer
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/pkg/packager/create.go
+++ src/pkg/packager/create.go
@@ -511,7 +511,7 @@ func (p *Packager) addComponent(index int, component types.ZarfComponent, isSkel
 
 		for _, url := range component.Repos {
 			// Pull all the references if there is no `@` in the string.
-			gitCfg := git.NewWithSpinner(p.cfg.State.GitServer, spinner)
+			gitCfg := git.NewWithSpinner(types.GitServerInfo{}, spinner)
 			if err := gitCfg.Pull(url, componentPath.Repos, false); err != nil {
 				return fmt.Errorf("unable to pull git repo %s: %w", url, err)
 			}
