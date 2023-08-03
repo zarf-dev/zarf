3ae38e5553cf782df932954533a369a67f6ecd9f
Bryan Finster <bryan.finster@gmail.com>
2023-08-03 08:00:10 -0500

    Add missing space.

--- hack/create-zarf-schema.sh
+++ hack/create-zarf-schema.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Create the json schema for the zarf.yaml
-go run main.go internal config-schema >zarf.schema.json
+go run main.go internal config-schema > zarf.schema.json
 
 # Create the json schema for the API and use it to create the typescript definitions
 go run main.go internal api-schema | npx quicktype -s schema -o src/ui/lib/api-types.ts
