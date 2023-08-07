24107fda687e1551bf255f36fddc878bc2b058ea
Case Wylie <cmwylie19@defenseunicorns.com>
2023-08-07 12:24:13 -0400

    Update src/pkg/utils/network/network.go
    Co-authored-by: Lucas Rodriguez <lucas.rodriguez9616@gmail.com>

--- src/pkg/utils/network/network.go
+++ src/pkg/utils/network/network.go
@@ -7,16 +7,14 @@ import (
 )
 
 // DoHostnamesMatch returns a boolean indicating if the hostname of two different URLs are the same.
-func DoHostnamesMatch(logger transform.Log, url1 string, url2 string) (bool, error) {
+func DoHostnamesMatch(url1 string, url2 string) (bool, error) {
 	parsedURL1, err := url.Parse(url1)
 	if err != nil {
-		logger("unable to parse the url (%s)", url1)
-		return false, err
+		return false, fmt.Errorf("unable to parse the url (%s): %w", url1, err)
 	}
 	parsedURL2, err := url.Parse(url2)
 	if err != nil {
-		logger("unable to parse the url (%s)", url2)
-		return false, err
+		return false, fmt.Errorf("unable to parse the url (%s): %w", url2, err)
 	}
 
 	return parsedURL1.Hostname() == parsedURL2.Hostname(), nil
