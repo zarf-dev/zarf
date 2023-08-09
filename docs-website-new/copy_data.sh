55b9ed31f38a1c324b2bf9bf012c9b0e94fd5a50
Bryan Finster <bryan.finster@gmail.com>
2023-08-09 12:24:19 -0500

    Copy arch image

--- docs-website-new/copy_data.sh
+++ docs-website-new/copy_data.sh
@@ -4,5 +4,7 @@ cp ../zarf.schema.json data/zarf_schema.json
 
 
 includePath="static/includes"
+imagePath="assets/img"
 mkdir -p ${includePath}
 cp ../CONTRIBUTING.md ${includePath}/CONTRIBUTING.md
+cp ../docs/.images/architecture.drawio.svg ${includePath}/architecture.drawio.svg
