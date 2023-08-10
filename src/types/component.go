5ece6711f06ce365452719030d385cf90aa61bcf
Case Wylie <cmwylie19@defenseunicorns.com>
2023-08-10 17:27:34 -0400

    Update src/types/component.go
    Co-authored-by: Wayne Starr <Racer159@users.noreply.github.com>

--- src/types/component.go
+++ src/types/component.go
@@ -84,7 +84,7 @@ type ZarfFile struct {
 	Target      string   `json:"target" jsonschema:"description=The absolute or relative path where the file or folder should be copied to during package deploy"`
 	Executable  bool     `json:"executable,omitempty" jsonschema:"description=(files only) Determines if the file should be made executable during package deploy"`
 	Symlinks    []string `json:"symlinks,omitempty" jsonschema:"description=List of symlinks to create during package deploy"`
-	ArchivePath string   `json:"archivePath,omitempty" jsonschema:"description=Local folder or file to be extracted into the package"`
+	ArchivePath string   `json:"archivePath,omitempty" jsonschema:"description=Local folder or file to be extracted from a 'source' archive"`
 }
 
 // ZarfChart defines a helm chart to be deployed.
