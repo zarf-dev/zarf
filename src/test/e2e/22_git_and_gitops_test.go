5b5396c1466a123e5fb83adb2f02ac040a2c3e5b
Danny Gershman <danny@radiusmethod.com>
2023-08-14 16:37:57 -0400

    Adds support to mutate repoURL in Application CRD and Repositories for ArgoCD (#1875)
    ## Description
    
    This adds support to have the Zarf Agent mutate the `repoURL` field
    inside of the `Application` CRD of ArgoCD. This would enable those who
    are utilizing Argo as part of their GitOps deployment strategy to gain
    the same benefits as the current support that the Agent has present for
    Flux.
    
    ## Related Issue
    
    [Slack
    Discussion](https://kubernetes.slack.com/archives/C03B6BJAUJ3/p1682172181361569)
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [ ] Test, docs, adr added or updated as needed
    - [x] [Contributor Guide
    Steps](https://github.com/defenseunicorns/zarf/blob/main/CONTRIBUTING.md#developer-workflow)
    followed
    
    ---------
    
    Co-authored-by: Wayne Starr <Racer159@users.noreply.github.com>

--- src/test/e2e/22_git_and_gitops_test.go
+++ src/test/e2e/22_git_and_gitops_test.go
@@ -5,6 +5,7 @@
 package test
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -17,20 +18,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGitAndFlux(t *testing.T) {
-	t.Log("E2E: Git and flux")
+func TestGit(t *testing.T) {
+	t.Log("E2E: Git")
 	e2e.SetupWithCluster(t)
 	tmpdir := t.TempDir()
 	cachePath := filepath.Join(tmpdir, ".cache-location")
 
-	buildPath := filepath.Join("src", "test", "packages", "22-git-and-flux")
+	buildPath := filepath.Join("src", "test", "packages", "22-git-data")
 	stdOut, stdErr, err := e2e.Zarf("package", "create", buildPath, "--zarf-cache", cachePath, "-o=build", "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 
 	path := fmt.Sprintf("build/zarf-package-git-data-test-%s-1.0.0.tar.zst", e2e.Arch)
 	defer e2e.CleanFiles(path)
 
-	// Deploy the gitops example
+	// Deploy the git data example
 	stdOut, stdErr, err = e2e.Zarf("package", "deploy", path, "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 
@@ -43,7 +44,20 @@ func TestGitAndFlux(t *testing.T) {
 	testGitServerConnect(t, tunnel.HTTPEndpoint())
 	testGitServerReadOnly(t, tunnel.HTTPEndpoint())
 	testGitServerTagAndHash(t, tunnel.HTTPEndpoint())
+}
+
+func TestGitOpsFlux(t *testing.T) {
+	t.Log("E2E: GitOps / Flux")
+	e2e.SetupWithCluster(t)
+
 	waitFluxPodInfoDeployment(t)
+}
+
+func TestGitOpsArgoCD(t *testing.T) {
+	t.Log("E2E: ArgoCD / Flux")
+	e2e.SetupWithCluster(t)
+
+	waitArgoDeployment(t)
 }
 
 func testGitServerConnect(t *testing.T, gitURL string) {
@@ -108,11 +122,46 @@ func waitFluxPodInfoDeployment(t *testing.T) {
 	stdOut, stdErr, err := e2e.Zarf("package", "deploy", path, "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 
+	// Tests the URL mutation for GitRepository CRD for Flux.
+	stdOut, stdErr, err = e2e.Kubectl("get", "gitrepositories", "podinfo", "-n", "flux-system", "-o", "jsonpath={.spec.url}")
+	expectedMutatedRepoURL := fmt.Sprintf("%s/%s/podinfo-1646971829.git", config.ZarfInClusterGitServiceURL, config.ZarfGitPushUser)
+	require.Equal(t, expectedMutatedRepoURL, stdOut)
+
 	// Remove the flux example when deployment completes
 	stdOut, stdErr, err = e2e.Zarf("package", "remove", "podinfo-flux", "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 
 	// Prune the flux images to reduce disk pressure
+	stdOut, stdErr, err = e2e.Zarf("tools", "registry", "prune", "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+}
+
+func waitArgoDeployment(t *testing.T) {
+	// Deploy the argocd example and verify that it works
+	path := fmt.Sprintf("build/zarf-package-argocd-%s.tar.zst", e2e.Arch)
+	stdOut, stdErr, err := e2e.Zarf("package", "deploy", path, "--components=argocd-apps", "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+
+	expectedMutatedRepoURL := fmt.Sprintf("%s/%s/podinfo-1646971829.git", config.ZarfInClusterGitServiceURL, config.ZarfGitPushUser)
+
+	// Tests the mutation of the private repository Secret for ArgoCD.
+	stdOut, stdErr, err = e2e.Kubectl("get", "secret", "argocd-repo-github-podinfo", "-n", "argocd", "-o", "jsonpath={.data.url}")
+	require.NoError(t, err, stdOut, stdErr)
+
+	expectedMutatedPrivateRepoURLSecret, err := base64.StdEncoding.DecodeString(stdOut)
+	require.NoError(t, err, stdOut, stdErr)
+	require.Equal(t, expectedMutatedRepoURL, string(expectedMutatedPrivateRepoURLSecret))
+
+	// Tests the mutation of the repoURL for Application CRD source(s) for ArgoCD.
+	stdOut, stdErr, err = e2e.Kubectl("get", "application", "apps", "-n", "argocd", "-o", "jsonpath={.spec.sources[0].repoURL}")
+	require.NoError(t, err, stdOut, stdErr)
+	require.Equal(t, expectedMutatedRepoURL, stdOut)
+
+	// Remove the argocd example when deployment completes
+	stdOut, stdErr, err = e2e.Zarf("package", "remove", "argocd", "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+
+	// Prune the ArgoCD images to reduce disk pressure
 	stdOut, stdErr, err = e2e.Zarf("tools", "registry", "prune", "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 }
