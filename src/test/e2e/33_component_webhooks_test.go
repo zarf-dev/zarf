9e3300ad272819095da3aed3c220c295af5356ca
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-08-17 15:16:49 -0500

    Add E2E test for component webhooks

---
+++ src/test/e2e/33_component_webhooks_test.go
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2021-Present The Zarf Authors
+
+// Package test provides e2e tests for Zarf.
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestComponentWebhooks(t *testing.T) {
+	t.Log("E2E: Component Webhooks")
+	e2e.SetupWithCluster(t)
+
+	// Deploy example pepr capability
+	webhookPath := fmt.Sprintf("build/zarf-package-component-webhooks-%s-0.0.1.tar.zst", e2e.Arch)
+	stdOut, stdErr, err := e2e.Zarf("package", "deploy", webhookPath, "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+
+	// Deploy dos-games package and ensure it waits for the Pepr webhook to complete.
+	gamesPath := fmt.Sprintf("build/zarf-package-dos-games-%s-1.0.0.tar.zst", e2e.Arch)
+	stdOut, stdErr, err = e2e.Zarf("package", "deploy", gamesPath, "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+	require.Contains(t, stdErr, "Waiting for component webhooks to complete")
+}
