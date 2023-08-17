59e1e9970b13ebaaa60335ff299c784b6ffdea1d
Bryan Finster <bryan.finster@gmail.com>
2023-08-17 09:53:05 -0500

    Clean up setup script

--- docs-website-new/setup-docs.sh
+++ docs-website-new/setup-docs.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+includePath="static/includes"
+mkdir -p ${includePath}
+cp ../CONTRIBUTING.md ${includePath}/CONTRIBUTING.md
+cp ../.images/architecture.drawio.svg ${includePath}/architecture.drawio.svg
 cp ../zarf.schema.json data/zarf_schema.json
 
 
-includePath="static/includes"
-imagePath="assets/img"
-mkdir -p ${includePath}
-cp ../CONTRIBUTING.md ${includePath}/CONTRIBUTING.md
-cp ../docs/.images/architecture.drawio.svg ${includePath}/architecture.drawio.svg
 
+# This converts the docs from the current format to work with Hugo. After conversion, the docs gene4ration scripts
+# should be modified to create the new format.
 hugodocs
 
-## One time conversion
-mkdir -p ../docs-hugo/docs
-cp -r ../docs/examples ../docs-hugo/docs
-mv ../docs ../docs-old
-mv ../docs-hugo/docs ..
+
+rm -rf ../docs-old && \
+mkdir -p ../docs-hugo/docs && \
+cp -R ../docs/examples ../docs-hugo/docs && \
+mv ../docs ../docs-old && \
+mv ../docs-hugo/docs .. && \
 rm -rf ../docs-hugo
