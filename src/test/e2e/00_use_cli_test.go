9f9b71ef90cf0305f6c83cfe8393c19886074a4f
Case Wylie <cmwylie19@defenseunicorns.com>
2023-08-24 19:00:10 -0400

    Update src/test/e2e/00_use_cli_test.go
    Commit CLI test suggestion
    
    Co-authored-by: Wayne Starr <Racer159@users.noreply.github.com>

--- src/test/e2e/00_use_cli_test.go
+++ src/test/e2e/00_use_cli_test.go
@@ -124,6 +124,12 @@ func TestUseCLI(t *testing.T) {
 		path := "zarf-package-distro-eks-multi-0.0.2.tar.zst"
 		stdOut, stdErr, err = e2e.Zarf("package", "deploy", path, "--confirm")
 		require.NoError(t, err, stdOut, stdErr)
+
+		require.FileExists(t, "binaries/eksctl_Darwin_x86_64")
+		require.FileExists(t, "binaries/eksctl_Darwin_arm64")
+		require.FileExists(t, "binaries/eksctl_Linux_x86_64")
+
+		e2e.CleanFiles("binaries/eksctl_Darwin_x86_64", "binaries/eksctl_Darwin_arm64", "binaries/eksctl_Linux_x86_64")
 	})
 
 	t.Run("zarf package create with tmpdir and cache", func(t *testing.T) {
