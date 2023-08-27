a94c2617e0cb91161bac847a7357b8222eb367d9
Case Wylie <cmwylie19@defenseunicorns.com>
2023-08-27 08:56:49 -0400

    cover new logic in unit test
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/pkg/transform/image.go
+++ src/pkg/transform/image.go
@@ -6,6 +6,7 @@ package transform
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/defenseunicorns/zarf/src/pkg/utils/helpers"
 	"github.com/distribution/distribution/reference"
@@ -30,7 +31,7 @@ func ImageTransformHost(targetHost, srcReference string) (string, error) {
 	}
 
 	// check if image has already been transformed
-	if targetHost == image.Host {
+	if targetHost == image.Host || strings.HasPrefix(targetHost, image.Host) {
 		return srcReference, nil
 	}
 
@@ -51,6 +52,12 @@ func ImageTransformHostWithoutChecksum(targetHost, srcReference string) (string,
 	if err != nil {
 		return "", err
 	}
+
+	// check if image has already been transformed
+	if targetHost == image.Host || strings.HasPrefix(targetHost, image.Host) {
+		return srcReference, nil
+	}
+
 	return fmt.Sprintf("%s/%s%s", targetHost, image.Path, image.TagOrDigest), nil
 }
 
