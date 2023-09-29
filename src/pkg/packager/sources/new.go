a1a1c6b3a9512714e069e049fac373b8d7eaa249
razzle <harry@razzle.cloud>
2023-09-29 11:08:27 -0500

    comments
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/pkg/packager/sources/new.go
+++ src/pkg/packager/sources/new.go
@@ -22,21 +22,15 @@ import (
 // While this interface defines three functions, LoadPackage, LoadPackageMetadata, and Collect; only one of them should be used within a packager function.
 //
 // These functions currently do not promise repeatability due to the side effect nature of loading a package.
+//
+// Signature and integrity validation is up to the implementation of the package source.
+//
+//	`sources.ValidatePackageSignature` and `sources.ValidatePackageIntegrity` can be leveraged for this purpose.
 type PackageSource interface {
 	// LoadPackage loads a package from a source.
-	//
-	// For the default sources included in Zarf, package integrity (checksums, signatures, etc.) is validated during this function
-	// and expects the package structure to follow the default Zarf package structure.
-	//
-	// If your package does not follow the default Zarf package structure, you will need to implement your own source.
-	LoadPackage(*layout.PackagePaths) error
+	LoadPackage(dst *layout.PackagePaths) error
+
 	// LoadPackageMetadata loads a package's metadata from a source.
-	//
-	// This function follows the same principles as LoadPackage, with a few exceptions:
-	//
-	// - Package integrity validation will display a warning instead of returning an error if
-	//   the package is signed but no public key is provided. This is to allow for the inspection and removal of packages
-	//   that are signed but the user does not have the public key for.
 	LoadPackageMetadata(dst *layout.PackagePaths, wantSBOM bool, skipValidation bool) error
 
 	// Collect relocates a package from its source to a tarball in a given destination directory.
