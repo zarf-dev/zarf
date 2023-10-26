64dbfe2a91ae711a15f082c68689e26820229984
Wayne Starr <me@racer159.com>
2023-10-26 10:48:43 -0600

    lint

--- src/pkg/utils/helpers/misc.go
+++ src/pkg/utils/helpers/misc.go
@@ -194,6 +194,7 @@ func StringToSlice(s string) []string {
 	return []string{}
 }
 
+// AppendIfNotExists appends a string to a slice of strings if it is not present already on the slice.
 func AppendIfNotExists(slice []string, item string) []string {
 	for _, s := range slice {
 		if s == item {
