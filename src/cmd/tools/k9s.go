5c8ddfd637a43102ce2c1f5844247ac4fcbaec00
Wayne Starr <me@racer159.com>
2023-11-07 16:24:14 -0700

    Improve the docs for k9s flag options

--- src/cmd/tools/k9s.go
+++ src/cmd/tools/k9s.go
@@ -10,22 +10,26 @@ import (
 	"github.com/defenseunicorns/zarf/src/config/lang"
 	k9s "github.com/derailed/k9s/cmd"
 	"github.com/spf13/cobra"
+
+	_ "unsafe"
 )
+
+//go:linkname k9sRootCmd github.com/derailed/k9s/cmd.rootCmd
+var k9sRootCmd *cobra.Command
 
 func init() {
 	k9sCmd := &cobra.Command{
-		Use:                "monitor",
-		Aliases:            []string{"m", "k9s"},
-		Short:              lang.CmdToolsMonitorShort,
-		DisableFlagParsing: true,
+		Use:     "monitor",
+		Aliases: []string{"m", "k9s"},
+		Short:   lang.CmdToolsMonitorShort,
 		Run: func(cmd *cobra.Command, args []string) {
 			// Hack to make k9s think it's all alone
 			os.Args = []string{os.Args[0]}
-			// Pass the rest of the arguments to k9s
-			os.Args = append(os.Args, args...)
 			k9s.Execute()
 		},
 	}
+
+	k9sCmd.Flags().AddFlagSet(k9sRootCmd.Flags())
 
 	toolsCmd.AddCommand(k9sCmd)
 }
