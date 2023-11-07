fa03f3529c6ca6faad8f3b35bf3ff8549c11999e
Austin Abro <AustinAbro321@gmail.com>
2023-11-07 13:48:13 +0000

    refactoring

--- src/pkg/lint/validate_schema.go
+++ src/pkg/lint/validate_schema.go
@@ -1,38 +1,10 @@
 package lint
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
 
-	"github.com/santhosh-tekuri/jsonschema"
 	"github.com/xeipuuv/gojsonschema"
 )
-
-func ValidateSchema2(unmarshalledYaml interface{}, jsonSchema []byte) error {
-
-	zarfYamlAsJsonBytes, err := json.Marshal(unmarshalledYaml)
-	if err != nil {
-		return err
-	}
-
-	compiler := jsonschema.NewCompiler()
-	inMemoryZarfSchema := "schema.json"
-
-	if err := compiler.AddResource(inMemoryZarfSchema, bytes.NewReader(jsonSchema)); err != nil {
-		return err
-	}
-	schema, err := compiler.Compile(inMemoryZarfSchema)
-	if err != nil {
-		return err
-	}
-	if err := schema.Validate(bytes.NewReader(zarfYamlAsJsonBytes)); err != nil {
-		return fmt.Errorf("schema validation error: %s", err)
-	}
-
-	return nil
-}
 
 func ValidateSchema(unmarshalledYaml interface{}, jsonSchema []byte) error {
 	schemaLoader := gojsonschema.NewBytesLoader(jsonSchema)
