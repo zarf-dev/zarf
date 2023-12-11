4cc3bf20d8d14ac8552e137ee513898bf7f8a807
Austin Abro <AustinAbro321@gmail.com>
2023-12-11 20:36:42 +0000

    refactor

---
+++ src/cmd/common/common.go
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2021-Present The Zarf Authors
+
+// Package common handles command configuration across all commands
+package common
+
+import (
+	"os"
+
+	"github.com/defenseunicorns/zarf/src/config/lang"
+	"github.com/defenseunicorns/zarf/src/pkg/message"
+	"github.com/defenseunicorns/zarf/src/types"
+)
+
+// SetBaseDirectory sets base directory on package config when given in args
+func SetBaseDirectory(args []string, pkgConfig *types.PackagerConfig) {
+	if len(args) > 0 {
+		pkgConfig.CreateOpts.BaseDir = args[0]
+	} else {
+		var err error
+		pkgConfig.CreateOpts.BaseDir, err = os.Getwd()
+		if err != nil {
+			message.Fatalf(err, lang.CmdPackageCreateErr, err.Error())
+		}
+	}
+}
