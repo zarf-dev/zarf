208ed8e148dd501f11e190bcef778302ca4c446a
Austin Abro <AustinAbro321@gmail.com>
2023-12-11 16:42:26 +0000

    fix lint script

--- hack/lint_all_zarf_packages.sh
+++ hack/lint_all_zarf_packages.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 use_build=$1
-BASEDIR=$(dirname "$0")
-cd ../$BASEDIR
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd $SCRIPTPATH
+cd ..
 find "." -type f -name 'zarf.yaml' | while read -r yaml_file; do
   dir=$(dirname "$yaml_file")
   if [[ "$dir" == *src/test/* ]] && [ "$use_build" != true ]; then
