802662194c144d8d31add21468470f5b4a846f1a
Austin Abro <AustinAbro321@gmail.com>
2023-12-12 17:35:51 +0000

    moving random to helpers

--- src/pkg/utils/helpers/random.go
+++ src/pkg/utils/helpers/random.go
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2021-Present The Zarf Authors
 
-// Package utils provides generic utility functions.
-package utils
+// Package helpers provides generic helper functions with no external imports
+package helpers
 
 import (
 	"crypto/rand"
-
-	"github.com/defenseunicorns/zarf/src/pkg/message"
 )
 
 // Very limited special chars for git / basic auth
@@ -15,25 +13,17 @@ import (
 const randomStringChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!~-"
 
 // RandomString generates a secure random string of the specified length.
-func RandomString(length int) string {
+func RandomString(length int) (string, error) {
 	bytes := make([]byte, length)
 
 	if _, err := rand.Read(bytes); err != nil {
-		message.Fatal(err, "unable to generate a random secret")
+		//message.Fatal(err, "unable to generate a random secret")
+		return "", err
 	}
 
 	for i, b := range bytes {
 		bytes[i] = randomStringChars[b%byte(len(randomStringChars))]
 	}
 
-	return string(bytes)
-}
-
-// First30last30 returns the source string that has been trimmed to 30 characters at the beginning and end.
-func First30last30(s string) string {
-	if len(s) > 60 {
-		return s[0:27] + "..." + s[len(s)-26:]
-	}
-
-	return s
+	return string(bytes), nil
 }
