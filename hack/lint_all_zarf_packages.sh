b69842171507b6ceb4f04d6890a694f165f6bcad
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2023-12-18 15:00:18 -0500

    Update hack/lint_all_zarf_packages.sh
    Co-authored-by: razzle <harry@razzle.cloud>

--- hack/lint_all_zarf_packages.sh
+++ hack/lint_all_zarf_packages.sh
@@ -4,7 +4,7 @@ ZARF_BIN=$1
 LINT_SRC_TEST=$2
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-cd $SCRIPTPATH
+cd "$SCRIPTPATH" || exit
 cd ..
 find "." -type f -name 'zarf.yaml' | while read -r yaml_file; do
   dir=$(dirname "$yaml_file")
