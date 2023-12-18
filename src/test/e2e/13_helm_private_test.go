0dbf1891fbd9553c53ce389ec8a12f9ce90a25f8
Austin Abro <AustinAbro321@gmail.com>
2023-12-18 17:07:22 +0000

    WIP

--- src/test/e2e/13_helm_private_test.go
+++ src/test/e2e/13_helm_private_test.go
@@ -1,10 +1,16 @@
 package test
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/defenseunicorns/zarf/src/pkg/cluster"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,7 +27,56 @@ func TestPrivateHelm(t *testing.T) {
 		os.Setenv("HELM_REPOSITORY_CONFIG", repoPath)
 		// TODO this doesn't work, do we care about giving this option through CLI
 		// _, _, err := e2e.Zarf("prepare", "find-images", packagePath, "--repository-config", repoPath)
-		_, _, err := e2e.Zarf("prepare", "find-images", packagePath)
-		require.NoError(t, err, "don't require an error because we want this to be a success")
+
+		c := cluster.NewClusterOrDie()
+		state, err := c.LoadZarfState()
+		require.NoError(t, err)
+		username := state.GitServer.PushUsername
+		password := state.GitServer.PushPassword
+
+		require.NoError(t, err)
+		tunnelGit, err := c.Connect(cluster.ZarfGit)
+		require.NoError(t, err)
+		defer tunnelGit.Close()
+
+		chartFilePath := filepath.Join("src", "test", "e2e", "podinfo-6.5.3.tgz")
+		url := fmt.Sprintf("http://%s/api/packages/%s/helm/api/charts", tunnelGit.Endpoint(), username)
+
+		// Open the file
+		file, err := os.Open(chartFilePath)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		body := &bytes.Buffer{}
+		writer := multipart.NewWriter(body)
+		part, err := writer.CreateFormFile("file", chartFilePath)
+		require.NoError(t, err)
+		_, err = io.Copy(part, file)
+		require.NoError(t, err)
+		writer.Close()
+
+		req, err := http.NewRequest("POST", url, body)
+		if err != nil {
+			panic(err)
+		}
+
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		req.SetBasicAuth(username, password)
+
+		client := &http.Client{}
+
+		// Execute the request
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		// helm repo add  --username {username} --password {password} gitea-temp-repo http://127.0.0.1:41861/api/packages/zarf-git-user/helm
+		// helm repo add   gitea-temp-repo http://127.0.0.1:41861/api/packages/zarf-git-user/helm
+		// helm cm-push ./{chart_file}.tgz {repo}
+
 	})
 }
