e3a74f2e7144c8e5c8e3dbb8ee674878b84b5286
razzle <harry@razzle.cloud>
2024-01-04 18:16:41 -0600

    re-org
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/pkg/interactive/components.go
+++ src/pkg/interactive/components.go
@@ -5,19 +5,14 @@
 package interactive
 
 import (
-	"fmt"
 	"path"
 	"slices"
 	"strings"
 
-	"github.com/AlecAivazis/survey/v2"
-	"github.com/defenseunicorns/zarf/src/config"
 	"github.com/defenseunicorns/zarf/src/config/lang"
 	"github.com/defenseunicorns/zarf/src/pkg/message"
-	"github.com/defenseunicorns/zarf/src/pkg/utils"
 	"github.com/defenseunicorns/zarf/src/pkg/utils/helpers"
 	"github.com/defenseunicorns/zarf/src/types"
-	"github.com/pterm/pterm"
 )
 
 type selectState int
@@ -182,72 +177,4 @@ func includedOrExcluded(component types.ZarfComponent, requestedComponentNames [
 
 	// All other cases we don't know if we should include or exclude yet
 	return unknown, ""
-}
-
-// SelectOptionalComponent prompts to confirm optional components
-func SelectOptionalComponent(component types.ZarfComponent) (confirmComponent bool) {
-	// Confirm flag passed, just use defaults
-	if config.CommonOptions.Confirm {
-		return component.Default
-	}
-
-	message.HorizontalRule()
-
-	displayComponent := component
-	displayComponent.Description = ""
-	utils.ColorPrintYAML(displayComponent, nil, false)
-	if component.Description != "" {
-		message.Question(component.Description)
-	}
-
-	prompt := &survey.Confirm{
-		Message: fmt.Sprintf("Deploy the %s component?", component.Name),
-		Default: component.Default,
-	}
-	if err := survey.AskOne(prompt, &confirmComponent); err != nil {
-		message.Fatalf(nil, lang.PkgDeployErrComponentSelectionCanceled, err.Error())
-	}
-
-	return confirmComponent
-}
-
-// SelectChoiceGroup prompts to select component groups
-func SelectChoiceGroup(componentGroup []types.ZarfComponent) types.ZarfComponent {
-	// Confirm flag passed, just use defaults
-	if config.CommonOptions.Confirm {
-		var componentNames []string
-		for _, component := range componentGroup {
-			// If the component is default, then return it
-			if component.Default {
-				return component
-			}
-			// Add each component name to the list
-			componentNames = append(componentNames, component.Name)
-		}
-		// If no default component was found, give up
-		message.Fatalf(nil, lang.PkgDeployErrNoDefaultOrSelection, strings.Join(componentNames, ","))
-	}
-
-	message.HorizontalRule()
-
-	var chosen int
-	var options []string
-
-	for _, component := range componentGroup {
-		text := fmt.Sprintf("Name: %s\n  Description: %s\n", component.Name, component.Description)
-		options = append(options, text)
-	}
-
-	prompt := &survey.Select{
-		Message: "Select a component to deploy:",
-		Options: options,
-	}
-
-	pterm.Println()
-
-	if err := survey.AskOne(prompt, &chosen); err != nil {
-		message.Fatalf(nil, lang.PkgDeployErrComponentSelectionCanceled, err.Error())
-	}
-
-	return componentGroup[chosen]
 }
