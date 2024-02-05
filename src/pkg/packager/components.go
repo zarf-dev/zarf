20d435a86d88e8b3e7e1ea96700c943c4709c57b
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2024-02-05 17:46:51 -0600

    Move filterComponents() to components.go and delete yaml.go

--- src/pkg/packager/components.go
+++ src/pkg/packager/components.go
@@ -6,6 +6,7 @@ package packager
 
 import (
 	"path"
+	"runtime"
 	"slices"
 	"strings"
 
@@ -23,6 +24,37 @@ const (
 	included
 	excluded
 )
+
+// filterComponents removes components not matching the current OS if filterByOS is set.
+func (p *Packager) filterComponents() {
+	// Filter each component to only compatible platforms.
+	filteredComponents := []types.ZarfComponent{}
+	for _, component := range p.cfg.Pkg.Components {
+		// Ignore only filters that are empty
+		var validArch, validOS bool
+
+		// Test for valid architecture
+		if component.Only.Cluster.Architecture == "" || component.Only.Cluster.Architecture == p.arch {
+			validArch = true
+		} else {
+			message.Debugf("Skipping component %s, %s is not compatible with %s", component.Name, component.Only.Cluster.Architecture, p.arch)
+		}
+
+		// Test for a valid OS
+		if component.Only.LocalOS == "" || component.Only.LocalOS == runtime.GOOS {
+			validOS = true
+		} else {
+			message.Debugf("Skipping component %s, %s is not compatible with %s", component.Name, component.Only.LocalOS, runtime.GOOS)
+		}
+
+		// If both the OS and architecture are valid, add the component to the filtered list
+		if validArch && validOS {
+			filteredComponents = append(filteredComponents, component)
+		}
+	}
+	// Update the active package with the filtered components.
+	p.cfg.Pkg.Components = filteredComponents
+}
 
 func (p *Packager) getSelectedComponents() []types.ZarfComponent {
 	var selectedComponents []types.ZarfComponent
