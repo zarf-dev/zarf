c35481402139162fb631bd04605ef075d0593a7d
Austin Abro <AustinAbro321@gmail.com>
2024-02-09 14:04:39 +0000

    rename

--- src/pkg/oci/copier.go
+++ src/pkg/oci/copier.go
@@ -55,8 +55,8 @@ func Copy(ctx context.Context, src *OrasRemote, dst *OrasRemote,
 		}
 		if exists {
 			src.log.Debug("Layer already exists in destination, skipping")
-			data := make([]byte, layer.Size)
-			progressBar.Write(data)
+			b := make([]byte, layer.Size)
+			progressBar.Write(b)
 			progressBar.UpdateTitle(fmt.Sprintf("[%d/%d] layers copied", idx+1, len(layers)))
 			sem.Release(1)
 			continue
