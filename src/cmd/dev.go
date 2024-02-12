a25d584e93c22b4b3059f1f8e21e73026ac1a404
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2024-02-12 12:31:30 -0600

    Refactor SetBaseDirectory() to match the razzle version

--- src/cmd/dev.go
+++ src/cmd/dev.go
@@ -40,7 +40,7 @@ var devDeployCmd = &cobra.Command{
 	Short: lang.CmdDevDeployShort,
 	Long:  lang.CmdDevDeployLong,
 	Run: func(_ *cobra.Command, args []string) {
-		pkgConfig.CreateOpts.SetBaseDirectory(args)
+		pkgConfig.CreateOpts.BaseDir = common.SetBaseDirectory(args)
 
 		v := common.GetViper()
 		pkgConfig.CreateOpts.SetVariables = helpers.TransformAndMergeMap(
@@ -185,8 +185,7 @@ var devFindImagesCmd = &cobra.Command{
 	Short:   lang.CmdDevFindImagesShort,
 	Long:    lang.CmdDevFindImagesLong,
 	Run: func(cmd *cobra.Command, args []string) {
-		// If a directory was provided, use that as the base directory
-		pkgConfig.CreateOpts.SetBaseDirectory(args)
+		pkgConfig.CreateOpts.BaseDir = common.SetBaseDirectory(args)
 
 		// Ensure uppercase keys from viper
 		v := common.GetViper()
@@ -232,7 +231,7 @@ var devLintCmd = &cobra.Command{
 	Short:   lang.CmdDevLintShort,
 	Long:    lang.CmdDevLintLong,
 	Run: func(cmd *cobra.Command, args []string) {
-		pkgConfig.CreateOpts.SetBaseDirectory(args)
+		pkgConfig.CreateOpts.BaseDir = common.SetBaseDirectory(args)
 		v := common.GetViper()
 		pkgConfig.CreateOpts.SetVariables = helpers.TransformAndMergeMap(
 			v.GetStringMapString(common.VPkgCreateSet), pkgConfig.CreateOpts.SetVariables, strings.ToUpper)
