b604441070c5d33a78e83c64bcce61de252a4865
Austin Abro <AustinAbro321@gmail.com>
2024-02-20 17:09:13 +0000

    update title

--- src/pkg/oci/copier.go
+++ src/pkg/oci/copier.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2021-Present The Zarf Authors
 
-// Package oci contains functions for interacting with Zarf packages stored in OCI registries.
+// Package oci contains functions for interacting with artifacts stored in OCI registries.
 package oci
 
 import (
