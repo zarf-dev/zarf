21f1d8af1616a1683e19b3f1d204032f95d0fa78
Wayne Starr <Racer159@users.noreply.github.com>
2024-02-20 11:08:12 -0700

    Update src/test/e2e/05_tarball_test.go
    Co-authored-by: Lucas Rodriguez <lucas.rodriguez9616@gmail.com>

--- src/test/e2e/05_tarball_test.go
+++ src/test/e2e/05_tarball_test.go
@@ -29,7 +29,7 @@ func TestMultiPartPackage(t *testing.T) {
 
 	e2e.CleanFiles(deployPath, outputFile)
 
-	// Create the package with a max size of 1MB
+	// Create the package with a max size of 20MB
 	stdOut, stdErr, err := e2e.Zarf("package", "create", createPath, "--max-package-size=20", "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 
