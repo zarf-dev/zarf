fffa8ad169e8c2c4e9a8a93dbebfd0842c9bcde7
Austin Abro <AustinAbro321@gmail.com>
2024-02-21 14:11:40 +0000

    simpliest test working

--- src/pkg/oci/pull_test.go
+++ src/pkg/oci/pull_test.go
@@ -7,12 +7,9 @@ package oci
 import (
 	"context"
 	"fmt"
-	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/defenseunicorns/zarf/src/pkg/utils"
 	"github.com/distribution/distribution/v3/configuration"
 	"github.com/distribution/distribution/v3/registry"
 	_ "github.com/distribution/distribution/v3/registry/auth/htpasswd"           // used for docker test registry
@@ -20,7 +17,6 @@ import (
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/phayes/freeport"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func TestPull(t *testing.T) {
@@ -28,23 +24,7 @@ func TestPull(t *testing.T) {
 
 		// This is the general flow that helm is doing
 		// I want to figure out what tool they are using to actually start their server
-
-		dir, _ := utils.MakeTempDir("/tmp/test-oci")
-
 		ctx := context.TODO()
-
-		testHtpasswdFileBasename := "authtest.htpasswd"
-		testUsername, testPassword := "username", "password"
-
-		pwBytes, err := bcrypt.GenerateFromPassword([]byte(testPassword), bcrypt.DefaultCost)
-		if err != nil {
-			t.Fatal("error generating bcrypt password for test htpasswd file")
-		}
-		htpasswdPath := filepath.Join(dir, testHtpasswdFileBasename)
-		err = os.WriteFile(htpasswdPath, []byte(fmt.Sprintf("%s:%s\n", testUsername, string(pwBytes))), 0644)
-		if err != nil {
-			t.Fatalf("error creating test htpasswd file")
-		}
 
 		// Registry config
 		config := &configuration.Configuration{}
