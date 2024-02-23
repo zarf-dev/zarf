8f41236670edaa25a77a96e88919487f4b0949f9
Vibhav Bobade <vibhav.bobde@gmail.com>
2024-02-24 03:47:26 +0530

    read file and update expected output to be a regex

--- src/test/e2e/13_find_images_test.go
+++ src/test/e2e/13_find_images_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"os"
 	"path/filepath"
+	"regexp"
 	"testing"
 )
 
@@ -14,11 +15,7 @@ func TestFindImages(t *testing.T) {
 		t.Log("E2E: Test Find Images")
 
 		testPackagePath := filepath.Join("examples", "dos-games")
-		expectedOutput := []byte{}
-		f, err := os.Open("src/test/packages/13-find-images/dos-games-find-images-expected.txt")
-		defer f.Close()
-
-		_, err = f.Read(expectedOutput)
+		expectedOutput, err := os.ReadFile("src/test/packages/13-find-images/dos-games-find-images-expected.txt")
 		require.NoError(t, err, "Expect no error here while reading expectedOutput of the expected output file")
 
 		stdout, _, err := e2e.Zarf("dev", "find-images", testPackagePath)
@@ -30,27 +27,21 @@ func TestFindImages(t *testing.T) {
 		t.Log("E2E: Test Find Images against a helm chart with why flag")
 
 		testPackagePath := filepath.Join("examples", "helm-charts")
-		expectedOutput := []byte{}
-		f, err := os.Open("src/test/packages/13-find-images/helm-charts-find-images-why-expected.txt")
-		defer f.Close()
-
-		_, err = f.Read(expectedOutput)
+		expectedOutput, err := os.ReadFile("src/test/packages/13-find-images/helm-charts-find-images-why-expected.txt")
 		require.NoError(t, err, "Expect no error here while reading expectedOutput of the expected output file")
 
 		stdout, _, err := e2e.Zarf("dev", "find-images", testPackagePath, "--why", "curlimages/curl:7.69.0")
 		require.NoError(t, err, "Expect no error here")
-		require.Contains(t, stdout, string(expectedOutput))
+		match, err := regexp.MatchString(string(expectedOutput), stdout)
+		require.NoError(t, err, "Expect no error here while matching expected output with actual output"
+		require.True(t, match, "Expected output does not match actual output")
 	})
 
 	t.Run("zarf test find images --why w/  manifests success", func(t *testing.T) {
 		t.Log("E2E: Test Find Images against a helm chart with why flag")
 
 		testPackagePath := filepath.Join("examples", "manifests")
-		expectedOutput := []byte{}
-		f, err := os.Open("src/test/packages/13-find-images/manifests-find-images-why-expected.txt")
-		defer f.Close()
-
-		_, err = f.Read(expectedOutput)
+		expectedOutput, err := os.ReadFile("src/test/packages/13-find-images/manifests-find-images-why-expected.txt")
 		require.NoError(t, err, "Expect no error here while reading expectedOutput of the expected output file")
 
 		stdout, _, err := e2e.Zarf("dev", "find-images", testPackagePath, "--why", "httpd:alpine3.18")
