9943d612de784a89e1aa56d45495de333e6944c1
Austin Abro <AustinAbro321@gmail.com>
2024-02-26 18:20:08 +0000

    spelling

--- src/pkg/utils/helpers/io.go
+++ src/pkg/utils/helpers/io.go
@@ -5,14 +5,13 @@
 package helpers
 
 const (
-	// ReadWriteUser is used for any internal file not normallly used by the end user or containing senstive data
+	// ReadWriteUser is used for any internal file not normally used by the end user or containing sensitive data
 	ReadWriteUser = 0600
 	// ReadAllWriteUser is used for any non sensitive file intended to be consumed by the end user
 	ReadAllWriteUser = 0644
 
 	// ReadWriteExecuteUser is used for any directory or executable not normally used by the end user or containing sensitive data
 	ReadWriteExecuteUser = 0700
-
 	// ReadExecuteAllWriteUser is used for any non sensitive directory or executable intended to be consumed by the end user
 	ReadExecuteAllWriteUser = 0755
 )
