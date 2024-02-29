17f34be2bd9e9d434404b4cac26b21401bc8ac80
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2024-02-29 17:13:48 -0600

    Use ReadZarfYAML on Deploy() and delete runMigrations()

--- src/pkg/packager/deploy.go
+++ src/pkg/packager/deploy.go
@@ -46,12 +46,12 @@ func (p *Packager) Deploy() (err error) {
 		return fmt.Errorf("unable to load the package: %w", err)
 	}
 
-	if err := utils.ReadYaml(p.layout.ZarfYAML, &p.cfg.Pkg); err != nil {
+	p.cfg.Pkg, p.warnings, err = p.layout.ReadZarfYAML(p.layout.ZarfYAML)
+	if err != nil {
 		return err
 	}
-	p.arch = config.GetArch(p.cfg.Pkg.Metadata.Architecture, p.cfg.Pkg.Build.Architecture)
 
-	p.runMigrations()
+	p.arch = config.GetArch(p.cfg.Pkg.Metadata.Architecture, p.cfg.Pkg.Build.Architecture)
 
 	if err := p.validateLastNonBreakingVersion(); err != nil {
 		return err
