bc7987c502fdcf751dbc2127cfa69e8334f3c363
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2024-03-04 13:21:49 -0500

    chore: refactor and purify the OCI library within Zarf. (#2235)
    ## Description
    
    Move OCI lib out, so it can be more easily used with other projects and
    contributed to by multiple teams.
    
    Fixes: #2252 #2288 
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [X] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [ ] Test, docs, adr added or updated as needed
    - [ ] [Contributor Guide
    Steps](https://github.com/defenseunicorns/zarf/blob/main/CONTRIBUTING.md#developer-workflow)
    followed
    
    ---------
    
    Co-authored-by: razzle <harry@razzle.cloud>
    Co-authored-by: Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
    Co-authored-by: Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
    Co-authored-by: Wayne Starr <Racer159@users.noreply.github.com>

---
+++ src/pkg/message/handler.go
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2021-Present The Zarf Authors
+
+// Package message provides a rich set of functions for displaying messages to the user.
+package message
+
+import (
+	"context"
+	"log/slog"
+)
+
+// ZarfHandler is a simple handler that implements the slog.Handler interface
+type ZarfHandler struct{}
+
+// Enabled is always set to true as zarf logging functions are already aware of if they are allowed to be called
+func (z ZarfHandler) Enabled(_ context.Context, _ slog.Level) bool {
+	return true
+}
+
+// WithAttrs is not suppported
+func (z ZarfHandler) WithAttrs(_ []slog.Attr) slog.Handler {
+	return z
+}
+
+// WithGroup is not supported
+func (z ZarfHandler) WithGroup(_ string) slog.Handler {
+	return z
+}
+
+// Handle prints the respective logging function in zarf
+// This function ignores any key pairs passed through the record
+func (z ZarfHandler) Handle(_ context.Context, record slog.Record) error {
+	level := record.Level
+	message := record.Message
+
+	switch level {
+	case slog.LevelDebug:
+		Debug(message)
+	case slog.LevelInfo:
+		Info(message)
+	case slog.LevelWarn:
+		Warn(message)
+	case slog.LevelError:
+		Warn(message)
+	}
+	return nil
+}
