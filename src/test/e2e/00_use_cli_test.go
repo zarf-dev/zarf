64a9f8548c1c891b52b11ff95849135d6e2d9ae0
Austin Abro <AustinAbro321@gmail.com>
2024-03-06 20:01:51 +0000

    moving find images tests together

--- src/test/e2e/00_use_cli_test.go
+++ src/test/e2e/00_use_cli_test.go
@@ -70,37 +70,6 @@ func TestUseCLI(t *testing.T) {
 		require.Contains(t, stdOut, yamlVersion, "Zarf version should be the same in all formats")
 	})
 
-	t.Run("zarf prepare find-images", func(t *testing.T) {
-		t.Parallel()
-		// Test `zarf prepare find-images` for a remote asset
-		stdOut, stdErr, err := e2e.Zarf("prepare", "find-images", "examples/helm-charts", "--kube-version=v1.23.0")
-		require.NoError(t, err, stdOut, stdErr)
-		require.Contains(t, stdOut, "ghcr.io/stefanprodan/podinfo:6.4.0", "The chart image should be found by Zarf")
-		// Test `zarf prepare find-images` with a chart that uses helm annotations
-		stdOut, stdErr, err = e2e.Zarf("prepare", "find-images", "src/test/packages/00-helm-annotations")
-		require.NoError(t, err, stdOut, stdErr)
-		require.Contains(t, stdOut, "registry1.dso.mil/ironbank/opensource/istio/pilot:1.17.2", "The pilot image should be found by Zarf")
-	})
-
-	t.Run("zarf prepare find-images --kube-version", func(t *testing.T) {
-		t.Parallel()
-		controllerImageWithTag := "quay.io/jetstack/cert-manager-controller:v1.11.1"
-		controlImageWithSignature := "quay.io/jetstack/cert-manager-controller:sha256-4f1782c8316f34aae6b9ab823c3e6b7e6e4d92ec5dac21de6a17c3da44c364f1.sig"
-
-		// Test `zarf prepare find-images` on a chart that has a `kubeVersion` declaration greater than the Helm default (v1.20.0)
-		// This should pass because we build Zarf specifying the kubeVersion value from the kubernetes client-go library instead
-		stdOut, stdErr, err := e2e.Zarf("prepare", "find-images", "src/test/packages/00-kube-version-override")
-		require.NoError(t, err, stdOut, stdErr)
-		require.Contains(t, stdOut, controllerImageWithTag, "The chart image should be found by Zarf")
-		require.Contains(t, stdOut, controlImageWithSignature, "The image signature should be found by Zarf")
-
-		// Test `zarf prepare find-images` with `--kube-version` specified and less than than the declared minimum (v1.21.0)
-		stdOut, stdErr, err = e2e.Zarf("prepare", "find-images", "--kube-version=v1.20.0", "src/test/packages/00-kube-version-override")
-		require.Error(t, err, stdOut, stdErr)
-		require.Contains(t, stdErr, "Problem rendering the helm template for cert-manager", "The kubeVersion declaration should prevent this from templating")
-		require.Contains(t, stdErr, "following charts had errors: [cert-manager]", "Zarf should print an ending error message")
-	})
-
 	t.Run("zarf deploy should fail when given a bad component input", func(t *testing.T) {
 		t.Parallel()
 		// Test for expected failure when given a bad component input
@@ -244,22 +213,4 @@ func TestUseCLI(t *testing.T) {
 		require.FileExists(t, tlsKey)
 	})
 
-	t.Run("zarf dev find-images with helm or manifest vars", func(t *testing.T) {
-		t.Parallel()
-
-		registry := "coolregistry.gov"
-		agentTag := "test"
-		stdOut, _, err := e2e.Zarf("prepare", "find-images", ".", "--registry-url", registry, "--create-set", fmt.Sprintf("agent_image_tag=%s", agentTag))
-		require.NoError(t, err)
-		internalRegistryImage := fmt.Sprintf("%s/%s:%s", registry, "defenseunicorns/zarf/agent", agentTag)
-		require.Contains(t, stdOut, internalRegistryImage, "registry image should be found with registry url")
-		require.Contains(t, stdOut, "busybox:latest", "Busybox image should be found as long as helm chart doesn't error")
-
-		path := filepath.Join("src", "test", "packages", "00-find-images-with-vars")
-		stdOut, _, err = e2e.Zarf("prepare", "find-images", path, "--deploy-set", "BUSYBOX_IMAGE=busybox:earliest")
-		require.NoError(t, err)
-
-		require.Contains(t, stdOut, "nginx:latest", "Manifests aren't interpreting vars")
-		require.Contains(t, stdOut, "busybox:earliest", "Values files aren't interpreting vars")
-	})
 }
--- src/test/e2e/13_find_images_test.go
+++ src/test/e2e/13_find_images_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -45,6 +46,56 @@ func TestFindImages(t *testing.T) {
 		stdout, _, err := e2e.Zarf("dev", "find-images", testPackagePath, "--why", "httpd:alpine3.18")
 		require.NoError(t, err)
 		require.Contains(t, stdout, string(expectedOutput))
+	})
+
+	t.Run("zarf prepare find-images", func(t *testing.T) {
+		t.Parallel()
+		// Test `zarf prepare find-images` for a remote asset
+		stdOut, stdErr, err := e2e.Zarf("prepare", "find-images", "examples/helm-charts")
+		require.NoError(t, err, stdOut, stdErr)
+		require.Contains(t, stdOut, "ghcr.io/stefanprodan/podinfo:6.4.0", "The chart image should be found by Zarf")
+		// Test `zarf prepare find-images` with a chart that uses helm annotations
+		stdOut, stdErr, err = e2e.Zarf("prepare", "find-images", "src/test/packages/00-helm-annotations")
+		require.NoError(t, err, stdOut, stdErr)
+		require.Contains(t, stdOut, "registry1.dso.mil/ironbank/opensource/istio/pilot:1.17.2", "The pilot image should be found by Zarf")
+	})
+
+	t.Run("zarf prepare find-images --kube-version", func(t *testing.T) {
+		t.Parallel()
+		controllerImageWithTag := "quay.io/jetstack/cert-manager-controller:v1.11.1"
+		controlImageWithSignature := "quay.io/jetstack/cert-manager-controller:sha256-4f1782c8316f34aae6b9ab823c3e6b7e6e4d92ec5dac21de6a17c3da44c364f1.sig"
+
+		// Test `zarf prepare find-images` on a chart that has a `kubeVersion` declaration greater than the Helm default (v1.20.0)
+		// This should pass because we build Zarf specifying the kubeVersion value from the kubernetes client-go library instead
+		stdOut, stdErr, err := e2e.Zarf("prepare", "find-images", "src/test/packages/00-kube-version-override")
+		require.NoError(t, err, stdOut, stdErr)
+		require.Contains(t, stdOut, controllerImageWithTag, "The chart image should be found by Zarf")
+		require.Contains(t, stdOut, controlImageWithSignature, "The image signature should be found by Zarf")
+
+		// Test `zarf prepare find-images` with `--kube-version` specified and less than than the declared minimum (v1.21.0)
+		stdOut, stdErr, err = e2e.Zarf("prepare", "find-images", "--kube-version=v1.20.0", "src/test/packages/00-kube-version-override")
+		require.Error(t, err, stdOut, stdErr)
+		require.Contains(t, stdErr, "Problem rendering the helm template for cert-manager", "The kubeVersion declaration should prevent this from templating")
+		require.Contains(t, stdErr, "following charts had errors: [cert-manager]", "Zarf should print an ending error message")
+	})
+
+	t.Run("zarf dev find-images with helm or manifest vars", func(t *testing.T) {
+		t.Parallel()
+
+		registry := "coolregistry.gov"
+		agentTag := "test"
+		stdOut, _, err := e2e.Zarf("prepare", "find-images", ".", "--registry-url", registry, "--create-set", fmt.Sprintf("agent_image_tag=%s", agentTag))
+		require.NoError(t, err)
+		internalRegistryImage := fmt.Sprintf("%s/%s:%s", registry, "defenseunicorns/zarf/agent", agentTag)
+		require.Contains(t, stdOut, internalRegistryImage, "registry image should be found with registry url")
+		require.Contains(t, stdOut, "busybox:latest", "Busybox image should be found as long as helm chart doesn't error")
+
+		path := filepath.Join("src", "test", "packages", "00-find-images-with-vars")
+		stdOut, _, err = e2e.Zarf("prepare", "find-images", path, "--deploy-set", "BUSYBOX_IMAGE=busybox:earliest")
+		require.NoError(t, err)
+
+		require.Contains(t, stdOut, "nginx:latest", "Manifests aren't interpreting vars")
+		require.Contains(t, stdOut, "busybox:earliest", "Values files aren't interpreting vars")
 	})
 
 }
