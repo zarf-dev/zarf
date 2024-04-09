c7eae7791a306b54462568f1193d90f75dd1e137
Austin Abro <AustinAbro321@gmail.com>
2024-04-09 17:04:13 +0000

    simplify

--- src/test/e2e/14_create_sha_index_test.go
+++ src/test/e2e/14_create_sha_index_test.go
@@ -10,12 +10,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateIndexSha(t *testing.T) {
-	t.Log("E2E: Create Templating")
+func TestCreateIndexShaErrors(t *testing.T) {
+	t.Log("E2E: CreateIndexShaErrors")
 
-	_, stderr, err := e2e.Zarf("package", "create", "src/test/packages/14-index-sha", "--confirm")
-	// Not sure why this isn't working
+	_, stderr, err := e2e.Zarf("package", "create", "src/test/packages/14-index-sha/image-index", "--confirm")
 	require.Error(t, err)
-	require.Contains(t, stderr, "docker.io/defenseunicorns/zarf-game:multi-tile-dark@sha256:f78e442f0f3eb3e9459b5ae6b1a8fda62f8dfe818112e7d130a4e8ae72b3cbff")
+	require.Contains(t, stderr, "ghcr.io/defenseunicorns/zarf/agent:v0.32.6@sha256:b3fabdc7d4ecd0f396016ef78da19002c39e3ace352ea0ae4baa2ce9d5958376")
+
+	_, stderr, err = e2e.Zarf("package", "create", "src/test/packages/14-index-sha/manifest-list", "--confirm")
+	require.Error(t, err)
+	require.Contains(t, stderr, "docker.io/defenseunicorns/zarf-game@sha256:f78e442f0f3eb3e9459b5ae6b1a8fda62f8dfe818112e7d130a4e8ae72b3cbff")
 
 }
