6f9346bfb4a460950c6b419e03c8a84b4484676d
Austin Abro <AustinAbro321@gmail.com>
2024-04-19 16:43:42 +0000

    check

--- hack/check-vulnerabilities.sh
+++ hack/check-vulnerabilities.sh
@@ -7,10 +7,10 @@ MAIN_BRANCH="main"
 TARGET_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 git checkout $MAIN_BRANCH
-zarf tools sbom scan . -o json --exclude './site' --exclude './examples' | grype -o template -t hack/.templates/compare.tmpl > build/main.json
+go run main.go tools sbom scan . -o json --exclude './site' --exclude './examples' | grype -o template -t hack/.templates/compare.tmpl > build/main.json
 
 git checkout $TARGET_BRANCH
-zarf tools sbom scan . -o json --exclude './site' --exclude './examples' | grype -o template -t hack/.templates/compare.tmpl > build/target.json
+go run main.go tools sbom scan . -o json --exclude './site' --exclude './examples' | grype -o template -t hack/.templates/compare.tmpl > build/target.json
 
 
 result=$(jq --slurp '.[0] - .[1]' build/target.json build/main.json)
