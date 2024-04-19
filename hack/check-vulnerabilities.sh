a3aa14d4862d3ef89b254a339faee3df3bc870c6
Austin Abro <AustinAbro321@gmail.com>
2024-04-19 16:33:21 +0000

    new compare workflow

---
+++ hack/check-vulnerabilities.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# # Exit on error
+set -e
+
+MAIN_BRANCH="main"
+TARGET_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+git checkout $MAIN_BRANCH
+zarf tools sbom scan . -o json --exclude './site' --exclude './examples' | grype -o template -t hack/.templates/compare.tmpl > build/main.json
+
+git checkout $TARGET_BRANCH
+zarf tools sbom scan . -o json --exclude './site' --exclude './examples' | grype -o template -t hack/.templates/compare.tmpl > build/target.json
+
+
+result=$(jq --slurp '.[0] - .[1]' build/target.json build/main.json)
+
+if [[ "$result" == "[]" ]]; then
+  echo "no new vulnerabilities on $TARGET_BRANCH"
+  exit 0
+else
+  echo "new CVEs have been added with IDs $result"
+  exit 1
+fi
