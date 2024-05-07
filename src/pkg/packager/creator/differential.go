bf691128d2506b9f3e729f456d9c52780d24cb05
Philip Laine <philip.laine@gmail.com>
2024-05-07 16:59:16 +0200

    refactor: remove copies from components to a filter (#2474)
    ## Description
    
    This change refactors remove copies from components to a filter.
    
    ## Related Issue
    
    Depends on: #2473
    Fixes #2337 
    
    ## Checklist before merging
    
    - [x] Test, docs, adr added or updated as needed
    - [x] [Contributor Guide
    Steps](https://github.com/defenseunicorns/zarf/blob/main/.github/CONTRIBUTING.md#developer-workflow)
    followed
    
    ---------
    
    Co-authored-by: razzle <razzle@defenseunicorns.com>

--- src/pkg/packager/creator/differential.go
+++ src/pkg/packager/creator/differential.go
@@ -5,17 +5,13 @@
 package creator
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/defenseunicorns/zarf/src/config"
-	"github.com/defenseunicorns/zarf/src/internal/packager/git"
 	"github.com/defenseunicorns/zarf/src/pkg/layout"
 	"github.com/defenseunicorns/zarf/src/pkg/packager/sources"
-	"github.com/defenseunicorns/zarf/src/pkg/transform"
 	"github.com/defenseunicorns/zarf/src/pkg/utils"
 	"github.com/defenseunicorns/zarf/src/types"
-	"github.com/go-git/go-git/v5/plumbing"
 )
 
 // loadDifferentialData sets any images and repos from the existing reference package in the DifferentialData and returns it.
@@ -57,48 +53,4 @@ func loadDifferentialData(diffPkgPath string) (diffData *types.DifferentialData,
 		DifferentialRepos:          allIncludedReposMap,
 		DifferentialPackageVersion: diffPkg.Metadata.Version,
 	}, nil
-}
-
-// removeCopiesFromComponents removes any images and repos already present in the reference package components.
-func removeCopiesFromComponents(components []types.ZarfComponent, loadedDiffData *types.DifferentialData) (diffComponents []types.ZarfComponent, err error) {
-	for _, component := range components {
-		newImageList := []string{}
-		newRepoList := []string{}
-
-		for _, img := range component.Images {
-			imgRef, err := transform.ParseImageRef(img)
-			if err != nil {
-				return nil, fmt.Errorf("unable to parse image ref %s: %s", img, err.Error())
-			}
-
-			imgTag := imgRef.TagOrDigest
-			includeImage := imgTag == ":latest" || imgTag == ":stable" || imgTag == ":nightly"
-			if includeImage || !loadedDiffData.DifferentialImages[img] {
-				newImageList = append(newImageList, img)
-			}
-		}
-
-		for _, repoURL := range component.Repos {
-			_, refPlain, err := transform.GitURLSplitRef(repoURL)
-			if err != nil {
-				return nil, err
-			}
-
-			var ref plumbing.ReferenceName
-			if refPlain != "" {
-				ref = git.ParseRef(refPlain)
-			}
-
-			includeRepo := ref == "" || (!ref.IsTag() && !plumbing.IsHash(refPlain))
-			if includeRepo || !loadedDiffData.DifferentialRepos[repoURL] {
-				newRepoList = append(newRepoList, repoURL)
-			}
-		}
-
-		component.Images = newImageList
-		component.Repos = newRepoList
-		diffComponents = append(diffComponents, component)
-	}
-
-	return diffComponents, nil
 }
