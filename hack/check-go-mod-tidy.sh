507dc70c0e3ba1827cfdbc907db6db4cc3d6c7e2
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2024-05-07 13:58:03 -0500

    fix: simplify go mod tidy check
    Adds go mod tidy to run before every build
    Removes script, workflow, make target, and pre-commit hook
    to simplify. Adding go mod tidy to our build step should
    catch most issues with out of sync deps.

--- hack/check-go-mod-tidy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-go mod tidy
-if ! git diff --quiet go.mod go.sum; then
-  echo "ERROR: Changes detected after running 'go mod tidy'. Please run 'go mod tidy' and commit the changes."
-  exit 1
-fi
