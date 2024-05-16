d78e23705821c4ba17bc8cbee061a0cff65272b9
Austin Abro <AustinAbro321@gmail.com>
2024-05-16 14:56:40 +0000

    using new versions of helm, allowing helm charts to be pulled

--- src/internal/packager/images/pull.go
+++ src/internal/packager/images/pull.go
@@ -165,9 +165,14 @@ func Pull(ctx context.Context, cfg PullConfig) (map[transform.Image]v1.Image, er
 				return fmt.Errorf("%s resolved to an index, please select a specific platform to use", refInfo.Reference)
 			}
 
-			img = cache.Image(img, cache.NewFilesystemCache(cfg.CacheDirectory))
-
 			manifest, err := img.Manifest()
+
+			// Only use the cached image if it is a container image as the crane cache implementation
+			// looks at specifics like diff ids which can fail non container OCI images like helm charts
+			if manifest.Config.MediaType.IsConfig() {
+				img = cache.Image(img, cache.NewFilesystemCache(cfg.CacheDirectory))
+			}
+
 			if err != nil {
 				return fmt.Errorf("unable to get manifest for %s: %w", refInfo.Reference, err)
 			}
