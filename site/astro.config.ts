75dc869d4b69ebfcd5d8adb86ef8a5e98f4d78a3
Xander Grzywinski <xandergrzyw@gmail.com>
2024-05-21 11:48:48 -0700

    actually add the thing for really real
    Signed-off-by: Xander Grzywinski <xandergrzyw@gmail.com>

--- site/astro.config.ts
+++ site/astro.config.ts
@@ -28,7 +28,11 @@ export default defineConfig({
       head: [
         {
           tag: "script",
-          content: "(function(w,d,s,l,i){ ... })(window,document,'script','dataLayer','G-N1XZ8ZXCWL');",
+          content: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','G-N1XZ8ZXCWL');`,
         },
       ],
       components: {
