ff83e191cc6170eb863833a34d16290fa1d0d43f
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2024-05-30 15:38:37 -0500

    test: remove validate pkg arch e2e test (#2563)
    ## Description
    Removes `TestMismatchedArchitectures` e2e test and refactors
    `TestValidatePackageArchitecture` unit test
    
    ## Related Issue
    
    Relates to #2562
    
    ## Checklist before merging
    
    - [x] Test, docs, adr added or updated as needed
    - [x] [Contributor Guide
    Steps](https://github.com/defenseunicorns/zarf/blob/main/.github/CONTRIBUTING.md#developer-workflow)
    followed

--- src/test/e2e/29_mismatched_checks_test.go
+++ src/test/e2e/29_mismatched_checks_test.go
@@ -15,29 +15,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestMismatchedArchitectures ensures that zarf produces an error
-// when the package architecture doesn't match the target cluster architecture.
-func TestMismatchedArchitectures(t *testing.T) {
-	t.Log("E2E: Mismatched architectures")
-	e2e.SetupWithCluster(t)
-
-	var (
-		mismatchedArch         = e2e.GetMismatchedArch()
-		mismatchedGamesPackage = fmt.Sprintf("zarf-package-dos-games-%s-1.0.0.tar.zst", mismatchedArch)
-		expectedErrorMessage   = fmt.Sprintf("this package architecture is %s", mismatchedArch)
-	)
-
-	// Build dos-games package with different arch than the cluster arch.
-	stdOut, stdErr, err := e2e.Zarf("package", "create", "examples/dos-games/", "--architecture", mismatchedArch, "--confirm")
-	require.NoError(t, err, stdOut, stdErr)
-	defer e2e.CleanFiles(mismatchedGamesPackage)
-
-	// Ensure zarf package deploy returns an error because of the mismatched architectures.
-	_, stdErr, err = e2e.Zarf("package", "deploy", mismatchedGamesPackage, "--confirm")
-	require.Error(t, err, stdErr)
-	require.Contains(t, e2e.StripMessageFormatting(stdErr), expectedErrorMessage)
-}
-
 // TestMismatchedVersions ensures that zarf produces a warning
 // when the initialized version of Zarf doesn't match the current CLI
 func TestMismatchedVersions(t *testing.T) {
