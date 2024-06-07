a04fc5f22e533ab478c55c42ccd99f918128b759
Austin Abro <AustinAbro321@gmail.com>
2024-06-07 17:38:50 +0000

    simplify

--- src/internal/packager/images/pull_test.go
+++ src/internal/packager/images/pull_test.go
@@ -19,12 +19,11 @@ func TestPull(t *testing.T) {
 
 		ref, err := transform.ParseImageRef("ghcr.io/stefanprodan/podinfo:sha256-57a654ace69ec02ba8973093b6a786faa15640575fbf0dbb603db55aca2ccec8.sig")
 		require.NoError(t, err)
-		tmpDestDir := t.TempDir()
-		tmpCacheDir := t.TempDir()
-		destDir := filepath.Join(tmpDestDir, "images")
+		destDir := t.TempDir()
+		cacheDir := t.TempDir()
 		pullConfig := PullConfig{
 			DestinationDirectory: destDir,
-			CacheDirectory:       tmpCacheDir,
+			CacheDirectory:       cacheDir,
 			ImageList: []transform.Image{
 				ref,
 			},
@@ -34,7 +33,7 @@ func TestPull(t *testing.T) {
 		require.NoError(t, err)
 		require.FileExists(t, filepath.Join(destDir, "blobs/sha256/3e84ea487b4c52a3299cf2996f70e7e1721236a0998da33a0e30107108486b3e"))
 
-		dir, err := os.ReadDir(tmpCacheDir)
+		dir, err := os.ReadDir(cacheDir)
 		require.NoError(t, err)
 		require.Empty(t, dir)
 	})
