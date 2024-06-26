5505953aa4ac739ab67e7ab4e4e4a6067916947d
razzle <harry@razzle.cloud>
2024-06-26 01:22:27 -0500

    functionality is working
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/pkg/packager/dev.go
+++ src/pkg/packager/dev.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/defenseunicorns/pkg/helpers/v2"
 	"github.com/defenseunicorns/zarf/src/config"
+	"github.com/defenseunicorns/zarf/src/pkg/cluster"
 	"github.com/defenseunicorns/zarf/src/pkg/layout"
 	"github.com/defenseunicorns/zarf/src/pkg/message"
 	"github.com/defenseunicorns/zarf/src/pkg/packager/creator"
@@ -22,7 +23,10 @@ import (
 // DevDeploy creates + deploys a package in one shot
 func (p *Packager) DevDeploy(ctx context.Context) error {
 	config.CommonOptions.Confirm = true
-	p.cfg.CreateOpts.SkipSBOM = !p.cfg.CreateOpts.NoYOLO
+	p.cfg.CreateOpts.SkipSBOM = true
+
+	// todo: implement dry-run for helm charts
+	// diff images before push
 
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -61,16 +65,61 @@ func (p *Packager) DevDeploy(ctx context.Context) error {
 		return fmt.Errorf("unable to set the active variables: %w", err)
 	}
 
+	preserveComponents := p.cfg.Pkg.Components
+
 	// If building in yolo mode, strip out all images and repos
 	if !p.cfg.CreateOpts.NoYOLO {
 		for idx := range p.cfg.Pkg.Components {
 			p.cfg.Pkg.Components[idx].Images = []string{}
 			p.cfg.Pkg.Components[idx].Repos = []string{}
 		}
+	} else {
+		cluster, _ := cluster.NewCluster()
+
+		dp, _ := cluster.GetDeployedPackage(ctx, p.cfg.Pkg.Metadata.Name)
+
+		booleanDiff := func(a, b []string) []string {
+			m := make(map[string]bool, len(a))
+			for _, item := range a {
+				m[item] = true
+			}
+			for _, item := range b {
+				// if the item only exists in b,
+				// add it to m
+				if _, ok := m[item]; !ok {
+					m[item] = true
+				} else {
+					// if item is in both a and b,
+					// delete it from m
+					delete(m, item)
+				}
+			}
+			var diff []string
+			for item := range m {
+				diff = append(diff, item)
+			}
+			return diff
+		}
+
+		if dp != nil {
+			// boolean diff the deployed package images with the package images
+			for _, c := range dp.Data.Components {
+				for idx, pending := range p.cfg.Pkg.Components {
+					if c.Name == pending.Name {
+						p.cfg.Pkg.Components[idx].Images = booleanDiff(c.Images, pending.Images)
+					}
+				}
+			}
+		}
 	}
 
 	if err := pc.Assemble(ctx, p.layout, p.cfg.Pkg.Components, p.cfg.Pkg.Metadata.Architecture); err != nil {
 		return err
+	}
+
+	if p.cfg.CreateOpts.NoYOLO {
+		// restore potentially mutate components
+		p.cfg.Pkg.Components = preserveComponents
 	}
 
 	message.HeaderInfof("📦 PACKAGE DEPLOY %s", p.cfg.Pkg.Metadata.Name)
