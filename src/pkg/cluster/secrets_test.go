2b08e4ac959fed92c20a441fe01e2628d20c098c
Austin Abro <AustinAbro321@gmail.com>
2024-06-28 12:47:57 +0000

    feat: flux OCI support in Zarf Agent

--- src/pkg/cluster/secrets_test.go
+++ src/pkg/cluster/secrets_test.go
@@ -4,25 +4,27 @@
 package cluster
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 
 	"github.com/defenseunicorns/zarf/src/types"
 )
 
-func TestGenerateRegistryPullCreds(t *testing.T) {
-	c := &Cluster{}
+func TestGenerateRegistryPullCredsWithOutSvc(t *testing.T) {
+	c := &Cluster{Clientset: fake.NewSimpleClientset()}
+	ctx := context.Background()
 	ri := types.RegistryInfo{
-		PushUsername: "push-user",
-		PushPassword: "push-password",
 		PullUsername: "pull-user",
 		PullPassword: "pull-password",
 		Address:      "example.com",
 	}
-	secret := c.GenerateRegistryPullCreds("foo", "bar", ri)
+	secret, err := c.GenerateRegistryPullCreds(ctx, "foo", "bar", ri)
+	require.NoError(t, err)
 	expectedSecret := corev1.Secret{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
@@ -43,11 +45,59 @@ func TestGenerateRegistryPullCreds(t *testing.T) {
 	require.Equal(t, expectedSecret, *secret)
 }
 
+func TestGenerateRegistryPullCredsWithSvc(t *testing.T) {
+	c := &Cluster{Clientset: fake.NewSimpleClientset()}
+	ctx := context.Background()
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "good-service",
+			Namespace: "whatever",
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeNodePort,
+			Ports: []corev1.ServicePort{
+				{
+					NodePort: 30001,
+					Port:     3333,
+				},
+			},
+			ClusterIP: "10.11.12.13",
+		},
+	}
+
+	_, err := c.Clientset.CoreV1().Services("whatever").Create(ctx, svc, metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	ri := types.RegistryInfo{
+		PullUsername: "pull-user",
+		PullPassword: "pull-password",
+		Address:      "127.0.0.1:30001",
+	}
+	secret, err := c.GenerateRegistryPullCreds(ctx, "foo", "bar", ri)
+	require.NoError(t, err)
+	expectedSecret := corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Secret",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "bar",
+			Namespace: "foo",
+			Labels: map[string]string{
+				ZarfManagedByLabel: "zarf",
+			},
+		},
+		Type: corev1.SecretTypeDockerConfigJson,
+		Data: map[string][]byte{
+			".dockerconfigjson": []byte(`{"auths":{"10.11.12.13:3333":{"auth":"cHVsbC11c2VyOnB1bGwtcGFzc3dvcmQ="},"127.0.0.1:30001":{"auth":"cHVsbC11c2VyOnB1bGwtcGFzc3dvcmQ="}}}`),
+		},
+	}
+	require.Equal(t, expectedSecret, *secret)
+}
+
 func TestGenerateGitPullCreds(t *testing.T) {
 	c := &Cluster{}
 	gi := types.GitServerInfo{
-		PushUsername: "push-user",
-		PushPassword: "push-password",
 		PullUsername: "pull-user",
 		PullPassword: "pull-password",
 	}
