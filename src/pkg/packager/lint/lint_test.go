adeb342f172d90257e16fa7c456e2ef7a93d2ce1
Jason Washburn <jason.washburn@gmail.com>
2024-06-29 07:44:26 -0500

    test: extend lint tests to include cosign signature

--- src/pkg/packager/lint/lint_test.go
+++ src/pkg/packager/lint/lint_test.go
@@ -217,10 +217,12 @@ func TestValidateComponent(t *testing.T) {
 		t.Parallel()
 		unpinnedImage := "registry.com:9001/whatever/image:1.0.0"
 		badImage := "badimage:badimage@@sha256:3fbc632167424a6d997e74f5"
+		cosignedImage := "ghcr.io/stefanprodan/podinfo:sha256-57a654ace69ec02ba8973093b6a786faa15640575fbf0dbb603db55aca2ccec8.sig"
 		component := types.ZarfComponent{Images: []string{
 			unpinnedImage,
 			"busybox:latest@sha256:3fbc632167424a6d997e74f52b878d7cc478225cffac6bc977eedfe51c7f4e79",
 			badImage,
+			cosignedImage,
 		}}
 		findings := checkForUnpinnedImages(component, 0)
 		expected := []types.PackageFinding{
@@ -330,6 +332,11 @@ func TestValidateComponent(t *testing.T) {
 			},
 			{
 				input:    "busybox:###ZARF_PKG_TMPL_BUSYBOX_IMAGE###",
+				expected: true,
+				err:      nil,
+			},
+			{
+				input:    "ghcr.io/stefanprodan/podinfo:sha256-57a654ace69ec02ba8973093b6a786faa15640575fbf0dbb603db55aca2ccec8.sig",
 				expected: true,
 				err:      nil,
 			},
