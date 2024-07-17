62253cecc6b94cd2d42d79caacf57061dad8548a
Philip Laine <philip.laine@gmail.com>
2024-07-17 19:20:44 +0200

    Rename org name in image references
    Signed-off-by: Philip Laine <philip.laine@gmail.com>

--- src/pkg/zoci/utils.go
+++ src/pkg/zoci/utils.go
@@ -43,5 +43,5 @@ func ReferenceFromMetadata(registryLocation string, metadata *types.ZarfMetadata
 
 // GetInitPackageURL returns the URL for the init package for the given version.
 func GetInitPackageURL(version string) string {
-	return fmt.Sprintf("ghcr.io/defenseunicorns/packages/init:%s", version)
+	return fmt.Sprintf("ghcr.io/zarf-dev/packages/init:%s", version)
 }
