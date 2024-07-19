78c3354aae1876c8faf5ab35d7ae325624567157
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2024-07-19 12:24:15 -0400

    chore: move public test repo (#2739)
    Signed-off-by: Austin Abro <austinabro321@gmail.com>
    Signed-off-by: Tim Seagren <timseagren@defenseunicorns.com>

--- src/test/e2e/22_git_and_gitops_test.go
+++ src/test/e2e/22_git_and_gitops_test.go
@@ -77,7 +77,7 @@ func testGitServerReadOnly(ctx context.Context, t *testing.T, gitURL string) {
 	gitCfg := git.New(zarfState.GitServer)
 
 	// Get the repo as the readonly user
-	repoName := "zarf-public-test-2469062884"
+	repoName := "zarf-public-test-2363058019"
 	getRepoRequest, _ := http.NewRequest("GET", fmt.Sprintf("%s/api/v1/repos/%s/%s", gitURL, zarfState.GitServer.PushUsername, repoName), nil)
 	getRepoResponseBody, _, err := gitCfg.DoHTTPThings(getRepoRequest, types.ZarfGitReadUser, zarfState.GitServer.PullPassword)
 	require.NoError(t, err)
@@ -101,7 +101,7 @@ func testGitServerTagAndHash(ctx context.Context, t *testing.T, gitURL string) {
 	// Init the state variable
 	zarfState, err := c.LoadZarfState(ctx)
 	require.NoError(t, err, "Failed to load Zarf state")
-	repoName := "zarf-public-test-2469062884"
+	repoName := "zarf-public-test-2363058019"
 
 	gitCfg := git.New(zarfState.GitServer)
 
