b460bd3c6d6741edc21669db1b3286a3f1dbb5bb
schristoff-du <167717759+schristoff-du@users.noreply.github.com>
2024-07-22 23:47:55 -0600

    appease linter
    Signed-off-by: schristoff-du <167717759+schristoff-du@users.noreply.github.com>

--- src/test/helpers/helpers.go
+++ src/test/helpers/helpers.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2021-Present The Zarf Authors
 
-// Package provides global testing helper functions
+// Package helpers provides global testing helper functions
 package helpers
 
 import (
