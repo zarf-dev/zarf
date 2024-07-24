db5396ff34e6b35e21b12c36569d82d9a92ba3fe
schristoff-du <167717759+schristoff-du@users.noreply.github.com>
2024-07-24 17:13:01 -0600

    more like go-away-ci-lint
    Signed-off-by: schristoff-du <167717759+schristoff-du@users.noreply.github.com>

--- src/test/testutil/testutil.go
+++ src/test/testutil/testutil.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2021-Present The Zarf Authors
 
-// Package helpers provides global testing helper functions
+// Package testutil provides global testing helper functions
 package testutil
 
 import (
