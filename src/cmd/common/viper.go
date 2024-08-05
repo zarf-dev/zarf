ee60eb814816abff0f067c141691c6a0d185ad46
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2024-08-05 13:41:07 -0400

    fix: ignore config file not found errors (#2838)
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/cmd/common/viper.go
+++ src/cmd/common/viper.go
@@ -167,9 +167,11 @@ func printViperConfigUsed() {
 	if !vInitialized {
 		return
 	}
-	// Optional, so ignore file not found errors
-	var notFoundErr *viper.ConfigFileNotFoundError
-	if vConfigError != nil && !errors.As(vConfigError, &notFoundErr) {
+	var notFoundErr viper.ConfigFileNotFoundError
+	if errors.As(vConfigError, &notFoundErr) {
+		return
+	}
+	if vConfigError != nil {
 		message.WarnErrf(vConfigError, lang.CmdViperErrLoadingConfigFile, vConfigError.Error())
 		return
 	}
