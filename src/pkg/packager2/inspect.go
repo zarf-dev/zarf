a55e52f71e553f85a15a681ad6c9eb24c79abf09
schristoff <28318173+schristoff@users.noreply.github.com>
2024-09-12 13:15:28 -0600

    spdx license
    Signed-off-by: schristoff <28318173+schristoff@users.noreply.github.com>

--- src/pkg/packager2/inspect.go
+++ src/pkg/packager2/inspect.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2021-Present The Zarf Authors
+
 // Package packager2 contains functions for inspecting packages.
 package packager2
 
