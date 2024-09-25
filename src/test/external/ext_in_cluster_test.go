8e36039f2584a4a2996f2eedc8d71980ccccfc2b
Kit Patella <kit@defenseunicorns.com>
2024-09-25 15:19:03 -0700

    fix: does this work now?
    Signed-off-by: Kit Patella <kit@defenseunicorns.com>

--- src/test/external/ext_in_cluster_test.go
+++ src/test/external/ext_in_cluster_test.go
@@ -200,12 +200,8 @@ func (suite *ExtInClusterTestSuite) Test_1_Deploy() {
 	_, _, err = exec.CmdWithTesting(suite.T(), exec.PrintCfg(), zarfBinPath, "destroy", "--confirm")
 	suite.NoError(err, "unable to teardown zarf")
 
-	// FIXME(mkcp): This code always fails to clean up the tmpdir. We're passing it a directory with contents and we
-	//  			need os.RemoveAll(temp) instead. However, we're also getting what looks to be a bug with concurrent
-	//     			writers causing a subtest to panic when we call RemoveAll(). Fixing this would require removing the
-	// 				shared state.
-	// REVIEW(mkcp): Thinking we delete this and
-	err = os.RemoveAll(temp) //nolint:errcheck
+	// Cleanup tmpdir
+	err = os.RemoveAll(temp)
 	suite.NoError(err, "failed to clean up tempdir")
 }
 
