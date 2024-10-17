537ce044984008af72e6e5f481c1f67912dd5af6
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2024-10-17 11:45:40 -0400

    feat: move pkg oci to Zarf and log during health checks (#3106)
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/internal/healthchecks/healthchecks.go
+++ src/internal/healthchecks/healthchecks.go
@@ -6,15 +6,21 @@ package healthchecks
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
-	pkgkubernetes "github.com/defenseunicorns/pkg/kubernetes"
 	"github.com/zarf-dev/zarf/src/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/cli-utils/pkg/kstatus/polling/aggregator"
+	"sigs.k8s.io/cli-utils/pkg/kstatus/polling/collector"
+	"sigs.k8s.io/cli-utils/pkg/kstatus/polling/event"
+	"sigs.k8s.io/cli-utils/pkg/kstatus/status"
 	"sigs.k8s.io/cli-utils/pkg/kstatus/watcher"
 	"sigs.k8s.io/cli-utils/pkg/object"
 )
 
-// Run waits for a list of objects to be reconciled
+// Run waits for a list of Zarf healthchecks to reach a ready state.
 func Run(ctx context.Context, watcher watcher.StatusWatcher, healthChecks []v1alpha1.NamespacedObjectKindReference) error {
 	objs := []object.ObjMetadata{}
 	for _, hc := range healthChecks {
@@ -32,9 +38,99 @@ func Run(ctx context.Context, watcher watcher.StatusWatcher, healthChecks []v1al
 		}
 		objs = append(objs, obj)
 	}
-	err := pkgkubernetes.WaitForReady(ctx, watcher, objs)
+	err := WaitForReady(ctx, watcher, objs)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+// WaitForReadyRuntime waits for all of the objects to reach a ready state.
+func WaitForReadyRuntime(ctx context.Context, sw watcher.StatusWatcher, robjs []runtime.Object) error {
+	objs := []object.ObjMetadata{}
+	for _, robj := range robjs {
+		obj, err := object.RuntimeToObjMeta(robj)
+		if err != nil {
+			return err
+		}
+		objs = append(objs, obj)
+	}
+	return WaitForReady(ctx, sw, objs)
+}
+
+// WaitForReady waits for all of the objects to reach a ready state.
+func WaitForReady(ctx context.Context, sw watcher.StatusWatcher, objs []object.ObjMetadata) error {
+	cancelCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	eventCh := sw.Watch(cancelCtx, objs, watcher.Options{})
+	statusCollector := collector.NewResourceStatusCollector(objs)
+	done := statusCollector.ListenWithObserver(eventCh, collector.ObserverFunc(
+		func(statusCollector *collector.ResourceStatusCollector, _ event.Event) {
+			rss := []*event.ResourceStatus{}
+			for _, rs := range statusCollector.ResourceStatuses {
+				if rs == nil {
+					continue
+				}
+				rss = append(rss, rs)
+			}
+			desired := status.CurrentStatus
+			if aggregator.AggregateStatus(rss, desired) == desired {
+				cancel()
+				return
+			}
+		}),
+	)
+	<-done
+
+	if statusCollector.Error != nil {
+		return statusCollector.Error
+	}
+
+	// Only check parent context error, otherwise we would error when desired status is achieved.
+	if ctx.Err() != nil {
+		errs := []error{}
+		for _, id := range objs {
+			rs := statusCollector.ResourceStatuses[id]
+			switch rs.Status {
+			case status.CurrentStatus:
+			case status.NotFoundStatus:
+				errs = append(errs, fmt.Errorf("%s: %s not found", rs.Identifier.Name, rs.Identifier.GroupKind.Kind))
+			default:
+				errs = append(errs, fmt.Errorf("%s: %s not ready", rs.Identifier.Name, rs.Identifier.GroupKind.Kind))
+			}
+		}
+		errs = append(errs, ctx.Err())
+		return errors.Join(errs...)
+	}
+
+	return nil
+}
+
+// ImmediateWatcher should only be used for testing and returns the set status immediately.
+type ImmediateWatcher struct {
+	status status.Status
+}
+
+// NewImmediateWatcher returns a ImmediateWatcher.
+func NewImmediateWatcher(status status.Status) *ImmediateWatcher {
+	return &ImmediateWatcher{
+		status: status,
+	}
+}
+
+// Watch watches the given objects and immediately returns the configured status.
+func (w *ImmediateWatcher) Watch(_ context.Context, objs object.ObjMetadataSet, _ watcher.Options) <-chan event.Event {
+	eventCh := make(chan event.Event, len(objs))
+	for _, obj := range objs {
+		eventCh <- event.Event{
+			Type: event.ResourceUpdateEvent,
+			Resource: &event.ResourceStatus{
+				Identifier: obj,
+				Status:     w.status,
+			},
+		}
+	}
+	close(eventCh)
+	return eventCh
 }
--- src/pkg/cluster/cluster.go
+++ src/pkg/cluster/cluster.go
@@ -17,9 +17,11 @@ import (
 	"sigs.k8s.io/cli-utils/pkg/kstatus/watcher"
 
 	"github.com/avast/retry-go/v4"
-	pkgkubernetes "github.com/defenseunicorns/pkg/kubernetes"
 
 	"github.com/zarf-dev/zarf/src/pkg/message"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
 
 const (
@@ -76,11 +78,11 @@ func NewClusterWithWait(ctx context.Context) (*Cluster, error) {
 // NewCluster creates a new Cluster instance and validates connection to the cluster by fetching the Kubernetes version.
 func NewCluster() (*Cluster, error) {
 	clusterErr := errors.New("unable to connect to the cluster")
-	clientset, config, err := pkgkubernetes.ClientAndConfig()
+	clientset, config, err := ClientAndConfig()
 	if err != nil {
 		return nil, errors.Join(clusterErr, err)
 	}
-	watcher, err := pkgkubernetes.WatcherForConfig(config)
+	watcher, err := WatcherForConfig(config)
 	if err != nil {
 		return nil, errors.Join(clusterErr, err)
 	}
@@ -95,4 +97,37 @@ func NewCluster() (*Cluster, error) {
 		return nil, errors.Join(clusterErr, err)
 	}
 	return c, nil
+}
+
+// ClientAndConfig returns a Kubernetes client and the rest config used to configure the client.
+func ClientAndConfig() (kubernetes.Interface, *rest.Config, error) {
+	loader := clientcmd.NewDefaultClientConfigLoadingRules()
+	clientCfg := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loader, nil)
+	cfg, err := clientCfg.ClientConfig()
+	if err != nil {
+		return nil, nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, nil, err
+	}
+	return clientset, cfg, nil
+}
+
+// WatcherForConfig returns a status watcher for the give Kubernetes configuration.
+func WatcherForConfig(cfg *rest.Config) (watcher.StatusWatcher, error) {
+	dynamicClient, err := dynamic.NewForConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+	httpClient, err := rest.HTTPClientFor(cfg)
+	if err != nil {
+		return nil, err
+	}
+	restMapper, err := apiutil.NewDynamicRESTMapper(cfg, httpClient)
+	if err != nil {
+		return nil, err
+	}
+	sw := watcher.NewDefaultStatusWatcher(dynamicClient, restMapper)
+	return sw, nil
 }
