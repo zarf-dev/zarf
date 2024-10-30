51e74e50dd47b873f7a77a1bd583832e6ab0020e
Austin Abro <AustinAbro321@gmail.com>
2024-10-30 14:08:28 +0000

    test if flakes
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/pkg/packager/actions/actions.go
+++ src/pkg/packager/actions/actions.go
@@ -302,6 +302,7 @@ func actionRun(ctx context.Context, cfg v1alpha1.ZarfComponentActionDefaults, cm
 	if !cfg.Mute {
 		execCfg.Stdout = spinner
 		execCfg.Stderr = spinner
+
 		if logger.Enabled(ctx) {
 			execCfg.Print = true
 		}
