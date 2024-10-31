1492ec741bb84580bd773a3c44ada5b28cf653b5
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2024-10-31 11:59:40 -0400

    feat: expose Helm skip schema validation option in Zarf schema (#3165)
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/internal/packager/helm/chart.go
+++ src/internal/packager/helm/chart.go
@@ -296,6 +296,8 @@ func (h *Helm) installChart(ctx context.Context, postRender *renderer) (*release
 	// Must be unique per-namespace and < 53 characters. @todo: restrict helm loadedChart name to this.
 	client.ReleaseName = h.chart.ReleaseName
 
+	client.SkipSchemaValidation = !h.chart.ShouldRunSchemaValidation()
+
 	// Namespace must be specified.
 	client.Namespace = h.chart.Namespace
 
@@ -328,6 +330,8 @@ func (h *Helm) upgradeChart(ctx context.Context, lastRelease *release.Release, p
 	client.Wait = !h.chart.NoWait
 
 	client.SkipCRDs = true
+
+	client.SkipSchemaValidation = !h.chart.ShouldRunSchemaValidation()
 
 	// Namespace must be specified.
 	client.Namespace = h.chart.Namespace
