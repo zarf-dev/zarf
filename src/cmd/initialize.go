e0c7bd63ed299793e194f954db98b3d21a24110b
Austin Abro <AustinAbro321@gmail.com>
2024-11-04 19:42:47 +0000

    improve info message
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/cmd/initialize.go
+++ src/cmd/initialize.go
@@ -133,7 +133,7 @@ func downloadInitPackage(ctx context.Context, cacheDirectory string) (string, er
 	// Give the user the choice to download the init-package and note that this does require an internet connection
 	message.Question(fmt.Sprintf(lang.CmdInitPullAsk, url))
 	message.Note(lang.CmdInitPullNote)
-	l.Info("the init package was not found locally, but can be pulled from", "url", fmt.Sprintf("oci://%s", url))
+	l.Info("the init package was not found locally, but can be pulled in connected environments", "url", fmt.Sprintf("oci://%s", url))
 
 	var confirmDownload bool
 	prompt := &survey.Confirm{
