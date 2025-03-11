5fcdc5726a165b9111c35216f035c2a1260fc618
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-03-11 23:16:36 +0000

    wip: t.CleanUp and debugging test
    Signed-off-by: Brandt Keller <brandt.keller@defenseunicorns.com>

--- src/test/e2e/38_ephemeral_container_test.go
+++ src/test/e2e/38_ephemeral_container_test.go
@@ -28,17 +28,19 @@ func TestEphemeralContainers(t *testing.T) {
 	stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--confirm")
 	require.NoError(t, err, stdOut, stdErr)
 
+	t.Cleanup(func() {
+		// cleanup
+		stdOut, stdErr, err = e2e.Zarf(t, "package", "remove", "basic-pod", "--confirm")
+		require.NoError(t, err, stdOut, stdErr)
+	})
+
 	// using a pod the package deploys - run a kubectl debug command
-	stdOut, stdErr, err = e2e.Kubectl(t, "debug", "test-pod", "-n", "test", "--image=busybox:1.36")
+	stdOut, stdErr, err = e2e.Kubectl(t, "debug", "test-pod", "-n", "test", "--image=busybox:1.36", "--profile", "general")
 	require.NoError(t, err, stdOut, stdErr)
 
 	// get the pod and inspect the image used by the ephemeral container
 	// it should have been mutated
 	podStdOut, _, err := e2e.Kubectl(t, "get", "pod", "test-pod", "-n", "test", "-o", "jsonpath={.status.ephemeralContainerStatuses[*].image}")
 	require.NoError(t, err)
-	require.Contains(t, podStdOut, "127.0.0.1:31337/library/busybox:1.36-zarf-")
-
-	// cleanup
-	stdOut, stdErr, err = e2e.Zarf(t, "package", "remove", "basic-pod", "--confirm")
-	require.NoError(t, err, stdOut, stdErr)
+	require.Contains(t, podStdOut, "127.0.0.1:31999/library/busybox:1.36-zarf-")
 }
