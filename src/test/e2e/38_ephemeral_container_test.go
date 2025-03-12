d3c848b21e6c00c1d5a1f0fe0aeeed84e006f9ea
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-03-12 16:53:49 +0000

    fix(e2e): update test to resolve race
    Signed-off-by: Brandt Keller <brandt.keller@defenseunicorns.com>

--- src/test/e2e/38_ephemeral_container_test.go
+++ src/test/e2e/38_ephemeral_container_test.go
@@ -32,14 +32,14 @@ func TestEphemeralContainers(t *testing.T) {
 	stdOut, stdErr, err = e2e.Kubectl(t, "debug", "test-pod", "-n", "test", "--image=busybox:1.36", "--profile", "general")
 	require.NoError(t, err, stdOut, stdErr)
 
-	// get the pod and inspect the image used by the ephemeral container
-	// it should have been mutated
+	// verify the ephemeral container was mutated
 	podStdOut, _, err := e2e.Kubectl(t, "get", "pod", "test-pod", "-n", "test", "-o", "jsonpath={.status.ephemeralContainerStatuses[*].image}")
+	t.Log("Ephemeral Container: ", podStdOut)
 	require.NoError(t, err)
 	require.Contains(t, podStdOut, "127.0.0.1:31337/library/busybox:1.36-zarf-")
 
+	// cleanup - should perform cleanup in the event of pass or fail - separate from defer or direct use
 	t.Cleanup(func() {
-		// cleanup
 		stdOut, stdErr, err = e2e.Zarf(t, "package", "remove", "basic-pod", "--confirm")
 		require.NoError(t, err, stdOut, stdErr)
 	})
