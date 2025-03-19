37f90f9624d5a0d48003d93640f9c4b342a3fc41
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-03-19 20:45:09 +0000

    fix: update wait to simplify / use ghcr image
    Signed-off-by: Brandt Keller <brandt.keller@defenseunicorns.com>

--- src/test/e2e/38_ephemeral_container_test.go
+++ src/test/e2e/38_ephemeral_container_test.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"path/filepath"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -35,44 +34,16 @@ func TestEphemeralContainers(t *testing.T) {
 	require.NoError(t, err, stdOut, stdErr)
 
 	// using a pod the package deploys - run a kubectl debug command
-	stdOut, stdErr, err = e2e.Kubectl(t, "debug", "test-pod", "-n", "test", "--image=busybox:1.36", "--profile", "general")
+	stdOut, stdErr, err = e2e.Kubectl(t, "debug", "test-pod", "-n", "test", "--image=ghcr.io/zarf-dev/images/alpine:3.21.3", "--profile", "general")
 	require.NoError(t, err, stdOut, stdErr)
 
-	resultCh := make(chan string)
-	defer close(resultCh)
-	timeoutCh := time.After(10 * time.Second)
-	go func() {
-		ticker := time.NewTicker(2 * time.Second)
-		defer ticker.Stop()
+	// wait for the ephemeralContainer to exist - as it need to traverse mutation/admission
+	stdOut, stdErr, err = e2e.Kubectl(t, "wait", "--namespace=test", "--for=jsonpath={.status.ephemeralContainerStatuses[*].image}=127.0.0.1:31337/zarf-dev/images/alpine:3.21.3-zarf-1792331847", "pod/test-pod", "--timeout=10s")
+	require.NoError(t, err, stdOut, stdErr)
 
-		for range ticker.C {
-			podStdOut, _, err := e2e.Kubectl(t, "get", "pod", "test-pod", "-n", "test", "-o", "jsonpath={.status.ephemeralContainerStatuses[*].image}")
-			if err != nil {
-				// If there's an error, log it but don't fail immediately
-				t.Log("Error checking ephemeral container status:", err)
-				continue
-			}
-
-			if podStdOut != "" {
-				t.Log("Ephemeral container detected!", podStdOut)
-				resultCh <- podStdOut
-				return
-			}
-
-			t.Log("Waiting for ephemeral...")
-		}
-	}()
-
-	var ephemeralContainer string
-
-	select {
-	case ephemeralContainer = <-resultCh:
-		t.Log("Ephemeral Container: ", ephemeralContainer)
-	case <-timeoutCh:
-		t.Log("Timeout reached! Ephemeral container not found.")
-		t.Fail()
-	}
+	podStdOut, stdErr, err := e2e.Kubectl(t, "get", "pod", "test-pod", "-n", "test", "-o", "jsonpath={.status.ephemeralContainerStatuses[*].image}")
+	require.NoError(t, err, podStdOut, stdErr)
 
 	// Ensure the image used contains the internal zarf registry (IE mutated)
-	require.Contains(t, ephemeralContainer, "127.0.0.1:31337/library/busybox:1.36-zarf-")
+	require.Contains(t, podStdOut, "127.0.0.1:31337/zarf-dev/images/alpine:3.21.3-zarf-1792331847")
 }
