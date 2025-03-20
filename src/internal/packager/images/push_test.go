d3513dbcaf147c659a909706abfa37e1544ee3ce
Austin Abro <AustinAbro321@gmail.com>
2025-03-20 13:28:01 +0000

    crane to ORAS
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/internal/packager/images/push_test.go
+++ src/internal/packager/images/push_test.go
@@ -24,10 +24,11 @@ func TestPush(t *testing.T) {
 	}{
 		{
 			name:            "push local images oras",
+			// This OCI format directory was created by building the package at src/test/packages/39-crane-to-oras with the ORAS implementation
 			SourceDirectory: "testdata/oras-oci-layout/images",
 			imageNames: []string{
 				"local-test:1.0.0",
-				"hello-world:latest",
+				"ghcr.io/zarf-dev/images/hello-world:latest",
 				"ghcr.io/stefanprodan/podinfo:sha256-57a654ace69ec02ba8973093b6a786faa15640575fbf0dbb603db55aca2ccec8.sig",
 				"ghcr.io/stefanprodan/charts/podinfo:6.4.0",
 				"hello-world@sha256:03b62250a3cb1abd125271d393fc08bf0cc713391eda6b57c02d1ef85efcc25c",
@@ -35,10 +36,11 @@ func TestPush(t *testing.T) {
 		},
 		{
 			name:            "push local images crane",
+			// This OCI format directory was created by building the package at src/test/packages/39-crane-to-oras with the Crane implementation (Zarf v0.49.1)
 			SourceDirectory: "testdata/crane-oci-layout/images",
 			imageNames: []string{
 				"local-test:1.0.0",
-				"hello-world:latest",
+				"ghcr.io/zarf-dev/images/hello-world:latest",
 				"ghcr.io/stefanprodan/podinfo:sha256-57a654ace69ec02ba8973093b6a786faa15640575fbf0dbb603db55aca2ccec8.sig",
 				"ghcr.io/stefanprodan/charts/podinfo:6.4.0",
 				"hello-world@sha256:03b62250a3cb1abd125271d393fc08bf0cc713391eda6b57c02d1ef85efcc25c",
