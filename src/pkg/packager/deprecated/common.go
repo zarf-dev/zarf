322932cc2ff02b887494f57048cc2630b0c5bb5f
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2025-04-08 13:44:26 -0400

    chore(deps): update to distribution/distribution v3 (#3642)
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>
    Signed-off-by: NevinAragam <nevin.aragam@gmail.com>

--- src/pkg/packager/deprecated/common.go
+++ src/pkg/packager/deprecated/common.go
@@ -28,7 +28,7 @@ type BreakingChange struct {
 // String returns the string representation of the BreakingChange.
 func (bc BreakingChange) String() string {
 	return fmt.Sprintf("%s\n\n  - %s\n    %s\n",
-		pterm.Bold.Sprintf(bc.title),
+		pterm.Bold.Sprint(bc.title),
 		pterm.Bold.Sprint("Mitigation:"),
 		strings.ReplaceAll(message.Paragraphn(96, "%s", pterm.FgLightCyan.Sprint(bc.mitigation)), "\n", "\n    "),
 	)
@@ -117,8 +117,8 @@ func PrintBreakingChanges(w io.Writer, deployedZarfVersion, cliVersion string) e
 	// Print information about the versions
 	format := pterm.FgYellow.Sprint("CLI version ") + "%s" + pterm.FgYellow.Sprint(" is being used to deploy to a cluster that was initialized with ") +
 		"%s" + pterm.FgYellow.Sprint(". Between these versions there are the following breaking changes to consider:")
-	cliVersion = pterm.Bold.Sprintf(cliVersion)
-	deployedZarfVersion = pterm.Bold.Sprintf(deployedZarfVersion)
+	cliVersion = pterm.Bold.Sprint(cliVersion)
+	deployedZarfVersion = pterm.Bold.Sprint(deployedZarfVersion)
 	message.Warnf(format, cliVersion, deployedZarfVersion)
 
 	// Print each applicable breaking change
