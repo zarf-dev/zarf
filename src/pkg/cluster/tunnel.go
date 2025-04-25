cf7edc4988a7330d3deaeed73a81fb4a2ed8ed0d
Harry Randazzo <harry@razzle.cloud>
2025-04-25 00:24:26 -0500

    Revert "stash this for later"
    This reverts commit 8db379ab2d93829732e6bae546e2587953fa3e02.
    
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- src/pkg/cluster/tunnel.go
+++ src/pkg/cluster/tunnel.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -405,6 +406,17 @@ func (tunnel *Tunnel) Close() {
 	close(tunnel.stopChan)
 }
 
+type slogWriter struct {
+	l      *slog.Logger
+	prefix string
+}
+
+func (w *slogWriter) Write(p []byte) (n int, err error) {
+	trimmed := strings.TrimRight(string(p), "\n")
+	w.l.Debug(w.prefix + trimmed)
+	return len(w.prefix) + len(trimmed), nil
+}
+
 // establish opens a tunnel to a kubernetes resource, as specified by the provided tunnel struct.
 func (tunnel *Tunnel) establish(ctx context.Context) (string, error) {
 	var err error
@@ -462,6 +474,9 @@ func (tunnel *Tunnel) establish(ctx context.Context) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("unable to create the dialer %w", err)
 	}
+
+	// out := &slogWriter{l, fmt.Sprintf("tunnel output for %s: ", tunnel.Endpoint())}
+	// errOut := &slogWriter{l, fmt.Sprintf("tunnel error output for %s: ", tunnel.Endpoint())}
 
 	// Construct a new PortForwarder struct that manages the instructed port forward tunnel.
 	ports := []string{fmt.Sprintf("%d:%d", localPort, tunnel.remotePort)}
