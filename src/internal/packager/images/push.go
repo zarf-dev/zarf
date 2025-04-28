b1fdd16fbfb79042d4c5ce9138b3eda185597b7f
Harry Randazzo <harry@razzle.cloud>
2025-04-28 15:35:21 -0500

    remove debugging
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- src/internal/packager/images/push.go
+++ src/internal/packager/images/push.go
@@ -222,10 +222,6 @@ func copyImage(ctx context.Context, src *oci.Store, remote oras.Target, srcName 
 	copyOpts := oras.DefaultCopyOptions
 	copyOpts.Concurrency = concurrency
 	copyOpts.WithTargetPlatform(desc.Platform)
-	copyOpts.PreCopy = func(_ context.Context, desc ocispec.Descriptor) error {
-		logger.From(ctx).Debug("pushing", "blob", desc)
-		return nil
-	}
 	_, err = oras.Copy(ctx, src, srcName, remote, dstName, copyOpts)
 	if err != nil {
 		return fmt.Errorf("failed to push image %s: %w", srcName, err)
