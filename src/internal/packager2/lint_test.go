d2213309931191121e501bf3317b870245fbcc86
Austin Abro <AustinAbro321@gmail.com>
2025-05-09 19:05:49 +0000

    make cache work in windows
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/internal/packager2/lint_test.go
+++ src/internal/packager2/lint_test.go
@@ -4,6 +4,7 @@ package packager2
 
 import (
 	"context"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -21,7 +22,12 @@ func TestLintPackageWithImports(t *testing.T) {
 	}
 	ctx := context.Background()
 	// TODO: Make cache optional for skeleton pulls https://github.com/zarf-dev/zarf/issues/3774
-	config.CommonOptions.CachePath = t.TempDir()
+	cacheDir, err := os.MkdirTemp("", "zarf-")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(cacheDir))
+	})
+	config.CommonOptions.CachePath = cacheDir
 	findings := []lint.PackageFinding{
 		// Test local import lints properly
 		{
@@ -45,7 +51,7 @@ func TestLintPackageWithImports(t *testing.T) {
 			Severity:    lint.SevWarn,
 		},
 	}
-	err := Lint(ctx, filepath.Join("testdata", "lint-with-imports"), "good-flavor", setVariables)
+	err = Lint(ctx, filepath.Join("testdata", "lint-with-imports"), "good-flavor", setVariables)
 	var lintErr *lint.LintError
 	require.ErrorAs(t, err, &lintErr)
 	require.ElementsMatch(t, findings, lintErr.Findings)
