f08f736646a8a51f8da9fc1cde0746c4e9062f99
Austin Abro <AustinAbro321@gmail.com>
2025-06-09 19:09:01 +0000

    deleting deadcode
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/pkg/interactive/prompt.go
+++ src/pkg/interactive/prompt.go
@@ -13,20 +13,6 @@ import (
 	"github.com/zarf-dev/zarf/src/pkg/logger"
 )
 
-// PromptSigPassword prompts the user for the password to their private key
-func PromptSigPassword() ([]byte, error) {
-	var password string
-
-	prompt := &survey.Password{
-		Message: "Private key password (empty for no password): ",
-	}
-	err := survey.AskOne(prompt, &password)
-	if err != nil {
-		return []byte{}, err
-	}
-	return []byte(password), nil
-}
-
 // PromptVariable prompts the user for a value for a variable
 func PromptVariable(ctx context.Context, variable v1alpha1.InteractiveVariable) (string, error) {
 	if variable.Description != "" {
