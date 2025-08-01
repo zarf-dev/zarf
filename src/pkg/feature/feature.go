9b912e060147c1eca527913c7713754ec99358ad
Kit Patella <kit@defenseunicorns.com>
2025-08-01 11:04:00 -0700

    implement feature package from ZEP-0035. begin tests
    Signed-off-by: Kit Patella <kit@defenseunicorns.com>

---
+++ src/pkg/feature/feature.go
@@ -0,0 +1,189 @@
+package feature
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+// Atoms wrapping the default and user-set collections of Features. These do not require mutexes because they will each
+// be modified very few times, with the public API for each ensuring there only an empty set can be written to.
+// Alternatively these could be sync.Maps but they're not written to enough where it matters.
+// e.g. These atoms are write once, ready many (WORM).
+var defaultFeatures = atomic.Value{} // map[Name]Feature
+var userFeatures = atomic.Value{}    // map[Name]Feature
+
+// Mode describes the two different ways that Features can be set. These are used as keys for All()'s return map.
+type Mode string
+
+var DefaultMode Mode = "default"
+var UserMode Mode = "user"
+
+type Name string
+type Description string
+type Enabled bool
+type Since string
+type Until string
+
+type Stage string
+
+var (
+	Alpha      Stage = "alpha"
+	Beta       Stage = "beta"
+	GA         Stage = "ga"
+	Deprecated Stage = "deprecated"
+)
+
+type Feature struct {
+	// Name stores the name of the feature flag.
+	Name
+	// Description describes how the flag is used.
+	Description
+	// Enabled describes whether a feature is explicitly enabled or disabled. A feature that does not exist in any set
+	// is considered disabled.
+	Enabled
+	// Since is the version a feature is first introduced in alpha stage.
+	Since
+	// Until is the version when a deprecated feature is fully removed. Historical versions included.
+	Until
+	// Stage describes what level of done-ness a feature is. TODO describe this better
+	Stage
+}
+
+// IsEnabled allows users to optimistically check for a feature. Useful for control flow. Any user-enabled or disabled
+// features take precedence over the default setting.
+func IsEnabled(name Name) bool {
+	// Check user features first
+	fUser, err := GetUser(name)
+	if err == nil {
+		return bool(fUser.Enabled)
+	}
+
+	// Fallback to default features
+	fDefault, err := GetDefault(name)
+	if err == nil {
+		return bool(fDefault.Enabled)
+	}
+
+	// Feature does not exist
+	return false
+}
+
+// Set takes a slice of one or many flags, inserting the features onto user-configured features. If a feature name is
+// provided that is already a part of the set, then Set will return an error.
+// TODO: Should we allow users to call this multiple times even if we don't allow them to overwrite features?
+func Set(features []Feature) error {
+	// Ensure user features haven't been set
+	m := AllUser()
+	if m != nil && len(m) > 0 {
+		return fmt.Errorf("user features have already been set")
+	}
+	userFeatures.Store(featuresToMap(features))
+	return nil
+}
+
+// SetDefault takes a slice of one or many flags, inserting the features onto the default feature set. If
+// a feature name is provided that is already a part of the set, then SetDefault will return an error. This function
+// can only be called once.
+func SetDefault(features []Feature) error {
+	// Ensure default features haven't been set
+	m := AllDefault()
+	if m != nil && len(m) > 0 {
+		return fmt.Errorf("default features have already been set")
+	}
+	defaultFeatures.Store(featuresToMap(features))
+	return nil
+}
+
+// Get takes a flag Name and returns the Feature struct. If the doesn't exist then it will error. It will check both the
+// default set and the user set, and if a flag exists in both it will return the user data for it.
+func Get(name Name) (Feature, error) {
+	// Get from user set
+	fu, uErr := GetUser(name)
+	if uErr == nil {
+		return fu, nil
+	}
+
+	// Fallback to default set
+	fd, dErr := GetDefault(name)
+	if dErr == nil {
+		return fd, nil
+	}
+
+	// Feature not found in either set
+	return Feature{}, fmt.Errorf("feature not found: %s", name)
+}
+
+// GetDefault takes a flag Name and returns the Feature struct from the default set.
+func GetDefault(name Name) (Feature, error) {
+	m := defaultFeatures.Load().(map[Name]Feature)
+	f, ok := m[name]
+	if !ok {
+		return f, fmt.Errorf("feature not found: %s", name)
+	}
+	return f, nil
+}
+
+// GetUser takes a flag Name and returns the Feature struct from the user set.
+func GetUser(name Name) (Feature, error) {
+	m := userFeatures.Load().(map[Name]Feature)
+	f, ok := m[name]
+	if !ok {
+		return f, fmt.Errorf("feature not found: %s", name)
+	}
+	return f, nil
+}
+
+// All returns all flags from both Default and User.
+func All() map[Mode]map[Name]Feature {
+	m := make(map[Mode]map[Name]Feature)
+	m[DefaultMode] = defaultFeatures.Load().(map[Name]Feature)
+	m[UserMode] = userFeatures.Load().(map[Name]Feature)
+	return m
+}
+
+// AllDefault returns all features with from the Default set for this version of Zarf.
+func AllDefault() map[Name]Feature {
+	return defaultFeatures.Load().(map[Name]Feature)
+}
+
+// AllUser returns all features that have been enabled by users.
+func AllUser() map[Name]Feature {
+	return userFeatures.Load().(map[Name]Feature)
+}
+
+func featuresToMap(fs []Feature) map[Name]Feature {
+	m := make(map[Name]Feature)
+	for _, f := range fs {
+		m[f.Name] = f
+	}
+	return m
+}
+
+func init() {
+	features := []Feature{
+		// FIXME: Example feature
+		// Owner: @zarf-maintainers
+		{
+			Name:        "foo",
+			Description: "foo does the thing of course",
+			Enabled:     true,
+			Since:       "v0.60.0",
+			Stage:       GA,
+		},
+		// FIXME: Example feature
+		// Owner: @zarf-maintainers
+		{
+			Name:        "bar",
+			Description: "bar was honestly always a bit buggy, use baz instead",
+			Enabled:     false,
+			Since:       "v0.52.0",
+			Until:       "v0.62.0",
+			Stage:       Deprecated,
+		},
+	}
+
+	err := SetDefault(features)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ src/pkg/feature/feature_test.go
@@ -0,0 +1,158 @@
+package feature
+
+import (
+	"testing"
+)
+
+func TestIsEnabled(t *testing.T) {
+	tt := []struct {
+		name     string
+		user     []Feature
+		defaults []Feature
+		fName    Name
+		expect   bool
+	}{
+		{
+			name:   "neither user nor default enabled",
+			expect: false,
+		},
+		{
+			name: "user enabled, default empty",
+			user: []Feature{
+				{Name: "foo", Enabled: true},
+			},
+			fName:  "foo",
+			expect: true,
+		},
+		{
+			name: "defaults enabled, user empty",
+			defaults: []Feature{
+				{Name: "foo", Enabled: true},
+			},
+			fName:  "foo",
+			expect: true,
+		},
+		{
+			name: "defaults enabled, user enabled",
+			defaults: []Feature{
+				{Name: "foo", Enabled: true},
+			},
+			user: []Feature{
+				{Name: "foo", Enabled: true},
+			},
+			fName:  "foo",
+			expect: true,
+		},
+		{
+			name: "defaults disabled, user enabled",
+			defaults: []Feature{
+				{Name: "foo", Enabled: false},
+			},
+			user: []Feature{
+				{Name: "foo", Enabled: true},
+			},
+			fName:  "foo",
+			expect: true,
+		},
+		{
+			name: "defaults enabled, user disabled",
+			defaults: []Feature{
+				{Name: "foo", Enabled: true},
+			},
+			user: []Feature{
+				{Name: "foo", Enabled: false},
+			},
+			fName:  "foo",
+			expect: false,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			// TODO:
+			// Preload atoms
+			// Run tests
+			// Clear atoms
+			t.Skip()
+		})
+	}
+}
+
+func TestSet(t *testing.T) {
+	tt := []struct {
+		name string
+		user []Feature
+	}{
+		{
+			name: "can set no features",
+		},
+		{
+			name: "can set one feature",
+		},
+		{
+			name: "can set many features",
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			// require.NoError(t, )
+			t.Skip()
+		})
+	}
+}
+
+func TestSet_Errors(t *testing.T) {
+	tt := []struct {
+		name   string
+		user   []Feature
+		expect error
+	}{
+		{
+			name: "setting one feature multiple times errors",
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			// require.NoError(t, )
+			t.Skip()
+		})
+	}
+}
+
+func TestSetDefault(t *testing.T) {
+	tt := []struct {
+		name     string
+		user     []Feature
+		defaults []Feature
+	}{
+		{},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Skip()
+		})
+	}
+}
+
+func TestGet(t *testing.T) {
+	t.Skip()
+}
+
+func TestGetUser(t *testing.T) {
+	t.Skip()
+}
+
+func TestGetDefault(t *testing.T) {
+	t.Skip()
+}
+
+func TestAll(t *testing.T) {
+	t.Skip()
+}
+
+func TestAllUser(t *testing.T) {
+	t.Skip()
+}
+
+func TestAllDefault(t *testing.T) {
+	t.Skip()
+}
