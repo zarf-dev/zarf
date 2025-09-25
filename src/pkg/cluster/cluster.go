777a560ee7be9b079f3063d69c6d8ecc8e277e9c
Austin Abro <austinabro321@gmail.com>
2025-09-25 13:56:18 -0400

    fix load
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/pkg/cluster/cluster.go
+++ src/pkg/cluster/cluster.go
@@ -313,6 +313,8 @@ func (c *Cluster) LoadState(ctx context.Context) (*state.State, error) {
 	if err != nil {
 		return nil, fmt.Errorf("%w: %w", stateErr, err)
 	}
+	// If registry mode is not set then this is an old Zarf cluster and we can assume it's using the nodeport method
+	s.RegistryInfo.RegistryMode = state.RegistryModeNodePort
 	state.DebugPrint(ctx, s)
 	return s, nil
 }
