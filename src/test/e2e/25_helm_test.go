fa91034ae94b8735f6e0b2f6632651a8784a56ac
Austin Abro <austinabro321@gmail.com>
2025-09-26 09:19:54 -0400

    rollback to four
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/test/e2e/25_helm_test.go
+++ src/test/e2e/25_helm_test.go
@@ -185,7 +185,7 @@ func testHelmUninstallRollback(t *testing.T, tmpdir string) {
 	// Ensure that we rollback properly
 	helmOut, err = exec.Command("helm", "history", "-n", "dos-games", "zarf-f53a99d4a4dd9a3575bedf59cd42d48d751ae866", "--max", "1").Output()
 	require.NoError(t, err)
-	require.Contains(t, string(helmOut), "Rollback to 3")
+	require.Contains(t, string(helmOut), "Rollback to 4")
 
 	// Remove the package.
 	stdOut, stdErr, err = e2e.Zarf(t, "package", "remove", "dos-games", "--confirm")
