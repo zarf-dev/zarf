587f7bdcad33b4c9db3ceea5355f56fa8c7b8e17
Kit Patella <kit@defenseunicorns.com>
2025-09-29 13:10:53 -0700

    add simple e2e smoke test for values templating
    Signed-off-by: Kit Patella <kit@defenseunicorns.com>

---
+++ src/test/e2e/42_values_test.go
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2021-Present The Zarf Authors
+
+// Package test provides e2e tests for Zarf.
+package test
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestValues(t *testing.T) {
+	t.Log("E2E: Values")
+
+	// Don't run this test in appliance mode
+	if e2e.ApplianceMode {
+		return
+	}
+
+	src := filepath.Join("src", "test", "packages", "42-values")
+	tmpdir := t.TempDir()
+
+	// Create the package
+	stdOut, stdErr, err := e2e.Zarf(t, "package", "create", src, "-o", tmpdir, "--skip-sbom", "--confirm", "features=\"values=true\"")
+	require.NoError(t, err, stdOut, stdErr)
+
+	packageName := fmt.Sprintf("zarf-package-test-values-%s.tar.zst", e2e.Arch)
+	path := filepath.Join(tmpdir, packageName)
+
+	// Deploy the package
+	stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+
+	// Verify the configmap was templated with the default value
+	kubectlOut, _, err := e2e.Kubectl(t, "get", "configmap", "test-values-configmap", "-o", "jsonpath='{.data.value}'")
+	require.NoError(t, err, "unable to get configmap")
+	require.Contains(t, kubectlOut, "default-value")
+
+	// Remove the package
+	stdOut, stdErr, err = e2e.Zarf(t, "package", "remove", "test-values", "--confirm")
+	require.NoError(t, err, stdOut, stdErr)
+}
