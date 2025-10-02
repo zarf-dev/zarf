2c5318920a1414bb9eeaf109386177678fdfa537
Kit Patella <kit@defenseunicorns.com>
2025-10-02 09:52:33 -0700

    don't store the e2e example in json.json.etc and yaml.yaml.etc
    Signed-off-by: Kit Patella <kit@defenseunicorns.com>

--- src/test/e2e/42_values_test.go
+++ src/test/e2e/42_values_test.go
@@ -37,10 +37,10 @@ func TestValues(t *testing.T) {
 	// Verify the action configmap was templated with the action-set values
 	kubectlOut, _, err = e2e.Kubectl(t, "get", "configmap", "test-action-configmap", "-o", "jsonpath='{.data.json}'")
 	require.NoError(t, err, "unable to get action configmap")
-	require.Contains(t, kubectlOut, "json-value")
+	require.Contains(t, kubectlOut, "my-value")
 	kubectlOut, _, err = e2e.Kubectl(t, "get", "configmap", "test-action-configmap", "-o", "jsonpath='{.data.yaml}'")
 	require.NoError(t, err, "unable to get action configmap")
-	require.Contains(t, kubectlOut, "yaml-value")
+	require.Contains(t, kubectlOut, "my-value")
 
 	// Remove the package
 	stdOut, stdErr, err = e2e.Zarf(t, "package", "remove", "test-values", "--confirm")
