6401c08797c29138ffb72c79f4afedf62fefcf11
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2025-10-06 15:03:04 -0400

    fix: cleanup temp directory on pull and differential package loads (#4234)
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/pkg/packager/create.go
+++ src/pkg/packager/create.go
@@ -66,6 +66,9 @@ func Create(ctx context.Context, packagePath string, output string, opts CreateO
 		if err != nil {
 			return "", fmt.Errorf("failed to load differential package: %w", err)
 		}
+		if err := pkgLayout.Cleanup(); err != nil {
+			return "", err
+		}
 		differentialPkg = pkgLayout.Pkg
 	}
 
