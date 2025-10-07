60d1e5c79e6d6daf985de2a28a2465eccdf2696f
Austin Abro <austinabro321@gmail.com>
2025-10-07 14:07:26 -0400

    confirm
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/cmd/initialize.go
+++ src/cmd/initialize.go
@@ -41,7 +41,6 @@ type initOptions struct {
 	retries                 int
 	publicKeyPath           string
 	skipSignatureValidation bool
-	confirm                 bool
 	ociConcurrency          int
 }
 
@@ -68,7 +67,7 @@ func newInitCommand() *cobra.Command {
 	cmd.Flags().StringToStringVar(&o.setVariables, "set", v.GetStringMapString(VPkgDeploySet), lang.CmdInitFlagSet)
 
 	// Continue to require --confirm flag for init command to avoid accidental deployments
-	cmd.Flags().BoolVarP(&o.confirm, "confirm", "c", false, lang.CmdInitFlagConfirm)
+	cmd.Flags().BoolVarP(&config.CommonOptions.Confirm, "confirm", "c", false, lang.CmdInitFlagConfirm)
 	cmd.Flags().StringVar(&o.optionalComponents, "components", v.GetString(VInitComponents), lang.CmdInitFlagComponents)
 	cmd.Flags().StringVar(&o.storageClass, "storage-class", v.GetString(VInitStorageClass), lang.CmdInitFlagStorageClass)
 
@@ -207,7 +206,7 @@ func (o *initOptions) findInitPackage(ctx context.Context, initPackageName strin
 		return filepath.Join(absCachePath, initPackageName), nil
 	}
 
-	if o.confirm {
+	if config.CommonOptions.Confirm {
 		return "", lang.ErrInitNotFound
 	}
 
