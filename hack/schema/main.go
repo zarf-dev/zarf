309ecb552f04bb28a53a9d8505bb3029c67eb127
Austin Abro <austinabro321@gmail.com>
2025-10-15 09:57:17 -0400

    WIP, things are working
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- hack/schema/main.go
+++ hack/schema/main.go
@@ -14,66 +14,7 @@ import (
 	"github.com/zarf-dev/zarf/src/api/v1beta1"
 )
 
-// updateRefs recursively updates all $ref paths in a schema to use a namespace prefix
-func updateRefs(schema *jsonschema.Schema, prefix string) {
-	if schema == nil {
-		return
-	}
-
-	// Update the $ref if it points to a definition
-	if schema.Ref != "" && len(schema.Ref) > 8 && schema.Ref[:8] == "#/$defs/" {
-		defName := schema.Ref[8:]
-		schema.Ref = "#/$defs/" + prefix + defName
-	}
-
-	// Recursively update refs in nested schemas
-	for _, s := range schema.AllOf {
-		updateRefs(s, prefix)
-	}
-	for _, s := range schema.AnyOf {
-		updateRefs(s, prefix)
-	}
-	for _, s := range schema.OneOf {
-		updateRefs(s, prefix)
-	}
-	updateRefs(schema.Not, prefix)
-	updateRefs(schema.If, prefix)
-	updateRefs(schema.Then, prefix)
-	updateRefs(schema.Else, prefix)
-	updateRefs(schema.Items, prefix)
-	updateRefs(schema.Contains, prefix)
-	updateRefs(schema.AdditionalProperties, prefix)
-	updateRefs(schema.PropertyNames, prefix)
-
-	// Update refs in properties
-	if schema.Properties != nil {
-		for pair := schema.Properties.Oldest(); pair != nil; pair = pair.Next() {
-			updateRefs(pair.Value, prefix)
-		}
-	}
-
-	// Update refs in pattern properties
-	for _, s := range schema.PatternProperties {
-		updateRefs(s, prefix)
-	}
-
-	// Update refs in dependent schemas
-	for _, s := range schema.DependentSchemas {
-		updateRefs(s, prefix)
-	}
-
-	// Update refs in prefix items
-	for _, s := range schema.PrefixItems {
-		updateRefs(s, prefix)
-	}
-
-	// Update refs in definitions themselves
-	for _, s := range schema.Definitions {
-		updateRefs(s, prefix)
-	}
-}
-
-func genSchema() (string, error) {
+func genSchemas() (string, error) {
 	// AddGoComments breaks if called with a absolute path, so we move to the directory of the go executable
 	// then use a relative path to the package
 	_, filename, _, ok := runtime.Caller(1)
@@ -85,16 +26,16 @@ func genSchema() (string, error) {
 		return "", err
 	}
 
-	// Generate v1alpha1 schema
-	reflectorV1Alpha1 := jsonschema.Reflector{ExpandedStruct: true}
+	// Generate v1alpha1 schema with DoNotReference to inline all types
+	reflectorV1Alpha1 := jsonschema.Reflector{DoNotReference: true, ExpandedStruct: true}
 	typePackagePathV1Alpha1 := filepath.Join("..", "..", "src", "api", "v1alpha1")
 	if err := reflectorV1Alpha1.AddGoComments("github.com/zarf-dev/zarf/hack/schema", typePackagePathV1Alpha1); err != nil {
 		return "", err
 	}
 	schemaV1Alpha1 := reflectorV1Alpha1.Reflect(&v1alpha1.ZarfPackage{})
 
-	// Generate v1beta1 schema
-	reflectorV1Beta1 := jsonschema.Reflector{ExpandedStruct: true}
+	// Generate v1beta1 schema with DoNotReference to inline all types
+	reflectorV1Beta1 := jsonschema.Reflector{DoNotReference: true, ExpandedStruct: true}
 	typePackagePathV1Beta1 := filepath.Join("..", "..", "src", "api", "v1beta1")
 	if err := reflectorV1Beta1.AddGoComments("github.com/zarf-dev/zarf/hack/schema", typePackagePathV1Beta1); err != nil {
 		return "", err
@@ -112,48 +53,20 @@ func genSchema() (string, error) {
 		Const: "zarf.dev/v1beta1",
 	})
 
-	// Namespace and merge $defs from both schemas to avoid conflicts
-	mergedDefs := make(jsonschema.Definitions)
-
-	// Add v1alpha1 definitions with namespace prefix
-	if schemaV1Alpha1.Definitions != nil {
-		for key, value := range schemaV1Alpha1.Definitions {
-			mergedDefs["v1alpha1-"+key] = value
-		}
-	}
-
-	// Add v1beta1 definitions with namespace prefix
-	if schemaV1Beta1.Definitions != nil {
-		for key, value := range schemaV1Beta1.Definitions {
-			mergedDefs["v1beta1-"+key] = value
-		}
-	}
-
-	// Update $refs in v1alpha1 schema to use namespaced definitions
-	updateRefs(schemaV1Alpha1, "v1alpha1-")
-
-	// Update $refs in v1beta1 schema to use namespaced definitions
-	updateRefs(schemaV1Beta1, "v1beta1-")
-
 	// Create a combined schema using if/then/else based on apiVersion
 	combinedSchema := &jsonschema.Schema{
 		Version:     "https://json-schema.org/draft/2020-12/schema",
 		Title:       "Zarf Package Schema",
 		Description: "Schema for Zarf packages supporting multiple API versions",
-		Definitions: mergedDefs,
-		AllOf: []*jsonschema.Schema{
-			{
-				If: &jsonschema.Schema{
-					Properties: propsV1Alpha1,
-				},
-				Then: schemaV1Alpha1,
+		If: &jsonschema.Schema{
+			Properties: propsV1Alpha1,
+		},
+		Then: schemaV1Alpha1,
+		Else: &jsonschema.Schema{
+			If: &jsonschema.Schema{
+				Properties: propsV1Beta1,
 			},
-			{
-				If: &jsonschema.Schema{
-					Properties: propsV1Beta1,
-				},
-				Then: schemaV1Beta1,
-			},
+			Then: schemaV1Beta1,
 		},
 	}
 
@@ -165,7 +78,7 @@ func genSchema() (string, error) {
 }
 
 func main() {
-	schema, err := genSchema()
+	schema, err := genSchemas()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
