e6c6a6faa4c8b9bd8a4c3746b008712b9f065533
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-10-18 03:50:21 +0000

    feat(helm): update FindAnnotatedImagesForChart to support dependency annotations
    Signed-off-by: Brandt Keller <brandt.keller@defenseunicorns.com>

--- src/internal/packager/helm/images.go
+++ src/internal/packager/helm/images.go
@@ -6,12 +6,13 @@ package helm
 import (
 	"github.com/defenseunicorns/pkg/helpers/v2"
 	"github.com/goccy/go-yaml"
+	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/chartutil"
 )
 
-// ChartImages captures the structure of the helm.sh/images annotation within the Helm chart.
-type ChartImages []struct {
+// ChartImage represents a single image entry in the helm.sh/images annotation.
+type ChartImage struct {
 	// Name of the image.
 	Name string `yaml:"name"`
 	// Image with tag.
@@ -22,6 +23,9 @@ type ChartImages []struct {
 	Dependency string `yaml:"dependency"`
 }
 
+// ChartImages captures the structure of the helm.sh/images annotation within the Helm chart.
+type ChartImages []ChartImage
+
 // FindAnnotatedImagesForChart attempts to parse any image annotations found in a chart archive or directory.
 func FindAnnotatedImagesForChart(chartPath string, values chartutil.Values) (images []string, err error) {
 	// Load a new chart.
@@ -31,27 +35,59 @@ func FindAnnotatedImagesForChart(chartPath string, values chartutil.Values) (ima
 	}
 	values = helpers.MergeMapRecursive(chart.Values, values)
 
-	imageAnnotation := chart.Metadata.Annotations["helm.sh/images"]
+	// Use a map to deduplicate images across parent chart and all dependencies
+	imageSet := make(map[string]bool)
 
-	var chartImages ChartImages
+	// Recursively find all images in the chart and its dependencies
+	findImagesRecursive(chart, values, imageSet)
 
-	err = yaml.Unmarshal([]byte(imageAnnotation), &chartImages)
-	if err != nil {
-		return images, err
-	}
-
-	for _, i := range chartImages {
-		// Only include the image if the current values/condition specify it should be included
-		if i.Condition != "" {
-			value, err := values.PathValue(i.Condition)
-			// We intentionally ignore the error here because the key could be missing from the values.yaml
-			if err == nil && value == true {
-				images = append(images, i.Image)
-			}
-		} else {
-			images = append(images, i.Image)
-		}
+	// Convert set to slice
+	for image := range imageSet {
+		images = append(images, image)
 	}
 
 	return images, nil
+}
+
+// findImagesRecursive recursively finds images in a chart and its dependencies.
+func findImagesRecursive(c *chart.Chart, values chartutil.Values, imageSet map[string]bool) {
+	// Process current chart's annotations
+	if imageAnnotation, ok := c.Metadata.Annotations["helm.sh/images"]; ok && imageAnnotation != "" {
+		var chartImages ChartImages
+		if err := yaml.Unmarshal([]byte(imageAnnotation), &chartImages); err == nil {
+			for _, i := range chartImages {
+				if shouldIncludeImage(i, values) {
+					imageSet[i.Image] = true
+				}
+			}
+		}
+	}
+
+	// Process dependencies recursively
+	for _, depChart := range c.Dependencies() {
+		var subchartValues chartutil.Values
+		if depChart.Name() != "" {
+			// Try to access subchart values using the dependency name as the key
+			if depValues, ok := values[depChart.Name()].(map[string]interface{}); ok {
+				subchartValues = chartutil.Values(depValues)
+			} else {
+				// If no specific values for this subchart, use empty values
+				subchartValues = chartutil.Values{}
+			}
+		} else {
+			subchartValues = chartutil.Values{}
+		}
+
+		findImagesRecursive(depChart, subchartValues, imageSet)
+	}
+}
+
+// shouldIncludeImage determines if an image should be included based on its condition.
+func shouldIncludeImage(img ChartImage, values chartutil.Values) bool {
+	if img.Condition == "" {
+		return true
+	}
+
+	value, err := values.PathValue(img.Condition)
+	return err == nil && value == true
 }
