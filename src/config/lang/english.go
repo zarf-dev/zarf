d69fd0be8070230e66362f00d7ce2b2a6c7854ea
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2025-10-24 10:57:07 -0400

    docs: mark `zarf dev generate` as stable (#4310)
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/config/lang/english.go
+++ src/config/lang/english.go
@@ -335,7 +335,7 @@ $ zarf package pull oci://ghcr.io/zarf-dev/packages/dos-games:1.2.0 -a skeleton`
 	CmdDevDeployLong       = "[beta] Creates and deploys a Zarf package from a given directory, setting options like YOLO mode for faster iteration."
 	CmdDevDeployFlagNoYolo = "Disable the YOLO mode default override and create / deploy the package as-defined"
 
-	CmdDevGenerateShort   = "[alpha] Creates a zarf.yaml automatically from a given remote (git) Helm chart"
+	CmdDevGenerateShort   = "Creates a zarf.yaml automatically from a given remote (git) Helm chart"
 	CmdDevGenerateExample = "zarf dev generate podinfo --url https://github.com/stefanprodan/podinfo.git --version 6.4.0 --gitPath charts/podinfo"
 
 	CmdDevPatchGitShort = "Converts all .git URLs to the specified Zarf HOST and with the Zarf URL pattern in a given FILE.  NOTE:\n" +
