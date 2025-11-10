abe706d7bfb09812fd053d176ee962cdb0bc3b90
Robin Lieb <34332703+robinlieb@users.noreply.github.com>
2025-11-10 19:56:47 +0100

    feat: add OCI support for Argo CD sources
    Signed-off-by: Robin Lieb <34332703+robinlieb@users.noreply.github.com>

--- src/internal/agent/hooks/argocd-application_test.go
+++ src/internal/agent/hooks/argocd-application_test.go
@@ -14,6 +14,8 @@ import (
 	"github.com/zarf-dev/zarf/src/internal/agent/operations"
 	"github.com/zarf-dev/zarf/src/pkg/state"
 	v1 "k8s.io/api/admission/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -33,10 +35,15 @@ func TestArgoAppWebhook(t *testing.T) {
 	t.Parallel()
 
 	ctx := context.Background()
-	s := &state.State{GitServer: state.GitServerInfo{
-		Address:      "https://git-server.com",
-		PushUsername: "a-push-user",
-	}}
+	s := &state.State{
+		GitServer: state.GitServerInfo{
+			Address:      "https://git-server.com",
+			PushUsername: "a-push-user",
+		},
+		RegistryInfo: state.RegistryInfo{
+			Address: "127.0.0.1:31999",
+		},
+	}
 	c := createTestClientWithZarfState(ctx, t, s)
 	handler := admission.NewHandler().Serve(ctx, NewApplicationMutationHook(ctx, c))
 
@@ -79,6 +86,93 @@ func TestArgoAppWebhook(t *testing.T) {
 			code: http.StatusOK,
 		},
 		{
+			name: "should be mutated for OCI repo",
+			admissionReq: createArgoAppAdmissionRequest(t, v1.Create, &Application{
+				Spec: ApplicationSpec{
+					Source: &ApplicationSource{RepoURL: "oci://ghcr.io/stefanprodan/charts/podinfo"},
+					Sources: []ApplicationSource{
+						{
+							RepoURL: "oci://ghcr.io/stefanprodan/manifests/podinfo",
+						},
+						{
+							RepoURL: "https://diff-git-server.com/almonds",
+						},
+					},
+				},
+			}),
+			patch: []operations.PatchOperation{
+				operations.ReplacePatchOperation(
+					"/spec/source/repoURL",
+					"oci://127.0.0.1:31999/stefanprodan/charts/podinfo",
+				),
+				operations.ReplacePatchOperation(
+					"/spec/sources/0/repoURL",
+					"oci://127.0.0.1:31999/stefanprodan/manifests/podinfo",
+				),
+				operations.ReplacePatchOperation(
+					"/spec/sources/1/repoURL",
+					"https://git-server.com/a-push-user/almonds-640159520",
+				),
+				operations.ReplacePatchOperation(
+					"/metadata/labels",
+					map[string]string{
+						"zarf-agent": "patched",
+					},
+				),
+			},
+			code: http.StatusOK,
+		},
+		{
+			name: "should be mutated for OCI repo with internal service registry",
+			admissionReq: createArgoAppAdmissionRequest(t, v1.Create, &Application{
+				Spec: ApplicationSpec{
+					Source: &ApplicationSource{RepoURL: "oci://ghcr.io/stefanprodan/charts/podinfo"},
+					Sources: []ApplicationSource{
+						{
+							RepoURL: "oci://ghcr.io/stefanprodan/manifests/podinfo",
+						},
+					},
+				},
+			}),
+			patch: []operations.PatchOperation{
+				operations.ReplacePatchOperation(
+					"/spec/source/repoURL",
+					"oci://10.11.12.13:5000/stefanprodan/charts/podinfo",
+				),
+				operations.ReplacePatchOperation(
+					"/spec/sources/0/repoURL",
+					"oci://10.11.12.13:5000/stefanprodan/manifests/podinfo",
+				),
+				operations.ReplacePatchOperation(
+					"/metadata/labels",
+					map[string]string{
+						"zarf-agent": "patched",
+					},
+				),
+			},
+			svc: &corev1.Service{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: corev1.SchemeGroupVersion.String(),
+					Kind:       "Service",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "zarf-docker-registry",
+					Namespace: "zarf",
+				},
+				Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeNodePort,
+					Ports: []corev1.ServicePort{
+						{
+							NodePort: 31999,
+							Port:     5000,
+						},
+					},
+					ClusterIP: "10.11.12.13",
+				},
+			},
+			code: http.StatusOK,
+		},
+		{
 			name: "should return internal server error on bad git URL",
 			admissionReq: createArgoAppAdmissionRequest(t, v1.Create, &Application{
 				Spec: ApplicationSpec{
@@ -92,7 +186,10 @@ func TestArgoAppWebhook(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
+			if tt.svc != nil {
+				_, err := c.Clientset.CoreV1().Services("zarf").Create(ctx, tt.svc, metav1.CreateOptions{})
+				require.NoError(t, err)
+			}
 			rr := sendAdmissionRequest(t, tt.admissionReq, handler)
 			verifyAdmission(t, rr, tt)
 		})
