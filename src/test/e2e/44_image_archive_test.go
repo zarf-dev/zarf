a42e3d0d1792603d3d410d65fb77b938d5160c5f
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2025-12-18 13:29:01 -0500

    fix: use fully qualified reference during image archive duplication check (#4489)
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/test/e2e/44_image_archive_test.go
+++ src/test/e2e/44_image_archive_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/zarf-dev/zarf/src/pkg/packager/layout"
 )
 
-func TestImageTarPush(t *testing.T) {
+func TestImageArchivePush(t *testing.T) {
 	t.Log("E2E: image tar push")
 
 	pkgDefinitionPath := filepath.Join("src", "test", "packages", "44-image-tar")
@@ -30,8 +30,6 @@ func TestImageTarPush(t *testing.T) {
 	err = pkgLayout.GetSBOM(t.Context(), sbomPath)
 	require.NoError(t, err)
 	require.FileExists(t, filepath.Join(sbomPath, "ghcr.io_zarf-dev_images_alpine_3.21.3.json"))
-	// Verify that the images sub-field in image archives was populated properly
-	require.Equal(t, []string{"ghcr.io/zarf-dev/images/alpine:3.21.3"}, pkgLayout.Pkg.Components[0].ImageArchives[0].Images)
 
 	_, _, err = e2e.Zarf(t, "package", "deploy", path, "--confirm", "--skip-version-check")
 	require.NoError(t, err)
