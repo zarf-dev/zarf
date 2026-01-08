cd96b4989e01e1ec67e787907e89e8df3636697b
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2026-01-08 09:04:04 -0500

    chore: remove re-declared vars in test loops (#4500)
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/internal/agent/hooks/flux-gitrepo_test.go
+++ src/internal/agent/hooks/flux-gitrepo_test.go
@@ -118,7 +118,6 @@ func TestFluxMutationWebhook(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			rr := sendAdmissionRequest(t, tt.admissionReq, handler)
--- src/internal/agent/hooks/flux-helmrepo_test.go
+++ src/internal/agent/hooks/flux-helmrepo_test.go
@@ -226,7 +226,6 @@ func TestFluxHelmMutationWebhook(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			c := createTestClientWithZarfState(ctx, t, s)
