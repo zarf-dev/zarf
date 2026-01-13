39471e80d404f95f0ecce1d20129e761e2c58818
Allen Conlon <software@conlon.dev>
2026-01-13 08:02:57 -0500

    chore: update test per pr comments
    Signed-off-by: Allen Conlon <software@conlon.dev>

--- src/test/e2e/45_volume_image_test.go
+++ src/test/e2e/45_volume_image_test.go
@@ -33,23 +33,23 @@ func TestVolumeImageMount(t *testing.T) {
 
 	if k8sVersion.LessThan(semver.MustParse("v1.35.0")) {
 		t.Skipf("Skipping test: Kubernetes version %s is less than 1.35.0", versionInfo.ServerVersion.GitVersion)
-	} else {
-		pkgDefinitionPath := filepath.Join("src", "pkg", "packager", "testdata", "find-images", "pod-volume-image")
-		tmpdir := t.TempDir()
-		_, _, err := e2e.Zarf(t, "package", "create", pkgDefinitionPath, "-o", tmpdir)
-		require.NoError(t, err)
+	} 
+	
+	pkgDefinitionPath := filepath.Join("src", "pkg", "packager", "testdata", "find-images", "pod-volume-image")
+	tmpdir := t.TempDir()
+	_, _, err := e2e.Zarf(t, "package", "create", pkgDefinitionPath, "-o", tmpdir)
+	require.NoError(t, err)
 
-		packageName := fmt.Sprintf("zarf-package-pod-volume-image-%s.tar.zst", e2e.Arch)
-		path := filepath.Join(tmpdir, packageName)
+	packageName := fmt.Sprintf("zarf-package-pod-volume-image-%s.tar.zst", e2e.Arch)
+	path := filepath.Join(tmpdir, packageName)
 
-		_, _, err = e2e.Zarf(t, "package", "deploy", path, "--confirm")
-		require.NoError(t, err)
+	_, _, err = e2e.Zarf(t, "package", "deploy", path, "--confirm")
+	require.NoError(t, err)
 
-		stdOut, _, err := e2e.Kubectl(t, "get", "pod", "-l", "app=agent", "-n", "pod-volume-image", "-o", "jsonpath={.items[].spec.volumes[].image.reference}")
-		require.NoError(t, err)
-		require.Equal(t, "127.0.0.1:31337/zarf-dev/zarf/agent:v0.68.1-zarf-2203613481", stdOut)
+	stdOut, _, err := e2e.Kubectl(t, "get", "pod", "-l", "app=agent", "-n", "pod-volume-image", "-o", "jsonpath={.items[].spec.volumes[].image.reference}")
+	require.NoError(t, err)
+	require.Equal(t, "127.0.0.1:31337/zarf-dev/zarf/agent:v0.68.1-zarf-2203613481", stdOut)
 
-		_, _, err = e2e.Zarf(t, "package", "remove", "pod-volume-image", "--confirm")
-		require.NoError(t, err)
-	}
+	_, _, err = e2e.Zarf(t, "package", "remove", "pod-volume-image", "--confirm")
+	require.NoError(t, err)
 }
