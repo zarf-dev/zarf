6a6e5fc651d60b0cb07d91f64c05cdc7e7ff5557
Allen Conlon <software@conlon.dev>
2026-03-04 18:53:37 -0500

    feat: update how the kubectl is called in docs
    Signed-off-by: Allen Conlon <software@conlon.dev>

--- src/cmd/kubectl.go
+++ src/cmd/kubectl.go
@@ -6,12 +6,14 @@ package cmd
 
 import (
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericiooptions"
+	cliflag "k8s.io/component-base/cli/flag"
 	kubectl "k8s.io/kubectl/pkg/cmd"
 
 	// Import to initialize client auth plugins.
-	"k8s.io/cli-runtime/pkg/genericiooptions"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
@@ -28,6 +30,22 @@ func newKubectlCommand() *cobra.Command {
 
 	kubectlCmd.Use = "kubectl"
 	kubectlCmd.Aliases = []string{"k"}
+	kubectlCmd = ReplaceCommandName("kubectl", "zarf tools kubectl", kubectlCmd)
+
+	kubectlCmd.SetGlobalNormalizationFunc(cliflag.WordSepNormalizeFunc)
+	kubectlCmd.SilenceErrors = true
 
 	return kubectlCmd
+}
+
+// Pulled from deckhouse-cli, thank you.
+// https://github.com/deckhouse/deckhouse-cli/blob/7e0c1e743b16c82134a062985dde161178bd45f6/cmd/commands/utils.go#L25
+func ReplaceCommandName(from, to string, c *cobra.Command) *cobra.Command {
+	c.Example = strings.ReplaceAll(c.Example, from, to)
+	// Need some investigation about links
+	// c.Long = strings.Replace(c.Long, from, to, -1)
+	for _, sub := range c.Commands() {
+		ReplaceCommandName(from, to, sub)
+	}
+	return c
 }
