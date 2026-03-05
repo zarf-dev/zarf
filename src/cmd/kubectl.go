7a84e3e9b1a326394b3f3e2c9d2c9fda29a80c87
Allen Conlon <software@conlon.dev>
2026-03-04 19:19:16 -0500

    ci: fix linting
    Signed-off-by: Allen Conlon <software@conlon.dev>

--- src/cmd/kubectl.go
+++ src/cmd/kubectl.go
@@ -17,7 +17,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
-// Updated command thanks to k0s
+// code credit, k0sproject/k0s
 // https://github.com/k0sproject/k0s/blob/df88db5f317bb84dcda797ff6a68956bc2e49683/cmd/kubectl/kubectl.go#L59
 func newKubectlCommand() *cobra.Command {
 	kubectlCmd := kubectl.NewKubectlCommand(kubectl.KubectlOptions{
@@ -38,7 +38,8 @@ func newKubectlCommand() *cobra.Command {
 	return kubectlCmd
 }
 
-// Pulled from deckhouse-cli, thank you.
+// ReplaceCommandName recursively replaces all references of one string with another in the Example string
+// code credit, deckhouse/deckhouse-cli
 // https://github.com/deckhouse/deckhouse-cli/blob/7e0c1e743b16c82134a062985dde161178bd45f6/cmd/commands/utils.go#L25
 func ReplaceCommandName(from, to string, c *cobra.Command) *cobra.Command {
 	c.Example = strings.ReplaceAll(c.Example, from, to)
