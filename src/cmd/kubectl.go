35c66ed64bd290ebbd240882fed1acecbdc2966a
Allen Conlon <software@conlon.dev>
2026-03-09 20:20:51 -0400

    feat: expanding support for kubectl plugins
    Signed-off-by: Allen Conlon <software@conlon.dev>

--- src/cmd/kubectl.go
+++ src/cmd/kubectl.go
@@ -5,11 +5,17 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"slices"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/genericiooptions"
-	kubectl "k8s.io/kubectl/pkg/cmd"
+	kubecmd "k8s.io/kubectl/pkg/cmd"
+	"k8s.io/kubectl/pkg/cmd/plugin"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 
 	// Import to initialize client auth plugins.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -18,7 +24,7 @@ import (
 // code credit, k0sproject/k0s
 // https://github.com/k0sproject/k0s/blob/df88db5f317bb84dcda797ff6a68956bc2e49683/cmd/kubectl/kubectl.go#L59
 func newKubectlCommand() *cobra.Command {
-	kubectlCmd := kubectl.NewKubectlCommand(kubectl.KubectlOptions{
+	kubectlCmd := kubecmd.NewKubectlCommand(kubecmd.KubectlOptions{
 		IOStreams: genericiooptions.IOStreams{
 			In:     os.Stdin,
 			Out:    os.Stdout,
@@ -30,5 +36,95 @@ func newKubectlCommand() *cobra.Command {
 	kubectlCmd.Aliases = []string{"k"}
 	kubectlCmd.SilenceErrors = true
 
+	hookKubectlPluginHandler(kubectlCmd)
+	patchPluginListSubcommand(kubectlCmd)
+
 	return ReplaceCommandName("kubectl", "zarf tools kubectl", kubectlCmd)
+}
+
+// hookKubectlPluginHandler patches the kubectl command in a way that it will
+// execute kubectl's plugin handler before actually executing the command.
+// code credit, k0sproject/k0s
+// https://github.com/k0sproject/k0s/blob/df88db5f317bb84dcda797ff6a68956bc2e49683/cmd/kubectl/kubectl.go#L83
+func hookKubectlPluginHandler(kubectlCmd *cobra.Command) {
+	originalFlagErrFunc := kubectlCmd.FlagErrorFunc()
+	kubectlCmd.SetFlagErrorFunc(func(cmd *cobra.Command, err error) error {
+		handleKubectlPlugins(kubectlCmd)
+		return originalFlagErrFunc(cmd, err)
+	})
+
+	originalPreRunE := kubectlCmd.PersistentPreRunE
+	kubectlCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		handleKubectlPlugins(kubectlCmd)
+
+		if cmd.Name() == cobra.ShellCompRequestCmd {
+			plugin.SetupPluginCompletion(cmd, args)
+		}
+
+		if cmd == kubectlCmd {
+			if err := cobra.NoArgs(cmd, args); err != nil {
+				return err
+			}
+		}
+
+		return originalPreRunE(cmd, args)
+	}
+}
+
+// handleKubectlPlugins calls kubectl's plugin handler and execs the plugin
+// without returning if there's any plugin available that handles the given
+// command line arguments. Will simply return otherwise.
+// code credit, k0sproject/k0s
+// https://github.com/k0sproject/k0s/blob/df88db5f317bb84dcda797ff6a68956bc2e49683/cmd/kubectl/kubectl.go#L134
+func handleKubectlPlugins(kubectlCmd *cobra.Command) {
+
+	calledAs := kubectlCmd.CalledAs()
+	if calledAs == "" {
+		return
+	}
+
+	argOffset := slices.Index(os.Args, calledAs)
+	if argOffset < 0 {
+		return
+	}
+
+	// var pluginCmd bool
+
+	// pluginCmd = kubectlCmd ==
+
+	fmt.Println("this is a test")
+	fmt.Println(argOffset)
+
+	_ = kubecmd.NewDefaultKubectlCommandWithArgs(kubecmd.KubectlOptions{
+		IOStreams: genericclioptions.IOStreams{
+			In:     kubectlCmd.InOrStdin(),
+			Out:    kubectlCmd.OutOrStdout(),
+			ErrOut: kubectlCmd.ErrOrStderr(),
+		},
+		Arguments: os.Args[argOffset:],
+		PluginHandler: &kubecmd.DefaultPluginHandler{
+			ValidPrefixes: plugin.ValidPluginFilenamePrefixes,
+		},
+	})
+}
+
+// patchPluginListSubcommand patches kubectl's "plugin list" command in a way
+// that it will look at the kubectl command, not at the zarf command for
+// detecting shadowed commands. Kubectl's current implementation of that command
+// looks at the root command for detecting collisions. In case of zarf, which
+// embeds kubectl as a subcommand rather than at the top level, this means that,
+// instead of looking at kubectl command itself, the logic would look at zarf and
+// produce the wrong output.
+// code credit, k0sproject/k0s
+// https://github.com/k0sproject/k0s/blob/df88db5f317bb84dcda797ff6a68956bc2e49683/cmd/kubectl/kubectl.go#L202
+func patchPluginListSubcommand(kubectlCmd *cobra.Command) {
+	cmd, _, err := kubectlCmd.Find([]string{"plugin", "list"})
+	cmdutil.CheckErr(err)
+
+	originalRun := cmd.Run
+	cmd.Run = func(cmd *cobra.Command, args []string) {
+		root := kubecmd.NewKubectlCommand(kubecmd.KubectlOptions{})
+		root.Use = strings.ReplaceAll(kubectlCmd.CommandPath(), " ", "-")
+		originalRun(root, args)
+	}
 }
