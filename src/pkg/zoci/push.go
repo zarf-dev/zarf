e9262d412aa04ca20400d6197c8b9c04e0028eb3
Austin Abro <37223396+AustinAbro321@users.noreply.github.com>
2026-03-17 12:41:23 -0400

    feat: use Zarf Package Config as image config (#4675)
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/pkg/zoci/push.go
+++ src/pkg/zoci/push.go
@@ -6,6 +6,7 @@ package zoci
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"maps"
@@ -101,15 +102,23 @@ func (r *Remote) PushPackage(ctx context.Context, pkgLayout *layout.PackageLayou
 	// this is a conservative total using layer sizes; progress still works fine.)
 	totalSize := oci.SumDescsSize(descs)
 
-	var publishedDesc ocispec.Descriptor
+	if annotations[ocispec.AnnotationTitle] == "" {
+		return ocispec.Descriptor{}, fmt.Errorf("invalid annotations: please include value for %q", ocispec.AnnotationTitle)
+	}
 
+	var publishedDesc ocispec.Descriptor
 	err = retry.Do(
 		func() error {
 			l.Info("pushing package to registry", "destination", r.Repo().Reference.String(),
 				"architecture", pkgLayout.Pkg.Build.Architecture, "size", utils.ByteFormat(float64(totalSize), 2))
-			manifestConfigDesc, cfgErr := r.OrasRemote.CreateAndPushManifestConfig(ctx, annotations, ZarfConfigMediaType)
-			if cfgErr != nil {
-				return cfgErr
+
+			manifestConfigBytes, err := json.Marshal(pkgLayout.Pkg)
+			if err != nil {
+				return err
+			}
+			manifestConfigDesc, err := r.PushLayer(ctx, manifestConfigBytes, ZarfConfigMediaType)
+			if err != nil {
+				return err
 			}
 
 			root, packErr := r.OrasRemote.PackAndTagManifest(ctx, src, descs, manifestConfigDesc, annotations)
@@ -128,7 +137,8 @@ func (r *Remote) PushPackage(ctx context.Context, pkgLayout *layout.PackageLayou
 			trackedRemote.StartReporting(ctx)
 			defer trackedRemote.StopReporting()
 
-			publishedDesc, copyErr := oras.Copy(ctx, src, root.Digest.String(), trackedRemote, "", copyOpts)
+			var copyErr error
+			publishedDesc, copyErr = oras.Copy(ctx, src, root.Digest.String(), trackedRemote, "", copyOpts)
 			if copyErr != nil {
 				return copyErr
 			}
