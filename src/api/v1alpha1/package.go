542af63b37167350199123eb50e0228896dbf15b
Austin Abro <austinabro321@gmail.com>
2026-04-21 11:15:24 -0400

    multi architecture docs
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/api/v1alpha1/package.go
+++ src/api/v1alpha1/package.go
@@ -224,7 +224,7 @@ type ZarfMetadata struct {
 	// Disable compression of this package.
 	Uncompressed bool `json:"uncompressed,omitempty"`
 	// The target cluster architecture for this package.
-	Architecture string `json:"architecture,omitempty" jsonschema:"example=arm64,example=amd64"`
+	Architecture string `json:"architecture,omitempty" jsonschema:"example=arm64,example=amd64,example=multi"`
 	// Yaml OnLy Online (YOLO): True enables deploying a Zarf package without first running zarf init against the cluster. This is ideal for connected environments where you want to use existing VCS and container registries.
 	YOLO bool `json:"yolo,omitempty"`
 	// Comma-separated list of package authors (including contact info).
