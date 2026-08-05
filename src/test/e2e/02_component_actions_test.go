ce5cbf315810259b068700081196196b7427b19d
Austin Abro <austinabro321@gmail.com>
2026-08-05 15:29:24 -0400

    fix flakey test
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/test/e2e/02_component_actions_test.go
+++ src/test/e2e/02_component_actions_test.go
@@ -58,7 +58,7 @@ func TestComponentActions(t *testing.T) {
 		t.Parallel()
 
 		// Deploy the simple script that should pass.
-		stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-and-remove", "--confirm")
+		stdOut, stdErr, err := e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-and-remove", "--confirm")
 		require.NoError(t, err, stdOut, stdErr)
 
 		// Check that the deploy artifacts were created.
@@ -79,7 +79,7 @@ func TestComponentActions(t *testing.T) {
 	t.Run("action on-deploy-with-timeout", func(t *testing.T) {
 		t.Parallel()
 		// Deploy the simple action that should fail the timeout.
-		stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-timeout", "--confirm")
+		stdOut, stdErr, err := e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-timeout", "--confirm")
 		require.Error(t, err, stdOut, stdErr)
 		require.Contains(t, stdErr, "after 1 second")
 		require.Contains(t, stdOut, "😭😭😭 this action failed because it took too long to run 😭😭😭")
@@ -89,7 +89,7 @@ func TestComponentActions(t *testing.T) {
 		t.Parallel()
 
 		// Test using a Zarf Variable within the action
-		stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-variable", "--confirm")
+		stdOut, stdErr, err := e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-variable", "--confirm")
 		require.NoError(t, err, stdOut, stdErr)
 		require.Contains(t, stdOut, "the dog says ruff")
 	})
@@ -97,7 +97,7 @@ func TestComponentActions(t *testing.T) {
 	t.Run("action on-deploy-with-dynamic-variable", func(t *testing.T) {
 		t.Parallel()
 		// Test using dynamic and multiple-variables
-		stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-dynamic-variable,on-deploy-with-multiple-variables", "--confirm")
+		stdOut, stdErr, err := e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-dynamic-variable,on-deploy-with-multiple-variables", "--confirm")
 		require.NoError(t, err, stdOut, stdErr)
 		require.Contains(t, stdOut, "the cat says meow")
 		require.Contains(t, stdOut, "the dog says ruff")
@@ -109,7 +109,7 @@ func TestComponentActions(t *testing.T) {
 		deployWithEnvVarArtifact := "test-filename-from-env.txt"
 
 		// Test using environment variables
-		stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-env-var", "--confirm")
+		stdOut, stdErr, err := e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-env-var", "--confirm")
 		require.NoError(t, err, stdOut, stdErr)
 		require.FileExists(t, deployWithEnvVarArtifact)
 
@@ -122,7 +122,7 @@ func TestComponentActions(t *testing.T) {
 		deployTemplatedArtifact := "test-templated.txt"
 
 		// Test using a templated file but without dynamic variables
-		stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-template-use-of-variable", "--confirm")
+		stdOut, stdErr, err := e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-with-template-use-of-variable", "--confirm")
 		require.NoError(t, err, stdOut, stdErr)
 		outTemplated, err := os.ReadFile(deployTemplatedArtifact)
 		require.NoError(t, err)
@@ -148,7 +148,7 @@ func TestComponentActions(t *testing.T) {
 
 	t.Run("action on-deploy-immediate-failure", func(t *testing.T) {
 		t.Parallel()
-		stdOut, stdErr, err = e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-immediate-failure", "--confirm")
+		stdOut, stdErr, err := e2e.Zarf(t, "package", "deploy", path, "--components=on-deploy-immediate-failure", "--confirm")
 		require.Error(t, err, stdOut, stdErr)
 		require.Contains(t, stdErr, "failed to deploy package")
 		// regression test to ensure that failed commands are not erroneously flagged as a timeout
