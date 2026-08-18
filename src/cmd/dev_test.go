e187f613e95a2068267538c47cd3073f96aa669a
Allen Conlon <software@conlon.dev>
2026-08-18 12:01:10 -0400

    feat: remove net tests
    Signed-off-by: Allen Conlon <software@conlon.dev>

--- src/cmd/dev_test.go
+++ src/cmd/dev_test.go
@@ -286,35 +286,6 @@ func TestDevSha256Sum(t *testing.T) {
 		expectedErr string
 	}{
 		{
-			// this is the version before we vendored the go depends, and is a lot quicker to download
-			name:      "dev sha tarball source v0.80.0",
-			sourceURL: "https://github.com/zarf-dev/zarf/archive/refs/tags/v0.80.0.tar.gz",
-			sha256sum: "67b5d5a2801db7dcce5f7d8ef46e36515a56a9b5f952f39cbe36166018167410",
-		},
-		{
-			// this is the version before we vendored the go depends, and is a lot quicker to download
-			name:        "dev sha tarball source v0.80.0 with extract path",
-			sourceURL:   "https://github.com/zarf-dev/zarf/archive/refs/tags/v0.80.0.tar.gz",
-			extractPath: "zarf-0.80.0/cosign.pub",
-			sha256sum:   "8361bdbf3fb0c5d2980c5cb2192536b81ba439466d85b8328f7bf5de6fce58eb",
-		},
-		{
-			name:      "dev sha tarball source v0.30.0",
-			sourceURL: "https://github.com/zarf-dev/zarf/archive/refs/tags/v0.30.0.tar.gz",
-			sha256sum: "a042c9ffec7907101b58dd5b1aee6e54d08d17d91e7e2572da22e4180e02decd",
-		},
-		{
-			name:        "dev sha tarball source v0.30.0 with extract path",
-			sourceURL:   "https://github.com/zarf-dev/zarf/archive/refs/tags/v0.30.0.tar.gz",
-			extractPath: "zarf-0.30.0/cosign.pub",
-			sha256sum:   "2aac6a3c85d8513545fa85c11ee0d58ea5125095052f77e8d33f22402dbe6e4e",
-		},
-		{
-			name:        "dev sha no url",
-			sourceURL:   "",
-			expectedErr: "accepts 1 arg(s), received 0",
-		},
-		{
 			name:      "dev sha of tar ball",
 			sourceURL: filepath.Join("..", "pkg", "packager", "assemble", "testdata", "zarf-package", "archive.tar"),
 			sha256sum: "69b50ad92da0d32398fe1e5950ab28991c2220603daa2bce1a470e9afde95687",
@@ -326,10 +297,9 @@ func TestDevSha256Sum(t *testing.T) {
 			extractPath: "archive-data.txt",
 		},
 		{
-			name:      "dev sha url does not exist",
-			sourceURL: "https://github.com/zarf-dev/zarf/archive/refs/tags/v99.99.99.tar.gz",
-			expectedErr: `unable to compute the SHA256SUM hash
-bad HTTP status: 404 Not Found`,
+			name:        "dev sha file does not exist",
+			sourceURL:   filepath.Join("..", "pkg", "packager", "assemble", "testdata", "zarf-package", "archive.tar.zstd"),
+			expectedErr: "unable to compute the SHA256SUM hash",
 		},
 	}
 
